8d3c33e92ff55a1bc798ab6d0792ef12d827394d
Martin Bernstorff <ryqiem@gmail.com>
2022-03-24 13:38:20 +0100

    Misc. updates (comitting to I can generalise to cancer project)

--- src/functions.r
+++ src/functions.r
@@ -1,4 +1,4 @@
-event_start_date_str <- "2014-01-01"
+event_start_date <- ymd("2014-01-01")
 
 str_contains_t2d_diag <- function(str) {
   t2d_regex_pattern <- "(:DE1[1-5].*)|(:DE16[0-2].*)|(:DO24.*)|(:DT383A.*)|(:DM142.*)|(:DG590.*)|(:DG632*)|(:DH280.*)|(:DH334.*)|(:DH360.*)|(:DH450.*)|(:DN083.*)"
@@ -22,9 +22,9 @@ keep_only_first_t2d_by_diag <- function(df, date_col_string) {
 }
 
 str_contains_t1d_diag <- function(str) {
-  t2d_regex_pattern <- "(:DE10.*)|(:DO240.*)"
+  t1d_regex_pattern <- "(:DE10.*)|(:DO240.*)"
 
-  if (isTRUE(str_detect(str, t2d_regex_pattern))) {
+  if (isTRUE(str_detect(str, t1d_regex_pattern))) {
     return(TRUE)
   }
   
