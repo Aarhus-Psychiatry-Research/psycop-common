1e67671d926e94566b0556b1f8ec0386bebf1b38
Martin Bernstorff <martinbernstorff@gmail.com>
2022-06-13 11:28:42 +0200

    Blackify

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Prediction of type 2 diabetes among patients with visits to psychiatric hospital departments.',
-    author='psycop',
-    license='',
+    version="0.1.0",
+    description="Prediction of type 2 diabetes among patients with visits to psychiatric hospital departments.",
+    author="psycop",
+    license="",
 )
