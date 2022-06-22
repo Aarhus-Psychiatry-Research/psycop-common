cd06f54a27709a3a75dac8815925ce2c8c510ccf
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-06-22 16:57:43 +0200

    added test

---
+++ tests/test_visualizations.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import pandas as pd
+import altair as alt
+
+from psycopt2d.visualization import plot_prob_over_time
+
+
+def test_prob_over_time():
+    """Test visualization runs on test set"""
+    repo_path = Path(__file__).parent
+    path = repo_path / "tests" / "test_data" / "synth_data.csv"
+    df = pd.read_csv(path)
+    alt.data_transformers.disable_max_rows()
+
+    plot_prob_over_time(
+        patient_id=df["dw_ek_borger"],
+        timestamp=df["timestamp"],
+        pred_prob=df["pred_prob"],
+        outcome_timestamp=df["timestamp_t2d_diag"],
+        label=df["label"],
+        look_behind=500,
+    )
