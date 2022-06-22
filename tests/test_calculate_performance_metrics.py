3b8f3f1ac28d92744b1a76d0ec775e6e0eec98d5
Lasse <lasseh0310@gmail.com>
2022-06-22 15:08:37 +0200

    remove wandb

--- tests/test_calculate_performance_metrics.py
+++ tests/test_calculate_performance_metrics.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
-import wandb
+# import wandb
 from psycopt2d.utils import calculate_performance_metrics
 
 
@@ -13,12 +13,15 @@ def synth_data():
 
 
 def test_log_performance_metrics(synth_data):
-    wandb.init(project="test")
+    # wandb.init(project="test")
     perf = calculate_performance_metrics(
         synth_data,
         outcome_col_name="label",
         prediction_probabilities_col_name="pred_prob",
         id_col_name="dw_ek_borger",
     )
-    wandb.log(perf)
-    wandb.finish()
+    # stupid test - is actested in psycop-ml-utils
+    # mainly used to test wandb but doesn't work GH actions
+    assert type(perf) == dict
+    # wandb.log(perf)
+    # wandb.finish()
