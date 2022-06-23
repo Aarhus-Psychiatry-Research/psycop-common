dbd1c805ff4414ca6b843dd4dc28c3ef757d3a05
Lasse <lasseh0310@gmail.com>
2022-06-23 13:30:04 +0200

    add font_size and doc string

--- src/psycopt2d/visualization/altair_utils.py
+++ src/psycopt2d/visualization/altair_utils.py
@@ -1,43 +1,31 @@
 from pathlib import Path
 from typing import Union
 
-import altair as alt
-import pandas as pd
 from altair.vegalite.v4.api import Chart
 
 
 def altair_chart_to_png(
     chart: Chart,
     filename: Union[str, Path],
-    height: 400,
-    width: 300,
+    height: int = 400,
+    width: int = 300,
+    font_size: int = 18,
 ) -> Path:
-    """TODO set axis labels font size.
+    """Helper to save Altair charts.
 
     Args:
         chart (Chart): Altair chart
         filename (Union[str, Path]): Filename. Suffix determines the file type
-        height (400): Height in pixels
-        width (300): Width in pixels
+        height (int): Height in pixels
+        width (int): Width in pixels
+        font_size (int): How large the axis labels should be
 
     Returns:
         Path: Path to the file (filename)
     """
     filename = Path(filename)
     chart = chart.properties(height=height, width=width)
+    chart = chart.configure_axis(labelFontSize=font_size, titleFontSize=font_size)
     chart.save(filename)
 
     return filename
-
-
-if __name__ == "__main__":
-
-    source = pd.DataFrame(
-        {
-            "a": ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
-            "b": [28, 55, 43, 91, 81, 53, 19, 87, 52],
-        },
-    )
-    chart = alt.Chart(source).mark_bar().encode(x="a", y="b")
-
-    chart = chart.properties(height=400, width=300)
