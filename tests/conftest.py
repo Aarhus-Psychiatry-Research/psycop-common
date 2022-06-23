16d0423565c2d7538798040f8b19a55c90cd8bf1
Lasse <lasseh0310@gmail.com>
2022-06-23 15:13:38 +0200

    move test utils

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,8 @@
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 import pytest
-
-
-def add_age_gender(df):
-    ids = pd.DataFrame({"dw_ek_borger": df["dw_ek_borger"].unique()})
-    ids["age"] = np.random.randint(17, 95, len(ids))
-    ids["gender"] = np.where(ids["dw_ek_borger"] > 30_000, "F", "M")
-
-    return df.merge(ids)
+from utils_for_testing import add_age_gender
 
 
 @pytest.fixture
