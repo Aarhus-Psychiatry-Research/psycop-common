89bf670902c2b0444238ffbd0d012ea250f14ae0
Martin Bernstorff <martinbernstorff@gmail.com>
2022-10-10 13:56:10 +0200

    chore: hide only 1 class AUC message

--- src/psycopt2d/visualization/performance_over_time.py
+++ src/psycopt2d/visualization/performance_over_time.py
@@ -29,7 +29,10 @@ def _calc_performance(df: pd.DataFrame, metric: Callable) -> float:
     if df.empty:
         return np.nan
     elif metric is roc_auc_score and len(df["y"].unique()) == 1:
-        msg.info("Only 1 class present in bin. AUC undefined. Returning np.nan")
+        # msg.info("Only 1 class present in bin. AUC undefined. Returning np.nan") This was hit almost once per month, making it very hard to read.
+        # Many of our models probably try to predict the majority class.
+        # I'm not sure how exactly we want to handle this, but thousands of msg.info is not ideal.
+        # For now, suppressing this message.
         return np.nan
     else:
         return metric(df["y"], df["y_hat"])
@@ -56,7 +59,9 @@ def create_performance_by_calendar_time_df(
     """
     df = pd.DataFrame({"y": labels, "y_hat": y_hat, "timestamp": timestamps})
     df["time_bin"] = df["timestamp"].astype(f"datetime64[{bin_period}]")
+
     output_df = df.groupby("time_bin").apply(_calc_performance, metric_fn)
+
     output_df = output_df.reset_index().rename({0: "metric"}, axis=1)
     return output_df
 
