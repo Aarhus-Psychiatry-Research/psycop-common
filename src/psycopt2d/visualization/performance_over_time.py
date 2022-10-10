ae12b23cfcb00af264813a92b9a6975928a28825
Martin Bernstorff <martinbernstorff@gmail.com>
2022-10-10 13:56:32 +0200

    lint: remove unused imports
    Don't show in message because it's being solved by autoflake.

--- src/psycopt2d/visualization/performance_over_time.py
+++ src/psycopt2d/visualization/performance_over_time.py
@@ -10,7 +10,6 @@ from typing import Optional, Union
 import numpy as np
 import pandas as pd
 from sklearn.metrics import f1_score, roc_auc_score
-from wasabi import msg
 
 from psycopt2d.utils import bin_continuous_data, round_floats_to_edge
 from psycopt2d.visualization.base_charts import plot_basic_chart
