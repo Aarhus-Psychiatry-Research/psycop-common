0cb1b2d83bca6152721d49d99d49452c3dcb3bd4
Martin Bernstorff <martinbernstorff@gmail.com>
2022-10-14 11:44:52 +0200

    linting

--- tests/test_load.py
+++ tests/test_load.py
@@ -5,7 +5,8 @@ from psycopt2d.load import load_train_and_val_from_file
 
 
 def test_load_feat_lookbehind_larger_than_min_lookbehind():
-    """Test that columns are dropped if their lookbehind are larger than min_lookbehind."""
+    """Test that columns are dropped if their lookbehind are larger than
+    min_lookbehind."""
     with initialize(version_base=None, config_path="../src/psycopt2d/config/"):
         cfg = compose(
             config_name="integration_testing.yaml",
