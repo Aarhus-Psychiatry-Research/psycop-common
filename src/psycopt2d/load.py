dec135510d912052327a5bb26d94a96ef0dc68d9
Martin Bernstorff <martinbernstorff@gmail.com>
2022-10-17 15:22:42 +0200

    refactor: split DatasetSpec into more classes

--- src/psycopt2d/load.py
+++ src/psycopt2d/load.py
@@ -1,5 +1,4 @@
 """Loader for the t2d dataset."""
-# from datetime import date, datetime, timedelta
 import re
 from collections.abc import Iterable
 from datetime import datetime, timedelta
@@ -17,19 +16,37 @@ from psycopt2d.utils import PROJECT_ROOT, coerce_to_datetime
 msg = Printer(timestamp=True)
 
 
+class DatasetTimeSpec(BaseModel):
+    """Specification of the time range of the dataset."""
+
+    # If a patient experiences the outcome before this date, all their prediction
+    # times will be dropped. Used for wash-in, to avoid including patients who
+    # were probably already experiencing the outcome before the study began.
+    drop_patient_if_outcome_before_date: Optional[Union[str, datetime]] = None
+
+    # Any prediction time before this date will be dropped
+    min_prediction_time_date: Optional[Union[str, datetime]] = None
+
+    # If the distance from the prediction time to the start of the dataset is less than this, the prediction time will be dropped
+    min_lookbehind_days: Optional[Union[int, float]] = None
+
+    # If the distance from the prediction time to the end of the dataset is less than this, the prediction time will be dropped
+    min_lookahead_days: Optional[Union[int, float]] = None
+
+
 class DatasetSpec(BaseModel):
     """Specification for loading a dataset."""
 
     split_names: Union[str, Iterable[str]]
-    split_dir_path: Union[str, Path]
-    drop_patient_if_outcome_before_date: Optional[Union[str, datetime]] = None
-    min_prediction_time_date: Optional[Union[str, datetime]] = None
-    pred_col_name_prefix: str = "pred_"
-    file_suffix: str = "parquet"
-    pred_time_colname: str = "timestamp"
     n_training_samples: Optional[int] = None
-    min_lookahead_days: Optional[Union[int, float]] = None
-    min_lookbehind_days: Optional[Union[int, float]] = None
+
+    split_dir_path: Union[str, Path]
+    file_suffix: str = "parquet"
+
+    time: DatasetTimeSpec
+
+    pred_col_name_prefix: str = "pred_"
+    pred_time_colname: str = "timestamp"
 
 
 def add_washin_timestamps(dataset: pd.DataFrame) -> pd.DataFrame:
@@ -59,12 +76,14 @@ class DataSplit:
         self,
         spec: DatasetSpec,
     ):
+        self.spec = spec
+
         # Init all the args
         self.split_names = spec.split_names
         self.pred_time_colname = spec.pred_time_colname
 
         # File handling
-        self.dir_path = spec.split_dir_path
+        self.dir_path = Path(spec.split_dir_path)
         self.file_suffix = spec.file_suffix
 
         # Column specifications
@@ -75,25 +94,15 @@ class DataSplit:
 
         # Computation
         self.train_samples = (
-            int(self.n_training_samples * 0.7) if self.n_training_samples else None
+            int(spec.n_training_samples * 0.7) if spec.n_training_samples else None
         )
         self.val_samples = (
-            int(self.n_training_samples * 0.3) if self.n_training_samples else None
+            int(spec.n_training_samples * 0.3) if spec.n_training_samples else None
         )
-
-        # Ahead
-        self.min_lookahead_days = spec.min_lookahead_days
-
-        # Behind
-        self.drop_patient_if_outcome_before_date = (
-            spec.drop_patient_if_outcome_before_date
-        )
-        self.min_prediction_time_date = spec.min_prediction_time_date
-        self.min_lookbehind_days = spec.min_lookbehind_days
 
         self.df = self.load_dataset_from_dir(
-            split_names=self.split_names,
-            nrows=self.n_training_samples,
+            split_names=spec.split_names,
+            nrows=spec.n_training_samples,
         )
 
     def load_dataset_from_dir(
@@ -112,13 +121,16 @@ class DataSplit:
             pd.DataFrame: The filtered dataset
         """
         # Handle input types
-        for timedelta_arg in (self.min_lookbehind_days, self.min_lookahead_days):
+        for timedelta_arg in (
+            self.spec.time.min_lookbehind_days,
+            self.spec.time.min_lookahead_days,
+        ):
             if timedelta_arg:
                 timedelta_arg = timedelta(days=timedelta_arg)  # type: ignore
 
         for date_arg in (
-            self.drop_patient_if_outcome_before_date,
-            self.min_prediction_time_date,
+            self.spec.time.drop_patient_if_outcome_before_date,
+            self.spec.time.min_prediction_time_date,
         ):
             if isinstance(date_arg, str):
                 date_arg = coerce_to_datetime(
@@ -223,7 +235,7 @@ class DataSplit:
 
         outcome_before_date = (
             dataset["timestamp_first_diabetes_any"]
-            < self.drop_patient_if_outcome_before_date
+            < self.spec.time.drop_patient_if_outcome_before_date
         )
 
         patients_to_drop = set(dataset["dw_ek_borger"][outcome_before_date].unique())
@@ -231,7 +243,8 @@ class DataSplit:
 
         # Removed dates before drop_patient_if_outcome_before_date
         dataset = dataset[
-            dataset[self.pred_time_colname] > self.drop_patient_if_outcome_before_date
+            dataset[self.pred_time_colname]
+            > self.spec.time.drop_patient_if_outcome_before_date
         ]
 
         return dataset
@@ -300,29 +313,48 @@ class DataSplit:
         Returns:
             pd.DataFrame: Processed dataset
         """
-        if self.drop_patient_if_outcome_before_date:
+        if self.spec.time.drop_patient_if_outcome_before_date:
             dataset = add_washin_timestamps(dataset=dataset)
 
         dataset = self.process_timestamp_dtype_and_nat(dataset)
-        if self.drop_patient_if_outcome_before_date:
+        if self.spec.time.drop_patient_if_outcome_before_date:
             dataset = self.drop_patients_with_event_in_washin(dataset=dataset)
 
         # Drop if later than min prediction time date
-        if self.min_prediction_time_date:
+        if self.spec.time.min_prediction_time_date:
             dataset = dataset[
-                dataset[self.pred_time_colname] > self.min_prediction_time_date
+                dataset[self.pred_time_colname]
+                > self.spec.time.min_prediction_time_date
             ]
 
+        dataset = self.drop_cols_and_rows_if_look_direction_not_met(dataset=dataset)
+
+        return dataset
+
+    def drop_cols_and_rows_if_look_direction_not_met(
+        self,
+        dataset: pd.DataFrame,
+    ) -> pd.DataFrame:
+        """Drop columns if they are outside the specification. Specifically:
+
+        - min_lookahead_days is insufficient for the column's lookahead
+        - min_lookbehind_days is insufficient for the column's lookbehind
+        - The dataset doesn't stretch far enough for the prediction time's lookahead
+        - The dataset doesn't stretch far enough for the prediction time's lookbehind
+
+        Args:
+            dataset (pd.DataFrame): Dataset to process.
+        """
         for direction in ("ahead", "behind"):
             if direction == "ahead":
-                if self.min_lookahead_days:
-                    n_days = self.min_lookahead_days
+                if self.spec.time.min_lookahead_days:
+                    n_days = self.spec.time.min_lookahead_days
                 else:
                     continue
 
             if direction == "behind":
-                if self.min_lookbehind_days:
-                    n_days = self.min_lookbehind_days
+                if self.spec.time.min_lookbehind_days:
+                    n_days = self.spec.time.min_lookbehind_days
                 else:
                     continue
 
@@ -353,17 +385,21 @@ def init_spec_from_cfg(cfg: DictConfig, split_name: str = "train") -> DatasetSpe
         split_dir_path = data_cfg.dir
         file_suffix = cfg.data.source
 
+    time_spec = DatasetTimeSpec(
+        drop_patient_if_outcome_before_date=data_cfg.drop_patient_if_outcome_before_date,
+        min_lookahead_days=data_cfg.min_lookahead_days,
+        min_lookbehind_days=data_cfg.min_lookbehind_days,
+        min_prediction_time_date=data_cfg.min_prediction_time_date,
+    )
+
     return DatasetSpec(
         split_names=split_name,
         split_dir_path=split_dir_path,
-        drop_patient_if_outcome_before_date=data_cfg.drop_patient_if_outcome_before_date,
-        min_prediction_time_date=data_cfg.min_prediction_time_date,
         pred_col_name_prefix=data_cfg.pred_col_name_prefix,
         file_suffix=file_suffix,
         pred_time_colname=data_cfg.pred_timestamp_col_name,
         n_training_samples=data_cfg.n_training_samples,
-        min_lookahead_days=data_cfg.min_lookahead_days,
-        min_lookbehind_days=data_cfg.min_lookbehind_days,
+        time=time_spec,
     )
 
 
