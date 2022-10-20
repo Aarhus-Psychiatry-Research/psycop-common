8f004a95fad22b91c2b639d9b6d9a2ba20aea9e1
bokajgd <bokajgd@gmail.com>
2022-10-20 11:28:06 +0200

    adding plot function

--- src/psycopt2d/visualization/performance_over_n_hba1c.py
+++ src/psycopt2d/visualization/performance_over_n_hba1c.py
@@ -18,7 +18,6 @@ def create_performance_by_n_hba1c(
     y_hat: Iterable[int],
     n_hba1c: Iterable[int],
     metric_fn: Callable,
-    bin_period: str,
 ) -> pd.DataFrame:
     """Calculate performance by number of HbA1c measurements.
 
@@ -38,3 +37,41 @@ def create_performance_by_n_hba1c(
 
     output_df = output_df.reset_index().rename({0: "metric"}, axis=1)
     return output_df
+
+
+def plot_performance_by_n_hba1c(
+    labels: Iterable[int],
+    y_hat_probs: Iterable[int],
+    n_hba1c: Iterable[int],
+    save_path: Optional[Path] = None,
+) -> Union[None, Path]:
+    """Plot bar plot of AUC by number of HbA1c measurements.
+
+    Args:
+        labels (Iterable[int]): True labels
+        y_hat_probs (Iterable[int]): Predicted probabilities
+        prediction_timestamps (Iterable[pd.Timestamp]): Timestamps of the predictions
+        n_hba1c (Iterable[int]): Number of HbA1c measurements
+        save_path (Path, optional): Path to save figure. Defaults to None.
+
+    Returns:
+        Union[None, Path]: Path to saved figure or None if not saved.
+    """
+
+    df = create_performance_by_n_hba1c(
+        labels=labels,
+        y_hat=y_hat_probs,
+        n_hba1c=n_hba1c,
+        metric_fn=roc_auc_score,
+    )
+
+    sort_order = np.arange(n_hba1c.values)
+    return plot_basic_chart(
+        x_values=df["n_hba1c"],
+        y_values=df["metric"],
+        x_title="Number of HbA1c measurements",
+        y_title="AUC",
+        sort_x=sort_order,
+        plot_type=["bar"],
+        save_path=save_path,
+    )
