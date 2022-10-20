0d460e84f7d3cee6e40d90c9f6b8d96542099540
Martin Bernstorff <martinbernstorff@gmail.com>
2022-10-20 15:48:20 +0200

    refactor: use objects

--- application/train_and_log_models.py
+++ application/train_and_log_models.py
@@ -5,12 +5,9 @@ Usage:
 - Replace the HYDRA_ARGS string with the desired arguments for `train_model.py`
 - Run this script from project root with `python src/psycopt2d/train_and_log_models.py
 """
-import os
 import subprocess
 import time
-from collections.abc import Iterable
 from pathlib import Path
-from typing import Union
 
 from pydantic import BaseModel, Field
 from wasabi import Printer
@@ -26,8 +23,8 @@ from psycopt2d.load import DataLoader, DatasetSpecification, DatasetTimeSpecific
 class PossibleLookDistanceDays(BaseModel):
     """Possible look distances."""
 
-    ahead: Iterable[Union[int, float]]
-    behind: Iterable[Union[int, float]]
+    ahead: list[str]
+    behind: list[str]
 
 
 class MetaConf(BaseModel):
@@ -44,7 +41,7 @@ class MetaConf(BaseModel):
 
 
 class WatcherConf(BaseModel):
-    """Confiugration for the watcher."""
+    """Configuration for the watcher."""
 
     archive_all: str = Field(
         default="false",
@@ -68,23 +65,31 @@ class WandbConf(BaseModel):
         default="psycop",
         description="The wandb entity to upload to (e.g. 'psycop' or your user name)",
     )
+    mode: str = Field(default="online", description="The wandb mode to use")
 
 
 class TrainConf(BaseModel):
     """Configuration for model training."""
+
+    gpu: bool = Field(default="false", description="Whether to use GPU")
 
     n_trials_per_cell_in_grid: int = Field(
         default=50,
         description="Number of trials per cell in the lookahead/lookbehind grid",
     )
 
-    conf_name: str = Field(default="integration_testing.yaml")
-
-    base_args: str = Field(
-        default=f"--multirun +model=xgboost project.wandb_mode='dryrun' model.args.tree_method='auto' --config-name {conf_name}",
+    model_conf: str = Field(
+        default="xgboost",
+        description="The model conf to open. For example, 'xgboost' or 'logistic_regression'.",
     )
 
-    possible_look_distance: PossibleLookDistanceDays
+    conf_name: str = Field(default="integration_testing.yaml")
+
+    multirun: bool = Field(
+        default=False, description="Whether to use Hydra to run multiple models."
+    )
+
+    possible_look_distances: PossibleLookDistanceDays
 
 
 def load_data(dataset_spec):
@@ -99,15 +104,11 @@ def infer_possible_look_directions(train):
     # Get potential lookaheads from outc_ columns
     outcome_col_names = infer_outcome_col_name(df=train, allow_multiple=True)
 
-    possible_lookahead_days = set(
-        infer_look_distance(
-            col_name=outcome_col_names,
-        ),
-    )
+    possible_lookahead_days = infer_look_distance(col_name=outcome_col_names)
 
     # Get potential lookbehinds from pred_ columns
     pred_col_names = infer_predictor_col_name(df=train, allow_multiple=True)
-    possible_lookbehind_days = set(infer_look_distance(col_name=pred_col_names))
+    possible_lookbehind_days = list(set(infer_look_distance(col_name=pred_col_names)))
 
     return PossibleLookDistanceDays(
         ahead=possible_lookahead_days,
@@ -134,26 +135,44 @@ def get_dataset_spec(data_dir_path: Path):
 
 
 def train_models_for_each_cell_in_grid(
-    base_conf_file_name: Union[str, Path],
-    base_args: str,
-    n_trials_per_cell_in_grid: int,
-    possible_look_distances: PossibleLookDistanceDays,
+    train_conf: TrainConf,
 ):
     """Train a model for each cell in the grid of possible look distances."""
     from random_word import RandomWords
 
     random_word = RandomWords()
 
-    for lookbehind in possible_look_distances.behind:
-        for lookahead in possible_look_distances.ahead:
+    for lookbehind in train_conf.possible_look_distances.behind:
+        for lookahead in train_conf.possible_look_distances.ahead:
             wandb_group = f"{random_word.get_random_word()}-{random_word.get_random_word()}-beh-{lookbehind}-ahead-{lookahead}"
 
-            command = f"python src/psycopt2d/train_model.py {base_args} data.min_lookbehind_days={lookbehind} data.min_lookahead_days={lookahead} +project.wandb_group={wandb_group} hydra.sweeper.n_trials={n_trials_per_cell_in_grid} --config-name {base_conf_file_name}"
+            subprocess_args: list[str] = [
+                "python",
+                "src/psycopt2d/train_model.py",
+                f"+model={train_conf.model_conf}",
+                f"data.min_lookbehind_days={lookbehind}",
+                f"data.min_lookahead_days={lookahead}",
+                f"project.wandb_group='{wandb_group}'",
+                f"hydra.sweeper.n_trials={train_conf.n_trials_per_cell_in_grid}",
+                "--config-name",
+                f"{meta_conf.conf_name}",
+            ]
 
-            msg.info("Sending command")
-            msg.info(command)
+            if train_conf.multirun:
+                subprocess_args.insert(2, "--multirun")
 
-            os.system(command)
+            if train_conf.model_conf == "xgboost" and not train_conf.gpu:
+                subprocess_args.insert(3, "++model.args.tree_method='auto'")
+
+            msg.info("Starting trainer with command")
+            msg.info(f'{" ".join(subprocess_args)}')
+
+            trainer = subprocess.Popen(  # pylint: disable=consider-using-with
+                args=subprocess_args,
+            )
+
+            while trainer.poll() is None:
+                time.sleep(1)
 
 
 if __name__ == "__main__":
@@ -170,51 +189,54 @@ if __name__ == "__main__":
     wandb_conf = WandbConf(
         entity="psycop",
         project_name="psycopt2d-testing",
+        mode="offline",
     )
 
-    watcher_conf = WatcherConf(archive_all="false", keep_alive_after_training_minutes=5)
-
+    watcher_conf = WatcherConf(archive_all="true", keep_alive_after_training_minutes=5)
     dataset_spec = get_dataset_spec(data_dir_path=meta_conf.data_dir)
-
     train = load_data(dataset_spec=dataset_spec)
-    possible_look_distance = infer_possible_look_directions(train)
+
+    possible_look_distances = infer_possible_look_directions(train)
 
     train_conf = TrainConf(
         conf_name=meta_conf.conf_name,
-        base_args=f"--multirun +model=xgboost project.wandb_mode='dryrun' model.args.tree_method='auto' --config-name {meta_conf.conf_name}",
-        n_trials_per_cell_in_grid=50,
+        multirun=False,
+        model_conf="xgboost",
+        n_trials_per_cell_in_grid=1,
+        possible_look_distances=possible_look_distances,
+        gpu=False,
     )
 
-    watcher = subprocess.Popen(  # pylint: disable=consider-using-with
-        [
-            "python",
-            "src/psycopt2d/model_training_watcher.py",
-            "--entity",
-            wandb_conf.entity,
-            "--project_name",
-            wandb_conf.project_name,
-            "--n_runs_before_eval",
-            str(watcher_conf.n_runs_before_first_eval),
-            "--overtaci",
-            meta_conf.overtaci,
-            "--timeout",
-            "None",
-            "--clean_wandb_dir",
-            watcher_conf.archive_all,
-        ],
-    )
+    if not train_conf.gpu:
+        msg.warn("Not using GPU for training")
+
+    # watcher = subprocess.Popen(  # pylint: disable=consider-using-with
+    #     [
+    #         "python",
+    #         "src/psycopt2d/model_training_watcher.py",
+    #         "--entity",
+    #         wandb_conf.entity,
+    #         "--project_name",
+    #         wandb_conf.project_name,
+    #         "--n_runs_before_eval",
+    #         str(watcher_conf.n_runs_before_first_eval),
+    #         "--overtaci",
+    #         meta_conf.overtaci,
+    #         "--timeout",
+    #         "None",
+    #         "--clean_wandb_dir",
+    #         watcher_conf.archive_all,
+    #     ],
+    # )
 
     train_models_for_each_cell_in_grid(
-        base_conf_file_name=train_conf.conf_name,
-        base_args=train_conf.base_args,
-        n_trials_per_cell_in_grid=train_conf.n_trials_per_cell_in_grid,
-        possible_look_distances=train_conf.possible_look_distance,
+        train_conf=train_conf,
     )
 
     msg.good(
         f"Training finished. Stopping the watcher in {watcher_conf.keep_alive_after_training_minutes} minutes...",
     )
 
-    time.sleep(60 * watcher_conf.keep_alive_after_training_minutes)
-    watcher.kill()
+    # time.sleep(60 * watcher_conf.keep_alive_after_training_minutes)
+    # watcher.kill()
     msg.good("Watcher stopped.")
