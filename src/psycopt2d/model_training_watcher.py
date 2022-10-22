091a3c02af4150dcccb46c60753f0b7380e5f588
Lasse <lasseh0310@gmail.com>
2022-10-22 09:48:46 +0200

    feat: make watcher store separate max performance per lookbehind/lookahead combination

--- src/psycopt2d/model_training_watcher.py
+++ src/psycopt2d/model_training_watcher.py
@@ -2,11 +2,13 @@
 import argparse
 import subprocess
 import time
+from collections import defaultdict
 from distutils.util import strtobool  # pylint: disable=deprecated-module
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 
 import wandb
+from pydantic import BaseModel
 from wandb.apis.public import Api  # pylint: disable=no-name-in-module
 from wandb.sdk.wandb_run import Run  # pylint: disable=no-name-in-module
 from wasabi import msg
@@ -25,7 +27,24 @@ from psycopt2d.utils.utils import (
 WANDB_DIR = PROJECT_ROOT / "wandb"
 
 
-class ModelTrainingWatcher:
+class RunInformation(BaseModel):
+    """Information about a wandb run."""
+
+    run_id: str
+    auc: float
+    lookbehind_days: int
+    lookahead_days: int
+    lookahead_lookbehind_combined: Optional[str] = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.lookahead_lookbehind_combined is None:
+            self.lookahead_lookbehind_combined = (
+                f"lookahead:{self.lookahead_days}_lookbehind:{self.lookbehind_days}"
+            )
+
+
+class ModelTrainingWatcher:  # pylint: disable=too-many-instance-attributes
     """Watch the wandb directory for new files and uploads them to wandb. Fully
     evaluates the best runs after a certain number of runs have been uploaded.
 
@@ -57,7 +76,8 @@ class ModelTrainingWatcher:
         self.verbose = verbose
         # A queue for runs waiting to be uploaded to WandB
         self.run_id_eval_candidates_queue: list[str] = []
-        self.max_performance = 0.0
+        # max performance by lookbehind/-ahead combination
+        self.max_performances: dict[str, float] = defaultdict(lambda: 0.0)
 
         self.archive_path = WANDB_DIR / "archive"
         self.archive_path.mkdir(exist_ok=True, parents=True)
@@ -81,7 +101,11 @@ class ModelTrainingWatcher:
         self.upload_unarchived_runs()
 
         if len(self.run_id_eval_candidates_queue) >= self.n_runs_before_eval:
-            self.evaluate_and_upload_records_and_archive()
+            run_information = self._get_run_information_for_all_in_queue()
+            self.run_id_eval_candidates_queue = self._get_unfinished_runs(
+                run_information=run_information
+            )
+            self._evaluate_and_archive_finished_runs(run_information=run_information)
 
     def _upload_run_dir(self, run_dir: Path) -> str:
         """Upload a single run to wandb."""
@@ -105,20 +129,24 @@ class ModelTrainingWatcher:
         return run_dir.name.split("-")[-1]
 
     def upload_unarchived_runs(self) -> None:
-        """Upload unarchived runs to wandb."""
+        """Upload unarchived runs to wandb. Only adds runs that have finished
+        training to the evaluation queue.
+
+        Raises:
+            ValueError: If wandb sync failed
+        """
         for run_folder in WANDB_DIR.glob(r"offline-run*"):
             run_id = self._get_run_id(run_folder)
 
             wandb_sync_stdout = self._upload_run_dir(run_folder)
 
             if "...done" not in wandb_sync_stdout:
-                if ".wandb file is empty" in wandb_sync_stdout:
-                    if self.verbose:
-                        msg.warn(f"Run {run_id} is still running. Skipping.")
-                else:
+                if ".wandb file is empty" not in wandb_sync_stdout:
                     raise ValueError(
                         f"wandb sync failed, returned: {wandb_sync_stdout}",
                     )
+                if self.verbose:
+                    msg.warn(f"Run {run_id} is still running. Skipping.")
                 continue
 
             self.run_id_eval_candidates_queue.append(run_id)
@@ -161,44 +189,67 @@ class ModelTrainingWatcher:
     def _get_run_wandb_dir(self, run_id: str) -> Path:
         return list(WANDB_DIR.glob(f"*offline-run*{run_id}*"))[0]
 
-    def _get_run_performance(self, run_id: str) -> Optional[float]:
-        """Get the performance of a single run and check if it failed."""
-        run = self._get_wandb_run(run_id)
-        if "roc_auc_unweighted" in run.summary:
-            return run.summary.roc_auc_unweighted
+    def _get_run_attribute(self, run: Run, attribute: str) -> Any:
+        """Get an attribute from a wandb run."""
+        if attribute in run.summary:
+            return run.summary[attribute]
         if self.verbose:
             msg.info(
-                f"Watcher: Run {run_id} has no performance metric. Pinging again at next eval time.",
+                f"Run {run.id} has no attribute {attribute}. Pinging again at next eval time."
             )
         return None
 
-    def evaluate_and_upload_records_and_archive(self) -> None:
-        """Evaluate the best runs."""
-        run_performances = {
-            run_id: self._get_run_performance(run_id)
-            for run_id in self.run_id_eval_candidates_queue
-        }
-        # sort runs by performance to not upload subpar runs
-        run_performances = dict(
-            sorted(
-                run_performances.items(),
-                key=lambda item: (item[1] is not None, item[1]),
-                reverse=True,
-            ),
-        )
-        # get runs with auc of None (attempted upload before run finished)
-        unfinished_runs = [
-            run_id for run_id, auc in run_performances.items() if auc is None
+    def _evaluate_and_archive_finished_runs(
+        self, run_information: list[RunInformation]
+    ) -> None:
+        """Evaluate the finished runs. Test their performance against the current
+        maximum for each lookbehind/-ahead days, and fully evaluate the best performing.
+        Move all wandb run dirs to the archive folder."""
+        finished_runs = [
+            run_info for run_info in run_information if run_info.auc is not None
         ]
 
-        for run_id, performance in run_performances.items():
-            if performance is not None and performance > self.max_performance:
-                msg.good(f"New record performance! AUC: {performance}")
-                self.max_performance = performance
-                self._do_evaluation(run_id)
-            self._archive_run_dir(run_dir=self._get_run_wandb_dir(run_id))
-        # reset run id queue and try to upload unfinished runs next time
-        self.run_id_eval_candidates_queue = unfinished_runs
+        for run_info in finished_runs:
+            if (
+                run_info.auc
+                > self.max_performances[run_info.lookbehind_lookahead_combination]
+            ):
+                msg.good(
+                    f"New record performance for {run_info.lookbehind_lookahead_combination}! AUC: {run_info.auc}"
+                )
+                self.max_performances[
+                    run_info.loobehind_lookhead_combination
+                ] = run_info.auc
+                self._do_evaluation(run_info.run_id)
+            self._archive_run_dir(run_dir=self._get_run_wandb_dir(run_info.run_id))
+
+    def _get_unfinished_runs(self, run_information: list[RunInformation]) -> list[str]:
+        """Get the run ids of the unfinished runs."""
+        return [run_info.run_id for run_info in run_information if run_info.auc is None]
+
+    def _get_run_information_for_all_in_queue(self):
+        """Get the performance and information of all runs in the evaluation queue
+        and sort by lookahead/lookbehind combination and AUC for faster uploading."""
+        return [
+            self._get_run_information(run_id)
+            for run_id in self.run_id_eval_candidates_queue
+        ].sort(
+            key=lambda run_info: (
+                run_info.lookahead_lookbehind_combined,
+                run_info.auc,
+            ),
+            reverse=True,
+        )
+
+    def _get_run_information(self, run_id: str) -> RunInformation:
+        """Get the run information for a single run."""
+        run = self._get_wandb_run(run_id)
+        return RunInformation(
+            run_id=run_id,
+            auc=self._get_run_attribute(run, "roc_auc_unweighted"),
+            lookbehind_days=self._get_run_attribute(run, "lookbehind_days"),
+            lookahead_days=self._get_run_attribute(run, "lookahead_days"),
+        )
 
     def archive_all_runs(self) -> None:
         """Archive all runs in the wandb directory."""
