b3c73f70bc6224da508f5f0876721b52f68f1035
bokajgd <bokajgd@gmail.com>
2022-10-25 13:23:53 +0200

    adding test

--- src/psycopt2d/visualization/performance_over_n_hba1c.py
+++ src/psycopt2d/visualization/performance_over_n_hba1c.py
@@ -6,9 +6,8 @@ from typing import Optional, Union
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.metrics import roc_auc_score
 
-from psycopt2d.utils import bin_continuous_data, round_floats_to_edge
 from psycopt2d.visualization.utils import calc_performance
 from psycopt2d.visualization.base_charts import plot_basic_chart
 
