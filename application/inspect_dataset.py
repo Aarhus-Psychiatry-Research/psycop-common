0a6a0e4b40f7227e9a7a79cc78f51d9d81dd14b4
Martin Bernstorff <martinbernstorff@gmail.com>
2022-11-01 10:46:58 +0100

    docs: pylint

--- application/inspect_dataset.py
+++ application/inspect_dataset.py
@@ -1,9 +1,10 @@
-"""Example of how to inspect a dataset using the configs"""
+"""Example of how to inspect a dataset using the configs."""
 from psycopt2d.load import load_train_from_cfg
 from psycopt2d.utils.config_schemas import load_cfg_as_pydantic
 
 
 def main():
+    """Main."""
     config_file_name = "default_config.yaml"
 
     cfg = load_cfg_as_pydantic(config_file_name=config_file_name)
