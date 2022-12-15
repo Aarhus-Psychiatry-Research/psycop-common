aa98eefd458fc60bf27204299a1247a2bc718967
Martin Bernstorff <ryqiem@gmail.com>
2022-12-15 11:51:31 +0100

    style: linting

--- src/psycop_feature_generation/loaders/raw/load_moves.py
+++ src/psycop_feature_generation/loaders/raw/load_moves.py
@@ -6,8 +6,9 @@ from psycop_feature_generation.loaders.raw.sql_load import sql_load
 
 
 def load_moves(n_rows: Optional[int] = None) -> pd.DataFrame:
-    """Get a dataframe with timestamps for each move into or out from the Central Denmark Region."""
-    view = f"[bopael_i_rm]"
+    """Get a dataframe with timestamps for each move into or out from the
+    Central Denmark Region."""
+    view = "[bopael_i_rm]"
 
     sql = f"SELECT * FROM [fct].{view}"
 
@@ -17,7 +18,8 @@ def load_moves(n_rows: Optional[int] = None) -> pd.DataFrame:
 
 
 def load_move_into_rm_for_exclusion(n_rows: Optional[int] = None) -> pd.DataFrame:
-    """Get a dataframe with timestamps for each move into or out from the Central Denmark Region."""
+    """Get a dataframe with timestamps for each move into or out from the
+    Central Denmark Region."""
     df = load_moves()
 
     df = df.drop(columns=["fraflytnings_datotid"])
