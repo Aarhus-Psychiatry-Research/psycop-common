eee7768ca74fce9b933c00ee778dba938c1680b5
Martin Bernstorff <ryqiem@gmail.com>
2022-12-19 14:15:07 +0100

    fix: rename preprocessing keys

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -12,15 +12,29 @@ from psycop_model_training.utils.utils import PROJECT_ROOT
 CONFIG_DIR_PATH_ABS = PROJECT_ROOT / "tests" / "config"
 CONFIG_DIR_PATH_REL = "../tests/config"
 
+CONFIG_DIR_PATH_APP_ABS = PROJECT_ROOT / "application" / "config"
+
 
 def get_config_file_names() -> list[str]:
     """Get all config file names."""
-    config_file_paths: list[Path] = list(CONFIG_DIR_PATH_ABS.glob("*.yaml"))
+    config_file_paths = list(CONFIG_DIR_PATH_ABS.glob("*.yaml"))
+
     return [f"{path.stem}.yaml" for path in config_file_paths]
 
 
 @pytest.mark.parametrize("config_file_name", get_config_file_names())
-def test_configs(config_file_name):
+def test_test_configs(config_file_name):
+    """Test that all configs load correctly."""
+    with initialize(version_base=None, config_path=CONFIG_DIR_PATH_REL):
+        cfg = compose(
+            config_name=config_file_name,
+        )
+
+    cfg = convert_omegaconf_to_pydantic_object(conf=cfg)
+
+
+@pytest.mark.parametrize("config_file_name", get_config_file_names())
+def test_app_configs(config_file_name):
     """Test that all configs load correctly."""
     with initialize(version_base=None, config_path=CONFIG_DIR_PATH_REL):
         cfg = compose(
