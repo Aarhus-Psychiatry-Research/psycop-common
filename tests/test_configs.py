6b37fffa4b3cd2edcdbdd513c24ae71f8d514268
Martin Bernstorff <martinbernstorff@gmail.com>
2022-12-22 15:08:33 +0100

    tests: test application configs

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -8,23 +8,24 @@ from psycop_model_training.utils.config_schemas.conf_utils import (
 )
 from psycop_model_training.utils.utils import PROJECT_ROOT
 
-CONFIG_DIR_PATH_ABS = PROJECT_ROOT / "tests" / "config"
-CONFIG_DIR_PATH_REL = "../tests/config"
+CONFIG_DIR_PATH_TEST_ABS = PROJECT_ROOT / "tests" / "config"
+CONFIG_DIR_PATH_TEST_REL = "../tests/config"
 
 CONFIG_DIR_PATH_APP_ABS = PROJECT_ROOT / "application" / "config"
+CONFIG_DIR_PATH_APP_REL = "../application/config"
 
 
-def get_config_file_names() -> list[str]:
+def get_test_config_file_names() -> list[str]:
     """Get all config file names."""
-    config_file_paths = list(CONFIG_DIR_PATH_ABS.glob("*.yaml"))
+    config_file_paths = list(CONFIG_DIR_PATH_TEST_ABS.glob("*.yaml"))
 
     return [f"{path.stem}.yaml" for path in config_file_paths]
 
 
-@pytest.mark.parametrize("config_file_name", get_config_file_names())
+@pytest.mark.parametrize("config_file_name", get_test_config_file_names())
 def test_test_configs(config_file_name):
     """Test that all configs load correctly."""
-    with initialize(version_base=None, config_path=CONFIG_DIR_PATH_REL):
+    with initialize(version_base=None, config_path=CONFIG_DIR_PATH_TEST_REL):
         cfg = compose(
             config_name=config_file_name,
         )
@@ -32,10 +33,17 @@ def test_test_configs(config_file_name):
     cfg = convert_omegaconf_to_pydantic_object(conf=cfg)
 
 
-@pytest.mark.parametrize("config_file_name", get_config_file_names())
+def get_app_config_file_names() -> list[str]:
+    """Get all config file names."""
+    config_file_paths = list(CONFIG_DIR_PATH_TEST_ABS.glob("*.yaml"))
+
+    return [f"{path.stem}.yaml" for path in config_file_paths]
+
+
+@pytest.mark.parametrize("config_file_name", get_app_config_file_names())
 def test_app_configs(config_file_name):
     """Test that all configs load correctly."""
-    with initialize(version_base=None, config_path=CONFIG_DIR_PATH_REL):
+    with initialize(version_base=None, config_path=CONFIG_DIR_PATH_APP_REL):
         cfg = compose(
             config_name=config_file_name,
         )
