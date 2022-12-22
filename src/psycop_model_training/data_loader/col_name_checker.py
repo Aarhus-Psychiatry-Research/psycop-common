ae6a6c2ee04fe8596555648cf8aee537ea4bd3c5
Martin Bernstorff <ryqiem@gmail.com>
2022-12-22 10:55:06 +0100

    feat: add suggested cols if missing using Levenshtein edit distance

--- src/psycop_model_training/data_loader/col_name_checker.py
+++ src/psycop_model_training/data_loader/col_name_checker.py
@@ -1,4 +1,7 @@
 """Check that all columns in the config exist in the dataset."""
+from typing import List
+
+import Levenshtein
 import pandas as pd
 
 from psycop_model_training.utils.config_schemas.data import ColumnNamesSchema
@@ -9,22 +12,63 @@ def check_columns_exist_in_dataset(
 ):
     """Check that all columns in the config exist in the dataset."""
     # Iterate over attributes in the config
-    missing_columns = []
+    error_strs = []
 
     for attr in dir(col_name_schema):
         # Skip private attributes
         if attr.startswith("_"):
             continue
 
+        col_name = getattr(col_name_schema, attr)
+
         # Skip col names that are not string
-        if not isinstance(getattr(col_name_schema, attr), str):
+        if not isinstance(col_name, str):
             continue
 
         # Check that the column exists in the dataset
-        if not getattr(col_name_schema, attr) in df:
-            missing_columns.append(getattr(col_name_schema, attr))
+        if not col_name in df:
+            most_likely_alternatives = get_most_likely_str_from_edit_distance(
+                candidate_strs=df.columns,
+                input_str=col_name,
+                n_str_to_return=3,
+            )
 
-    if missing_columns:
-        raise ValueError(
-            f"Columns in config but not in dataset: {missing_columns}. Columns in dataset: {df.columns}"
-        )
+            error_str = f"Column '{col_name}' in config but not in dataset.\n"
+            error_str += f"    Did you mean {most_likely_alternatives}? \n"
+            error_strs.append(error_str)
+
+    if error_strs:
+        raise ValueError("\n".join(error_strs))
+
+
+def get_most_likely_str_from_edit_distance(
+    candidate_strs: list[str],
+    input_str: str,
+    n_str_to_return: int,
+    edit_distance_threshold: int = 15,
+) -> List[str]:
+    """Get most likely string from edit distance.
+
+    Args:
+        candidate_strs (list[str]): List of candidate strings.
+        input_str (str): The incorrect string.
+        n_str_to_return (int): Number of strings to return.
+        edit_distance_threshold (int, optional): Maximum edit distance to consider. Defaults to 5.
+
+    Returns:
+        str: String from candidate_strs that is most similar to input_str by edit distance.
+    """
+    # Compute the Levenshtein distance between the input string and each candidate string.
+    distances = [
+        Levenshtein.distance(input_str, candidate) for candidate in candidate_strs
+    ]
+
+    # Sort the candidate strings by their Levenshtein distance from the input string.
+    sorted_candidates = [
+        x
+        for distance, x in sorted(zip(distances, candidate_strs))
+        if distance <= edit_distance_threshold
+    ]
+
+    # Return the first `n_str_to_return` elements of the sorted list of candidate strings.
+    return sorted_candidates[:n_str_to_return]
