e3928eb904cf84f6f5243d04dfe2e8056b190d72
Martin Bernstorff <martinbernstorff@gmail.com>
2022-12-22 15:05:52 +0100

    fix: missing imports

--- tests/model_evaluation/test_visualizations.py
+++ tests/model_evaluation/test_visualizations.py
@@ -10,22 +10,32 @@ import pandas as pd
 import pytest
 from sklearn.metrics import f1_score, roc_auc_score
 
-from psycop_model_training.model_eval.base_artifacts.plots import (
-    create_sensitivity_by_time_to_outcome_df,
-    plot_auc_by_time_from_first_visit,
-    plot_basic_chart,
-    plot_feature_importances,
-    plot_metric_by_calendar_time,
-    plot_metric_by_cyclic_time,
-    plot_metric_by_time_until_diagnosis,
+from application.artifacts.plots.performance_by_n_hba1c import (
     plot_performance_by_n_hba1c,
-    plot_prob_over_time,
-    plot_sensitivity_by_time_to_outcome_heatmap,
+)
+from psycop_model_training.model_eval.base_artifacts.plots.base_charts import (
+    plot_basic_chart,
+)
+from psycop_model_training.model_eval.base_artifacts.plots.feature_importance import (
+    plot_feature_importances,
 )
 from psycop_model_training.model_eval.base_artifacts.plots.performance_by_age import (
     plot_performance_by_age,
 )
+from psycop_model_training.model_eval.base_artifacts.plots.performance_over_time import (
+    plot_auc_by_time_from_first_visit,
+    plot_metric_by_calendar_time,
+    plot_metric_by_cyclic_time,
+    plot_metric_by_time_until_diagnosis,
+)
+from psycop_model_training.model_eval.base_artifacts.plots.prob_over_time import (
+    plot_prob_over_time,
+)
 from psycop_model_training.model_eval.base_artifacts.plots.roc_auc import plot_auc_roc
+from psycop_model_training.model_eval.base_artifacts.plots.sens_over_time import (
+    create_sensitivity_by_time_to_outcome_df,
+    plot_sensitivity_by_time_to_outcome_heatmap,
+)
 from psycop_model_training.model_eval.dataclasses import EvalDataset
 from psycop_model_training.utils.utils import PROJECT_ROOT, positive_rate_to_pred_probs
 
@@ -180,4 +190,15 @@ def test_plot_feature_importances():
 
 
 def test_plot_roc_auc(synth_eval_dataset: EvalDataset):
+    plot_auc_roc(eval_dataset=synth_eval_dataset)
+
+    plot_feature_importances(
+        feature_importance_dict=feature_importance_dict,
+        top_n_feature_importances=n_features,
+        save_path="tmp",
+    )
+
+
+def test_plot_roc_auc(synth_eval_dataset: EvalDataset):
+    plot_auc_roc(eval_dataset=synth_eval_dataset)
     plot_auc_roc(eval_dataset=synth_eval_dataset)
