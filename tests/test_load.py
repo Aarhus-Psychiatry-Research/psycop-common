25e18232cc1de4187b1e830c08386e1ddf9e0a24
Martin Bernstorff <martinbernstorff@gmail.com>
2022-12-23 10:42:02 +0100

    style: linting

--- tests/test_load.py
+++ tests/test_load.py
@@ -15,8 +15,7 @@ def test_load_lookbehind_exceeds_lookbehind_threshold(
     muteable_test_config: FullConfigSchema,
 ):
     """Test that columns are dropped if their lookbehind are larger than the
-    lookbehind threshold.
-    """
+    lookbehind threshold."""
     cfg = muteable_test_config
 
     n_cols_before_filtering = load_and_filter_train_from_cfg(cfg=cfg).shape[1]
@@ -32,8 +31,7 @@ def test_load_lookbehind_not_in_lookbehind_combination(
     muteable_test_config: FullConfigSchema,
 ):
     """Test that columns are dropped if their lookbehind is not in the
-    specified lookbehind combination list.
-    """
+    specified lookbehind combination list."""
     cfg = muteable_test_config
 
     n_cols_before_filtering = load_and_filter_train_from_cfg(cfg=cfg).shape[1]
@@ -46,9 +44,8 @@ def test_load_lookbehind_not_in_lookbehind_combination(
 
 
 def test_check_columns_exist_in_dataset():
-    """Test that the check_columns_exist_in_dataset function raises an
-    error if a column is missing from the dataframe.
-    """
+    """Test that the check_columns_exist_in_dataset function raises an error if
+    a column is missing from the dataframe."""
 
     test_schema = ColumnNamesSchema(
         pred_timestamp="pred_timestamp",
