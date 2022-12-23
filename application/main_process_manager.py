3e0744099eb9222d9503eb0413f5579c36cd18bb
Martin Bernstorff <ryqiem@gmail.com>
2022-12-23 11:06:21 +0100

    fix: init wandb before adding wandb decorator

--- application/main_process_manager.py
+++ application/main_process_manager.py
@@ -13,16 +13,16 @@ from psycop_model_training.application_modules.get_search_space import (
 )
 from psycop_model_training.application_modules.process_manager_setup import setup
 from psycop_model_training.application_modules.trainer_spawner import spawn_trainers
+from psycop_model_training.config_schemas.full_config import FullConfigSchema
 from psycop_model_training.data_loader.data_loader import DataLoader
 
 
 @wandb_alert_on_exception
-def main():
+def main(
+    cfg: FullConfigSchema,
+    wandb_group: str,
+):
     """Main."""
-    config_file_name = "default_config.yaml"
-
-    cfg, wandb_group = setup(config_file_name=config_file_name)
-
     # Load dataset without dropping any rows for inferring
     # which look distances to grid search over
     train_df = DataLoader(cfg=cfg).load_dataset_from_dir(split_names="train")
@@ -35,11 +35,17 @@ def main():
 
     spawn_trainers(
         cfg=cfg,
-        config_file_name=config_file_name,
+        config_file_name=CONFIG_FILE_NAME,
         wandb_prefix=wandb_group,
         trainer_specs=trainer_specs,
     )
 
 
 if __name__ == "__main__":
-    main()
+    CONFIG_FILE_NAME = "default_config.yaml"
+
+    # Must run cfg before main to ensure that wandb is initialized
+    # before adding wandb_alert_on_exception decorator
+    cfg, wandb_group = setup(config_file_name=CONFIG_FILE_NAME)
+
+    main(cfg=cfg, wandb_group=wandb_group)
