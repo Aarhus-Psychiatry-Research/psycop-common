3b15c9b83252d490939f8eaf90909473773a82f9
Martin Bernstorff <martinbernstorff@gmail.com>
2023-02-06 10:29:51 +0100

    style: linting

--- tests/application_modules/test_wandb_handler.py
+++ tests/application_modules/test_wandb_handler.py
@@ -12,7 +12,7 @@ def test_wandb_handler_fullconfig_parsing(
     """Test that the wandb handler can parse a fullconfig object to a flattened
     dict, ready for upload to wandb."""
     cfg_parsed = WandbHandler(
-        cfg=muteable_test_config
+        cfg=muteable_test_config,
     )._get_cfg_as_dict()  # pylint: disable=protected-access
 
     for k, v in cfg_parsed.items():
