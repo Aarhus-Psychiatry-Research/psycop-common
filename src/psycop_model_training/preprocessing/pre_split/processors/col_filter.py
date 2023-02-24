c5b730939612aef5de1760bf45d7b2773c45cadd
Martin Bernstorff <martinbernstorff@gmail.com>
2023-02-24 11:36:57 +0100

    fix: incorrect guard for inferring col name

--- src/psycop_model_training/preprocessing/pre_split/processors/col_filter.py
+++ src/psycop_model_training/preprocessing/pre_split/processors/col_filter.py
@@ -163,8 +163,8 @@ class PresSplitColFilter:
         df = dataset.drop(col_to_drop, axis=1)
 
         if (
-            self.cfg.debug is not None
-            and self.cfg.debug.assert_outcome_col_matching_lookahead_exists
+            self.cfg.debug is None
+            or self.cfg.debug.assert_outcome_col_matching_lookahead_exists
         ):
             if not len(infer_outcome_col_name(df)) == 1:
                 raise ValueError(
