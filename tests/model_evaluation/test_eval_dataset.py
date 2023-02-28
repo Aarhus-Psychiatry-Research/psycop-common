188a73ca190efca791e11271ed2bfa78823d704b
Martin Bernstorff <martinbernstorff@gmail.com>
2023-02-28 14:30:04 +0100

    refactor: remove the to_df method

--- tests/model_evaluation/test_eval_dataset.py
+++
@@ -1,7 +0,0 @@
-from psycop_model_training.model_eval.dataclasses import EvalDataset
-
-
-def test_plot_time_from_first_positive_to_event(synth_eval_dataset: EvalDataset):
-    """Test that the plot_time_from_first_positive_to_event function runs
-    without error."""
-    synth_eval_dataset.to_df()
