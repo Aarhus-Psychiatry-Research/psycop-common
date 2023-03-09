602f4f388c4015330f5c789510e399fc4a2cd878
Martin Bernstorff <ryqiem@gmail.com>
2023-03-09 16:18:28 +0100

    feat: make sql loader verbose

--- src/psycop_feature_generation/loaders/raw/sql_load.py
+++ src/psycop_feature_generation/loaders/raw/sql_load.py
@@ -12,7 +12,7 @@ from pathlib import Path
 
 # Create a memory cache with the desired directory to store cached results
 cache_dir = Path("E:/shared_resources/sql_cache/")
-memory = Memory(location=cache_dir, verbose=0)
+memory = Memory(location=cache_dir, verbose=1)
 
 
 @memory.cache
