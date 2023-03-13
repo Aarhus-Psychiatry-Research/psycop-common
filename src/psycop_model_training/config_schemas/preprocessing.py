3a5bbbed56bf6f1580abad6aa8e59d667af1d691
Martin Bernstorff <ryqiem@gmail.com>
2023-03-13 13:32:10 +0100

    fix: min_lookahead_days is not optional, required to select outcome column

--- src/psycop_model_training/config_schemas/preprocessing.py
+++ src/psycop_model_training/config_schemas/preprocessing.py
@@ -38,7 +38,7 @@ class PreSplitPreprocessingConfigSchema(BaseModel):
     # Whether to convert datetimes to ordinal.
 
     # Looking ahead
-    min_lookahead_days: Optional[int]
+    min_lookahead_days: float
     # Drop all prediction times where (max timestamp in the dataset) - (current timestamp) is less than min_lookahead_days
 
     min_age: Optional[float]  # Minimum age to include in the dataset
