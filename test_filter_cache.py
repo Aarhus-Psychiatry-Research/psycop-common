8cf3ef334fed42ba8e7b2e92fc78cc015e6d3baa
Martin Bernstorff <martinbernstorff@gmail.com>
2023-03-13 12:37:55 +0100

    feat: add example of caching filtered datasets

---
+++ test_filter_cache.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from psycop_model_training.config_schemas.data import ColumnNamesSchema, DataSchema
+from psycop_model_training.config_schemas.preprocessing import (
+    PreSplitPreprocessingConfigSchema,
+)
+from psycop_model_training.data_loader.data_loader import DataLoader
+from psycop_model_training.data_loader.utils import load_and_filter_train_from_cfg
+from psycop_model_training.preprocessing.pre_split.full_processor import (
+    process_full_dataset,
+)
+
+if __name__ == "__main__":
+    dataset_path = Path(
+        "E:/shared_resources/t2d/feature_sets/psycop_t2d_adminmanber_features_2023_03_09_18_37",
+    )
+
+    data_cfg = DataSchema(dir=dataset_path)
+    dataset = DataLoader(data_cfg=data_cfg).load_dataset_from_dir(split_names=["train"])
+
+    processed_dataset = process_full_dataset(
+        dataset=dataset,
+        pre_split_cfg=PreSplitPreprocessingConfigSchema(),
+    )
