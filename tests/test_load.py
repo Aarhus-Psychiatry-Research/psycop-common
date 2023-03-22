651b6f35909380707582a900b1e1953a7ab769b6
Martin Bernstorff <martinbernstorff@gmail.com>
2023-03-22 11:00:42 +0100

    ci: test test failure

--- tests/test_load.py
+++ tests/test_load.py
@@ -37,10 +37,10 @@ def test_load_lookbehind_not_in_lookbehind_combination(
     n_cols_before_filtering = load_and_filter_train_from_cfg(cfg=cfg).shape[1]
 
     cfg.preprocessing.pre_split.lookbehind_combination = [60]
-
     n_cols_after_filtering = load_and_filter_train_from_cfg(cfg=cfg).shape[1]
 
     assert n_cols_before_filtering - n_cols_after_filtering == 3
+    raise ValueError("Testing test failure")
 
 
 def test_check_columns_exist_in_dataset():
