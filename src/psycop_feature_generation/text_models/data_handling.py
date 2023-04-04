0740571d3fd292695b7c63f83052f26ea9f8c901
erikperfalk <erik.perfalk@gmail.com>
2023-04-04 10:24:01 +0200

    mypy changes

--- src/psycop_feature_generation/text_models/data_handling.py
+++ src/psycop_feature_generation/text_models/data_handling.py
@@ -2,23 +2,23 @@ from typing import Literal
 
 import pandas as pd
 
-from src.psycop_feature_generation.application_modules.save_dataset_to_disk import (
+from psycop_feature_generation.application_modules.save_dataset_to_disk import (
     filter_by_split_ids,
     get_split_id_df,
 )
-from src.psycop_feature_generation.loaders.raw.load_text import load_text_sfis
+from psycop_feature_generation.loaders.raw.load_text import load_text_sfis
 
 
 def load_text_split(
     text_sfi_names=str,
     split_name=Literal["train", "val"],
 ) -> pd.DataFrame:
-    """Load specified text sfi and only keeps data from the specified split"""
+    """Loads specified text sfi and only keeps data from the specified split"""
 
     text_df = load_text_sfis(text_sfi_names=text_sfi_names)
 
     split_id_df = get_split_id_df(split_name=split_name)
 
-    text_split_df = filter_by_split_ids(df_to_split=text_df, splid_id_df=split_id_df)
+    text_split_df = filter_by_split_ids(df_to_split=text_df, split_id_df=split_id_df)
 
     return text_split_df
