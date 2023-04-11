4b144af7f0d67807fec77ad9d2e92018eeebf5d9
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-11 10:38:44 +0200

    init: add integration test

--- tests/test_descriptive_stats_table.py
+++ tests/test_descriptive_stats_table.py
@@ -10,42 +10,40 @@ from psycop_model_training.training_output.dataclasses import EvalDataset
 
 
 def test_generate_descriptive_stats_table(synth_eval_dataset: EvalDataset):
-    """Test that table is generated correctly."""
+    """Test descriptive stats table."""
 
-    col_name = "pred_f_20_max"
-
-    additional_columns_df = pd.DataFrame(
-        {
-            "dw_ek_borger": synth_eval_dataset.ids,
-            col_name: [randint(0, 1) for _ in range(len(synth_eval_dataset.y))],
-        },
-    )
-
-    table1 = DescriptiveStatsTable(
-        eval_dataset=synth_eval_dataset,
-        additional_columns_df=additional_columns_df,
-    )
-
-    output_table: pd.DataFrame = table1.generate_descriptive_stats_table(  # type: ignore
-        output_format="df",
-    )
-
-    # Assert that there are no NaN values in the table
-    assert not output_table.isnull().values.any()
-
-    # Assert that all generic statistics are present
-    assert (
-        output_table["category"]
-        .isin(
-            [
-                "(visit_level) age (mean / interval)",
-                "(visit_level) visits followed by positive outcome",
-                "(patient_level) patients_with_positive_outcome",
-                "(patient level) time_to_first_positive_outcome",
+    variable_group_specs = [
+        VariableGroup(
+            group_title="Patients",
+            group_column_str="dw_ek_borger",
+            add_total_row=True,
+            row_specs=[
+                RowSpec( # The binary case
+                    row_title="Female",
+                    row_column_str="is_female",
+                ),
+                RowSpec( # The categorical case
+                    row_title="Female",
+                    row_column_str="is_female",
+                    categories = [0, 1],
+                )
+                Rowspec( # The continuous case
+                    row_title="citizen_id",
+                    row_column_str="dw_ek_borger",
+                )
             ],
         )
-        .any()
+    ]
+
+    datasets = [
+        DatasetSpec(dataset_name="Train", dataset=train_df),
+        DatasetSpec(dataset_name="Test", dataset=test_df),
+    ]
+
+    descriptive_table = DescriptiveStatsTable(
+        variable_group_specs=variable_group_specs, datasets=datasets
     )
 
-    # Assert that all additional columns are present
-    assert f"(visit level) {col_name}" in output_table["category"].values
+    print(descriptive_table)
+
+    pass
