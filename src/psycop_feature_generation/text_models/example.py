c38176857e0dd294b618763b14d69d4e3d05d35d
signekb <signebroedbaek@gmail.com>
2023-04-12 13:24:40 +0200

    fix: renaming in preprocessing

--- src/psycop_feature_generation/text_models/example.py
+++ src/psycop_feature_generation/text_models/example.py
@@ -1,8 +1,9 @@
-from psycop_feature_generation.text_models.preprocessing import tfidf_preprocessing
+from psycop_feature_generation.text_models.preprocessing import text_preprocessing
 
 if __name__ == "__main__":
-    df = tfidf_preprocessing(
+    df = text_preprocessing(
         text_sfi_names="Aktuelt psykisk",
+        include_sfi_name=True,
         n_rows=10,
         split_name=["train", "val"],
     )
--- src/psycop_feature_generation/text_models/preprocessing.py
+++ src/psycop_feature_generation/text_models/preprocessing.py
@@ -13,9 +13,9 @@ def convert_series_to_lower_case(text_series: pd.Series) -> pd.Series:
 
 def remove_symbols_from_series(text_series=pd.Series) -> pd.Series:
     res = []
-    for word in text_series:
-        text_series = re.sub("[^ÆØÅæøåA-Za-z0-9]+", " ", word)
-        res.append(text_series)
+    for text in text_series:
+        text = re.sub("[^ÆØÅæøåA-Za-z0-9 ]+", "", text)
+        res.append(text)
 
     return pd.Series(res)
 
@@ -23,25 +23,31 @@ def remove_symbols_from_series(text_series=pd.Series) -> pd.Series:
 def remove_stop_words_from_series(text_series: pd.Series) -> pd.Series:
     regex_stop_words = re.compile(r"\b%s\b" % r"\b|\b".join(map(re.escape, stop_words)))
     res = []
-    for word in text_series:
-        text_series = re.sub(regex_stop_words, "", str(word))
-        res.append(text_series)
+    for text in text_series:
+        text = re.sub(regex_stop_words, "", text)
+        text = re.sub(" +", " ", text)
+        res.append(text)
+
     return pd.Series(res)
 
 
 ### preprocessing for specific models for text
-def tfidf_preprocessing(
+def text_preprocessing(
     text_sfi_names=str,
+    include_sfi_name: bool = False,
     n_rows: int = None,
     split_name=Literal["train", "val"],
 ) -> pd.DataFrame:
-    df_to_preprocess = load_text_split(
-        text_sfi_names=text_sfi_names, n_rows=n_rows, split_name=split_name
+    df = load_text_split(
+        text_sfi_names=text_sfi_names,
+        include_sfi_name=include_sfi_name,
+        n_rows=n_rows,
+        split_name=split_name,
     )
 
-    df_to_preprocess["text"] = convert_series_to_lower_case(df_to_preprocess["text"])
-    df_to_preprocess["text"] = remove_symbols_from_series(df_to_preprocess["text"])
-    df_to_preprocess["text"] = remove_stop_words_from_series(df_to_preprocess["text"])
-    preprocessed_df = df_to_preprocess
 
-    return preprocessed_df
+    df["text"] = convert_series_to_lower_case(df["text"])
+    df["text"] = remove_symbols_from_series(df["text"])
+    df["text"] = remove_stop_words_from_series(df["text"])
+
+    return df
