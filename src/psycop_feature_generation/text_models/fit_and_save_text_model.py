8ec1ceec5ca966375f11facb04692974f3919f16
signekb <signebroedbaek@gmail.com>
2023-04-12 17:05:56 +0200

    create initial script for fitting and saving a text model

---
+++ src/psycop_feature_generation/text_models/fit_and_save_text_model.py
@@ -0,0 +1,12 @@
+"""Fit and save a model"""
+from text_models.text_models_pipeline import (
+    bow_model_pipeline,
+    tfidf_model_pipeline,
+    lda_model_pipeline,
+)
+
+if __name__ == "__main__":
+    tfidf_model_pipeline(
+        view="psycop_train_val_all_sfis_all_years_lowercase_stopwords_and_symbols_removed",
+        # sfi_type=["Aktuelt psykisk"],
+    )
