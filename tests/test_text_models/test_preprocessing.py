0294d47aaa54e369c68019a1c6146ce5d071de38
sarakolding <sarakolding@live.dk>
2023-04-12 13:31:05 +0200

    fix preproc testsø

--- tests/test_text_models/test_preprocessing.py
+++ tests/test_text_models/test_preprocessing.py
@@ -9,7 +9,7 @@ def test_remove_symbols_from_series():
     text_series = pd.Series(["Jeg er paf!", "jeg hedder, erik og er 97 år?"])
     text_series = remove_symbols_from_series(text_series=text_series)
 
-    expected = pd.Series(["Jeg er paf ", "jeg hedder erik og er 97 år "])
+    expected = pd.Series(["Jeg er paf", "jeg hedder erik og er 97 år"])
 
     assert pd.testing.assert_series_equal(text_series, expected) is None
 
@@ -18,6 +18,6 @@ def test_remove_stop_words_from_series():
     text_series = pd.Series(["jeg er paf", "jeg hedder erik"])
     text_series = remove_stop_words_from_series(text_series=text_series)
 
-    expected = pd.Series(["  paf", " hedder erik"])
+    expected = pd.Series([" paf", " hedder erik"])
 
     assert pd.testing.assert_series_equal(text_series, expected) is None
