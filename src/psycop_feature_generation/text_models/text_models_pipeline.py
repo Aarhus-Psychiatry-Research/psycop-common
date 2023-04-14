22b6a9e317436859ff2fad9f149345a9b19d673e
signekb <signebroedbaek@gmail.com>
2023-04-14 16:14:31 +0200

    fix: general clean-up and change corpus in fit functions to list

--- src/psycop_feature_generation/text_models/text_models_pipeline.py
+++ src/psycop_feature_generation/text_models/text_models_pipeline.py
@@ -17,15 +17,30 @@ log.setLevel(logging.INFO)
 
 
 def bow_model_pipeline(
-    view: str = None,
+    view: str = "psycop_train_val_all_sfis_all_years_lowercase_stopwords_and_symbols_removed",
     sfi_type: Iterable[str] = ["All_sfis"],
     n_rows: int = None,
-    ngram_range: tuple = (1, 1),
+    ngram_range: tuple = (1, 2),
     max_df: float = 0.95,
     min_df: int = 2,
     max_features: int = 500,
     save_path: str = "E:/shared_resources/text_models",
 ) -> str:
+    """Pipeline for fitting and saving a bag-of-words model
+
+    Args:
+        view (str, optional): SQL table with text data to fit model on. Defaults to "psycop_train_val_all_sfis_all_years_lowercase_stopwords_and_symbols_removed".
+        sfi_type (Iterable[str], optional): Which sfi types to include. Defaults to ["All_sfis"].
+        n_rows (int, optional): How many rows to include in the loaded data. If None, all are included. Defaults to None.
+        ngram_range (tuple, optional): The lower and upper boundary of the range of n-values for different word n-grams or char n-grams to be extracted. All values of n such such that min_n <= n <= max_n will be used. For example an ngram_range of (1, 1) means only unigrams, (1, 2) means unigrams and bigrams. Defaults to (1, 2).
+        max_df (float, optional): The proportion of documents the words should appear in to be included. Defaults to 0.95.
+        min_df (int, optional): Remove words occuring in less than min_df documents. Defaults to 2.
+        max_features (int | None, optional): If not None, build a vocabulary that only consider the top max_features ordered by term frequency across the corpus. Otherwise, all features are used. Defaults to 500.
+        save_path (str, optional): Path where the model will be saved. Defaults to "E:/shared_resources/text_models".
+
+    Returns:
+        str: Log info on the path and filename of the fitted bow model.
+    """
     # create model filename from params
     max_df_str = str(max_df).replace(".", "")
     ngram_range_str = "".join(c for c in str(ngram_range) if c.isdigit())
@@ -60,7 +75,7 @@ def bow_model_pipeline(
 
     # fit model
     bow = fit_bow(
-        corpus["text"],
+        corpus=corpus["text"].tolist(),
         ngram_range=ngram_range,
         max_df=max_df,
         min_df=min_df,
@@ -78,15 +93,31 @@ def bow_model_pipeline(
 
 
 def tfidf_model_pipeline(
-    view: str = None,
+    view: str = "psycop_train_val_all_sfis_all_years_lowercase_stopwords_and_symbols_removed",
     sfi_type: Iterable[str] = ["All_sfis"],
     n_rows: int = None,
-    ngram_range: tuple = (1, 1),
+    ngram_range: tuple = (1, 2),
     max_df: float = 0.95,
     min_df: int = 2,
     max_features: int = 500,
     save_path: str = "E:/shared_resources/text_models",
 ) -> str:
+    """Pipeline for fitting and saving a term frequency-inverse document frequency (tf-idf) model
+
+    Args:
+        view (str, optional): SQL table with text data to fit model on. Defaults to "psycop_train_val_all_sfis_all_years_lowercase_stopwords_and_symbols_removed".
+        sfi_type (Iterable[str], optional): Which sfi types to include. Defaults to ["All_sfis"].
+        n_rows (int, optional): How many rows to include in the loaded data. If None, all are included. Defaults to None.
+        ngram_range (tuple, optional): The lower and upper boundary of the range of n-values for different word n-grams or char n-grams to be extracted. All values of n such such that min_n <= n <= max_n will be used. For example an ngram_range of (1, 1) means only unigrams, (1, 2) means unigrams and bigrams. Defaults to (1, 2).
+        max_df (float, optional): The proportion of documents the words should appear in to be included. Defaults to 0.95.
+        min_df (int, optional): Remove words occuring in less than min_df documents. Defaults to 2.
+        max_features (int | None, optional): If not None, build a vocabulary that only consider the top max_features ordered by term frequency across the corpus. Otherwise, all features are used. Defaults to 500.
+        save_path (str, optional): Path where the model will be saved. Defaults to "E:/shared_resources/text_models".
+
+    Returns:
+        str: Log info on the path and filename of the fitted tf-idf model.
+    """
+
     # create model filename from params
     max_df_str = str(max_df).replace(".", "")
     ngram_range_str = "".join(c for c in str(ngram_range) if c.isdigit())
@@ -121,7 +152,7 @@ def tfidf_model_pipeline(
 
     # fit model
     tfidf = fit_tfidf(
-        corpus["text"],
+        corpus=corpus["text"].tolist(),
         ngram_range=ngram_range,
         max_df=max_df,
         min_df=min_df,
@@ -139,17 +170,34 @@ def tfidf_model_pipeline(
 
 
 def lda_model_pipeline(
-    view: str = None,
+    view: str = "psycop_train_val_all_sfis_all_years_lowercase_stopwords_and_symbols_removed",
     sfi_type: Iterable[str] = ["All_sfis"],
     n_rows: int = None,
-    ngram_range: tuple = (1, 1),
+    ngram_range: tuple = (1, 2),
     max_df: float = 0.95,
     min_df: int = 2,
     max_features: int = 500,
     n_components: int = 20,
-    n_top_words: int = 10,
+    n_top_words: int = 50,
     save_path: str = "E:/shared_resources/text_models",
 ) -> str:
+    """Pipeline for fitting and saving a lda topic model
+
+    Args:
+        view (str, optional): SQL table with text data to fit model on. Defaults to "psycop_train_val_all_sfis_all_years_lowercase_stopwords_and_symbols_removed".
+        sfi_type (Iterable[str], optional): Which sfi types to include. Defaults to ["All_sfis"].
+        n_rows (int, optional): How many rows to include in the loaded data. If None, all are included. Defaults to None.
+        ngram_range (tuple, optional): The lower and upper boundary of the range of n-values for different word n-grams or char n-grams to be extracted. All values of n such such that min_n <= n <= max_n will be used. For example an ngram_range of (1, 1) means only unigrams, (1, 2) means unigrams and bigrams. Defaults to (1, 2).
+        max_df (float, optional): The proportion of documents the words should appear in to be included. Defaults to 0.95.
+        min_df (int, optional): Remove words occuring in less than min_df documents. Defaults to 2.
+        max_features (int | None, optional): If not None, build a vocabulary that only consider the top max_features ordered by term frequency across the corpus. Otherwise, all features are used. Defaults to 500.
+        save_path (str, optional): Path where the model will be saved. Defaults to "E:/shared_resources/text_models".
+        n_components (int, optional): Number of components/topics to include. Defaults to 20.
+        n_top_words (into, optional): Number of words to extract from each topic. Defaults to 50.
+
+    Returns:
+        str: Log info on the path and filename of the fitted bow model.
+    """
     # create model filename from params
     max_df_str = str(max_df).replace(".", "")
     ngram_range_str = "".join(c for c in str(ngram_range) if c.isdigit())
@@ -184,7 +232,7 @@ def lda_model_pipeline(
 
     # fit model
     lda, model_topics = fit_lda(
-        corpus["text"],
+        corpus=corpus["text"].tolist(),
         ngram_range=ngram_range,
         max_df=max_df,
         min_df=min_df,
