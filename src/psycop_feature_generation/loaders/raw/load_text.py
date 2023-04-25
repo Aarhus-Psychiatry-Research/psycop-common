d7fe1b7aa26bf6466a59b8b724c6b389e13256ac
sarakolding <sarakolding@live.dk>
2023-04-25 17:31:46 +0200

    suppress mypy warning

--- src/psycop_feature_generation/loaders/raw/load_text.py
+++ src/psycop_feature_generation/loaders/raw/load_text.py
@@ -179,7 +179,7 @@ def load_text_split(
     text_split_df = filter_by_split_ids(
         df_to_split=text_df,
         split_id_df=split_id_df,
-        split_name=split_name,
+        split_name=split_name, # type: ignore
     )
 
     return text_split_df
