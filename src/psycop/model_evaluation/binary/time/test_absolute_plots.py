85c931a0b9364b658e8ec5fbf3f49cd406bbcd05
Martin Bernstorff <martinbernstorff@gmail.com>
2023-05-02 11:18:22 +0200

    tests: remove unused function

--- src/psycop/model_evaluation/binary/time/test_absolute_plots.py
+++ src/psycop/model_evaluation/binary/time/test_absolute_plots.py
@@ -4,7 +4,6 @@ from typing import Literal
 import pytest
 from psycop.model_evaluation.binary.time.absolute_plots import (
     plot_metric_by_absolute_time,
-    plot_prob_over_time,
 )
 from psycop.model_evaluation.utils import TEST_PLOT_PATH
 from psycop.model_training.training_output.dataclasses import EvalDataset
@@ -23,16 +22,4 @@ def test_plot_performance_by_calendar_time(
         bin_period=bin_period,
         confidence_interval=0.95,
         save_path=TEST_PLOT_PATH / f"test_{bin_period}.png",  # type: ignore
-    )
-
-
-def test_prob_over_time(subsampled_eval_dataset: EvalDataset, tmp_path: str):
-    plot_prob_over_time(
-        patient_id=subsampled_eval_dataset.ids,
-        timestamp=subsampled_eval_dataset.pred_timestamps,
-        pred_prob=subsampled_eval_dataset.y_hat_probs,
-        outcome_timestamp=subsampled_eval_dataset.outcome_timestamps,
-        label=subsampled_eval_dataset.y,
-        look_behind_distance=500,
-        save_path=Path(tmp_path),
     )
