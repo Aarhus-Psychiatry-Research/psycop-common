921e2ae95f6b8e1baf4e470e8b01b7c1cd268a67
Martin Bernstorff <martinbernstorff@gmail.com>
2023-05-02 10:28:28 +0200

    tests: refactor tests

---
+++ src/psycop/model_evaluation/binary/global_performance/test_global_performance_plots.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from psycop.model_evaluation.binary.global_performance.precision_recall import (
+    plot_precision_recall,
+)
+from psycop.model_evaluation.binary.global_performance.roc_auc import plot_auc_roc
+from psycop.model_evaluation.utils import TEST_PLOT_PATH
+from psycop.model_training.training_output.dataclasses import EvalDataset
+
+
+def test_plot_roc_auc(subsampled_eval_dataset: EvalDataset):
+    plot_auc_roc(
+        eval_dataset=subsampled_eval_dataset,
+        save_path=TEST_PLOT_PATH / "roc_auc.png",
+        n_bootstraps=10,
+    )
+
+
+def test_plot_precision_recall(subsampled_eval_dataset: EvalDataset):
+    plot_precision_recall(
+        eval_dataset=subsampled_eval_dataset,
+        save_path=Path(
+            TEST_PLOT_PATH / "precision_recall.png",
+        ),
+    )
