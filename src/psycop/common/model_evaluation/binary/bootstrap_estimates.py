5253a64bbbb1b1c1cf520a7d0f5f65e42ea295e7
Martin Bernstorff <martinbernstorff@gmail.com>
2023-05-15 09:47:02 +0200

    refactor: split out estimate bootstrapping

---
+++ src/psycop/common/model_evaluation/binary/bootstrap_estimates.py
@@ -0,0 +1,48 @@
+from collections.abc import Callable
+from typing import Any
+
+import numpy as np
+import pandas as pd
+from scipy.stats import bootstrap
+
+
+def bootstrap_estimates(
+    metric: Callable,
+    input_1: pd.Series,
+    input_2: pd.Series,
+    n_bootstraps: int = 100,
+    ci_width: float = 0.95,
+    **kwargs: Any,
+) -> pd.Series:
+    _kwargs = {
+        "method": "basic",
+        "n_resamples": n_bootstraps,
+    }
+    _kwargs.update(kwargs)
+
+    # Calculate the confidence interval
+    def metric_wrapper(
+        true: np.ndarray,
+        pred: np.ndarray,
+        **kwargs: Any,  # noqa: ARG001
+    ) -> float:
+        # bootstrap function requires the metric function to
+        # be able to take additional arguments (notably the length of the array)
+        try:
+            return metric(true, pred)
+        except ValueError as e:
+            print(repr(e))
+            return np.nan
+
+    boot = bootstrap(
+        (input_1, input_2),
+        statistic=metric_wrapper,
+        confidence_level=ci_width,
+        paired=True,
+        **_kwargs,
+    )
+    low, high = boot.confidence_interval.low, boot.confidence_interval.high
+
+    return pd.Series(
+        {"ci": (low, high)},
+    )
