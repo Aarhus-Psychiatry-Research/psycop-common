f6cde36b9f95153b12968175e5ec8531786b8409
Martin Bernstorff <martinbernstorff@gmail.com>
2023-05-17 09:55:17 +0200

    feat: print a4 conversion factor

--- src/psycop/common/model_evaluation/patchwork/patchwork_grid.py
+++ src/psycop/common/model_evaluation/patchwork/patchwork_grid.py
@@ -9,7 +9,9 @@ def create_patchwork_grid(
     n_in_row: int,
     add_subpanels_letters: bool = True,
 ) -> pw.Bricks:
-    """Create a grid from a sequence of ggplto objects."""
+    """Create a grid from a sequence of ggplot objects."""
+    print_a4_ratio(plots, single_plot_dimensions, n_in_row)
+
     bricks = [pw.load_ggplot(plot, figsize=single_plot_dimensions) for plot in plots]
 
     alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -33,3 +35,22 @@ def create_patchwork_grid(
     # Combine the rows
     patchwork = pw.stack(rows, operator="/")
     return patchwork
+
+
+def print_a4_ratio(
+    plots: Sequence[pn.ggplot],
+    single_plot_dimensions: tuple[float, float],
+    n_in_row: int,
+):
+    """Print conversion factor to A4 for a given grid of plots."""
+    n_rows = int(len(plots) / n_in_row)
+    total_height = single_plot_dimensions[1] * n_rows
+    total_width = single_plot_dimensions[0] * n_in_row
+    a4_ratio = 297 / 210
+    new_plot_height_for_a4 = (a4_ratio * total_width - total_height) / n_rows
+
+    print(f"height/width ratio is {total_height/total_width}")
+    print(f"A4 ratio is {a4_ratio}")
+    print(
+        f"You could decrease single_plot_height to {new_plot_height_for_a4} for the patchwork to fit A4"
+    )
