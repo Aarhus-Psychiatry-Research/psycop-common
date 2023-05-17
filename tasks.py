beba5ca405b0bc461c2a8f7a5f1d624842578d99
Martin Bernstorff <martinbernstorff@gmail.com>
2023-05-17 13:53:26 +0200

    build: run tests with 4 cores

--- tasks.py
+++ tasks.py
@@ -348,7 +348,7 @@ def test(
     if not pytest_args:
         pytest_args = [
             "src/psycop",
-            "-n 2",
+            "-n 4",
             "-rfE",
             "--failed-first",
             "-p no:cov",
