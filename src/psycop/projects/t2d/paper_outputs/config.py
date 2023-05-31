71b8dd04e56288244188dd7fbaa017740adb7cd1
Martin Bernstorff <martinbernstorff@gmail.com>
2023-05-31 15:27:51 +0200

    feat: increase size of axis labels in t2d pn theme

--- src/psycop/projects/t2d/paper_outputs/config.py
+++ src/psycop/projects/t2d/paper_outputs/config.py
@@ -19,7 +19,9 @@ EVAL_GROUP = RunGroup(name=EVAL_GROUP_NAME)
 ################
 date_str = datetime.now().strftime("%Y-%m-%d")
 
-PN_THEME = pn.theme_bw() + pn.theme(panel_grid=pn.element_blank())
+PN_THEME = pn.theme_bw() + pn.theme(
+    panel_grid=pn.element_blank(), axis_title=pn.element_text(size=14)
+)
 
 
 @dataclass
