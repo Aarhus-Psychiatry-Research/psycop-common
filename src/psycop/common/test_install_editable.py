8b24ad0c66398e587d98e0a92a4cdd66cd63a6fd
Martin Bernstorff <ryqiem@gmail.com>
2023-06-15 10:12:34 +0200

    test: first commit

---
+++ src/psycop/common/test_install_editable.py
@@ -0,0 +1,2 @@
+def test_editable():
+    print("Install editable, Martin's version")
---
+++ src/psycop/projects/testing/test.py
@@ -0,0 +1,4 @@
+from psycop.common.test_install_editable import test_editable
+
+if __name__ == "__main__":
+    test_editable()
