c7ba4732058824b037ff92ade9387b249c7ea3e6
Martin Bernstorff <martinbernstorff@gmail.com>
2023-07-10 14:01:03 +0200

    misc.

--- psycop/common/patient_print/test_patient_printer.py
+++ psycop/common/patient_print/test_patient_printer.py
@@ -5,6 +5,7 @@ from typing import Sequence
 import matplotlib.pyplot as plt
 import matplotlib.style as mplstyle
 import polars as pl
+from matplotlib.markers import MarkerStyle
 
 from psycop.common.test_utils.str_to_df import str_to_pl_df
 
@@ -29,6 +30,7 @@ class HealthPrintPredictionTime:
     x_axis: str = "rel_time"
     y_axis: str = "type"
     color: str = "value"
+    interpretable_plots: bool = False
 
 
 @dataclass(frozen=True)
@@ -66,22 +68,58 @@ no_diabetes_patient = PatientType(
 )
 
 
+def test_save_health_print():
+    save_health_print(
+        HealthPrintPredictionTime(
+            df=diabetes_patient.data.filter(pl.col("type") == "HbA1c"),
+            i=0,
+            x_min=0,
+            x_max=-365,
+            y_min=2,
+            y_max=4,
+            subtype="diabetes" if diabetes_patient.diabetes else "control",
+            output_dir=Path("test_output"),
+            interpretable_plots=False,
+        )
+    )
+
+    pass
+
+
 def save_health_print(
     data: HealthPrintPredictionTime,
 ):
+    in_per_feat = 1 / 10
     df = data.df
+
+    if not data.interpretable_plots:
+        n_x_vals = abs(data.x_min - data.x_max)
+        x_inches = (n_x_vals / 16) * in_per_feat
+
+        n_features = len(df[data.y_axis].unique())
+        y_inches = n_features * in_per_feat
+    else:
+        x_inches = 400
+        y_inches = 300
+
+    fig = plt.figure(figsize=(x_inches, y_inches))
     plt.scatter(
         df[data.x_axis],
         df[data.y_axis],
         c=df[data.color],
         vmin=data.y_min,
         vmax=data.y_max,
+        marker=MarkerStyle("|"),
+        s=5,
     )
     plt.xlim(data.x_min, data.x_max)
 
     data.output_dir.mkdir(exist_ok=True, parents=True)
 
-    plt.savefig(f"{data.output_dir}/{data.subtype}_{data.i}.jpg")
+    # Save
+    plt.plot()
+    plt.gca().set_position([-0, 0, 1.1, 1.1])
+    plt.savefig(f"{data.output_dir}/{data.subtype}_{data.i}.png", dpi=100)
     plt.close()
 
 
@@ -104,6 +142,7 @@ def test_patient_print(n_plots: int, patient: PatientType, tmp_path: Path):
             y_max=4,
             subtype="diabetes" if patient.diabetes else "control",
             output_dir=tmp_path,
+            interpretable_plots=False,
         )
         for i in range(n_plots)
     ]
