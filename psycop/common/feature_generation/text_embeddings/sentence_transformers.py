9b998f8cfd71e79a33cdd08024a45314923ee345
Lasse <lasseh0310@gmail.com>
2023-07-20 13:02:43 +0200

    feat: sentence transformer embedding

--- psycop/common/feature_generation/text_embeddings/sentence_transformers.py
+++ psycop/common/feature_generation/text_embeddings/sentence_transformers.py
@@ -1,12 +1,26 @@
 """Apply sentence transformer to text data and save to disk"""
-from sentence_transformers import SentenceTransformer
 import pandas as pd
+from sentence_transformers import SentenceTransformer
 
-def embed_text(model: SentenceTransformer, text: list[str]) -> pl.DataFrame:
+from psycop.common.feature_generation.loaders.raw.load_text import load_all_notes
+from psycop.common.global_utils.paths import TEXT_EMBEDDINGS_DIR
+
+
+def embed_text_to_df(model, text: list[str]) -> pd.DataFrame:
     embeddings = model.encode(text)
     return pd.DataFrame(embeddings)
 
 
 
 if __name__ == "__main__":
+    model_str = "paraphrase-multilingual-MiniLM-L12-v2"
+
+    all_notes = load_all_notes(n_rows=100, include_sfi_name=True)
+
+    model = SentenceTransformer(model_str)    
+    embeddings = embed_text_to_df(model, all_notes["value"].tolist())
     
+    all_notes = all_notes.drop(columns=["value"])
+    all_notes = pd.concat([all_notes, embeddings], axis=1)
+
+    all_notes.to_parquet(TEXT_EMBEDDINGS_DIR / f"text_embeddings_{model_str}.parquet")
--- psycop/common/global_utils/paths.py
+++ psycop/common/global_utils/paths.py
@@ -2,4 +2,5 @@ from pathlib import Path
 
 PSYCOP_PKG_ROOT = Path(__file__).parent.parent
 OVARTACI_SHARED_DIR = Path(r"E:\shared_resources")
+TEXT_EMBEDDINGS_DIR = OVARTACI_SHARED_DIR / "text_embeddings"
 RANDOM_SEED = 42
