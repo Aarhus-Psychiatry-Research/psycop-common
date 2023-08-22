cf7b32a95615ea0f502755f0ef8f0ad92ee62d15
Martin Bernstorff <martinbernstorff@gmail.com>
2023-08-22 14:51:01 +0200

    mis.

--- psycop/common/feature_generation/sequences/mlm_sequence_loaders/test_a_diagnosis_formatting.py
+++ psycop/common/feature_generation/sequences/mlm_sequence_loaders/test_a_diagnosis_formatting.py
@@ -1,5 +1,5 @@
 from psycop.common.feature_generation.sequences.mlm_sequence_loaders.diagnoses_sequences import (
-    format_diagnosis_columns,
+    DiagnosisLoader,
 )
 from psycop.common.test_utils.str_to_df import str_to_pl_df
 
@@ -12,7 +12,7 @@ def test_a_diagnosis_formatting():
     """,
     )
 
-    formatted_df = format_diagnosis_columns(df=df.lazy())
+    formatted_df = DiagnosisLoader().format_diagnosis_columns(df=df.lazy())
 
     resulting_diagnoses = formatted_df.collect().get_column("value").to_list()
     assert resulting_diagnoses == ["DF431", "DF431", "ALFC3", "DF329"]
