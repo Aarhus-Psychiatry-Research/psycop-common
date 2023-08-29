370aebecca6512326678ab21a784141878854069
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2023-08-29 09:22:12 +0200

    fix: Added type hints

--- psycop/common/sequence_models/dataset.py
+++ psycop/common/sequence_models/dataset.py
@@ -8,11 +8,11 @@ from psycop.common.data_structures import Patient
 
 
 class PatientDataset(Dataset):
-    def __init__(self, patients: list[Patient]):
-        self.patients = patients
+    def __init__(self, patients: list[Patient]) -> None:
+        self.patients: list[Patient] = patients
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.patients)
 
-    def __getitem__(self, idx: int):
+    def __getitem__(self, idx: int) -> Patient:
         return self.patients[idx]
