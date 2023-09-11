13f5a3841d7371be25e2743865d62e985b837f15
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2023-09-11 15:22:55 +0200

    fix: fix error from static type checks

--- psycop/common/data_structures/__init__.py
+++ psycop/common/data_structures/__init__.py
@@ -1,3 +1,5 @@
 from .patient import Patient
 from .static_feature import StaticFeature
 from .temporal_event import TemporalEvent
+
+__all__ = ["Patient", "StaticFeature", "TemporalEvent"]
--- psycop/common/sequence_models/__init__.py
+++ psycop/common/sequence_models/__init__.py
@@ -2,3 +2,12 @@ from .dataset import PatientDataset
 from .embedders import BEHRTEmbedder, Embedder
 from .tasks import BEHRTForMaskedLM
 from .trainer import TrainableModule, Trainer
+
+__all__ = [
+    "BEHRTEmbedder",
+    "BEHRTForMaskedLM",
+    "Embedder",
+    "PatientDataset",
+    "TrainableModule",
+    "Trainer",
+]
