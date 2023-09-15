8c0b038f8ccb82090e1b4b24308fab70b1b2d02b
Jakob Grøhn <bokajgd@gmail.com>
2023-09-15 14:18:03 +0200

    feat: descriptive stats

--- psycop/projects/forced_admission_inpatient/model_eval/dataset_description/train_val_descriptive_comparison.py
+++ psycop/projects/forced_admission_inpatient/model_eval/dataset_description/train_val_descriptive_comparison.py
@@ -1,24 +1,72 @@
 """Load and compare train and validation splits."""
 import sys
 from pathlib import Path
-from typing import Optional, Union
 
-import pandas as pd
-import wandb
 import hydra
+import numpy as np
+import pandas as pd
 
-from psycop.common.model_training.data_loader.utils import (
-    load_and_filter_split_from_cfg,
-)
 from psycop.common.model_training.config_schemas.conf_utils import (
     convert_omegaconf_to_pydantic_object,
 )
 from psycop.common.model_training.config_schemas.full_config import FullConfigSchema
+from psycop.common.model_training.data_loader.utils import (
+    load_and_filter_split_from_cfg,
+)
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 CONFIG_PATH = PROJECT_ROOT / "model_training" / "config"
 REPO_ROOT = Path(__file__).resolve().parents[4]
-SYNTH_DATA_PATH = REPO_ROOT / "common" / "test_utils" / "test_data" / "flattened" / "synth_flattened_with_outcome.csv"
+SYNTH_DATA_PATH = (
+    REPO_ROOT
+    / "common"
+    / "test_utils"
+    / "test_data"
+    / "flattened"
+    / "synth_flattened_with_outcome.csv"
+)
+
+
+def _generate_general_descriptive_stats(
+    df: pd.DataFrame,
+    split_name: str,
+) -> pd.DataFrame:
+    """Generate descriptive stats table."""
+
+    unique_patients = df["dw_ek_borger"].nunique()
+    mean_entries_per_patient = df.shape[0] / unique_patients
+    mean_age = df["pred_age_in_years"].mean()
+
+    female_ratio = df["pred_sex"].sum() / df.shape[0]
+
+    pred_cols = _get_pred_cols_df(df)
+    na_ratios = pred_cols.isna().sum().sum() / pred_cols.shape[0] * pred_cols.shape[1]
+
+    outcomes = df["outc_t2d_within_30_days_maximum_fallback_0_dichotomous"].sum()
+    outcome_rate = outcomes / df.shape[0]
+
+    stats = pd.DataFrame(
+        data={
+            "split_name": [split_name],
+            "number_of_entries": [df.shape[0]],
+            "unique_patients": [unique_patients],
+            "mean_entries_per_patient": [mean_entries_per_patient],
+            "mean_age": [mean_age],
+            "female_ratio": [female_ratio],
+            "outcome_rate": [outcome_rate],
+            "outcomes": [outcomes],
+            "na_ratios": [na_ratios],
+        },
+    )
+
+    return stats
+
+
+def _get_pred_cols_df(df: pd.DataFrame) -> pd.DataFrame:
+    """Get columns that start with pred_."""
+    pred_cols = [col for col in df.columns if col.startswith("pred_")]
+
+    return df[pred_cols]
 
 
 @hydra.main(
@@ -26,32 +74,57 @@ SYNTH_DATA_PATH = REPO_ROOT / "common" / "test_utils" / "test_data" / "flattened
     config_name="default_config",
     version_base="1.2",
 )
-def main(cfg: FullConfigSchema, synth_data: bool = True):
+def main(
+    cfg: FullConfigSchema,
+    synth_data: bool = True,
+    save: bool = False,
+) -> pd.DataFrame:
     """Main."""
-    cfg = convert_omegaconf_to_pydantic_object(cfg)
+    cfg = convert_omegaconf_to_pydantic_object(cfg)  # type: ignore
 
     if sys.platform == "win32":
         (PROJECT_ROOT / "wandb" / "debug-cli.onerm").mkdir(exist_ok=True, parents=True)
 
     if synth_data:
+        df = pd.read_csv(SYNTH_DATA_PATH, index_col=0)
 
-        df = pd.read_csv(SYNTH_DATA_PATH)
+        df["pred_age_in_years"] = np.random.randint(18, 100, df.shape[0])
+        df["pred_sex"] = np.random.randint(0, 2, df.shape[0])
 
-    train_df = load_and_filter_split_from_cfg(
-        data_cfg=cfg.data,
-        pre_split_cfg=cfg.preprocessing.pre_split,
-        split="train",
-    )
+        df = df.rename(columns={"citizen_ids": "dw_ek_borger"})
 
-    val_df = load_and_filter_split_from_cfg(
-        data_cfg=cfg.data,
-        pre_split_cfg=cfg.preprocessing.pre_split,
-        split="train",
-    )
+        train_df = df.sample(frac=0.8, random_state=42)
+        val_df = df.drop(train_df.index)
 
-    return train_df, val_df
+    else:
+        train_df = load_and_filter_split_from_cfg(
+            data_cfg=cfg.data,
+            pre_split_cfg=cfg.preprocessing.pre_split,
+            split="train",
+        )
+
+        val_df = load_and_filter_split_from_cfg(
+            data_cfg=cfg.data,
+            pre_split_cfg=cfg.preprocessing.pre_split,
+            split="train",
+        )
+
+    train_stats = _generate_general_descriptive_stats(train_df, split_name="train")
+    val_stats = _generate_general_descriptive_stats(val_df, split_name="val")
+
+    stats = pd.concat([train_stats, val_stats], axis=0).reset_index(drop=True)
+
+    if save:
+        output_dir = (
+            Path("E:\\shared_resources\\forced_admissions_inpatient")
+            / "data_split_comparisons"
+        )
+
+        stats.to_csv(output_dir / "train_val_descriptive_comparison.csv")
+
+    return stats
 
 
 if __name__ == "__main__":
     main()
-    print('Done!')
+    print("Done!")
