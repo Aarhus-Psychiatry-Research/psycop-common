5bbdd7c06527865f88d1b5a6c43cef2e685ee0f5
bokajgd <bokajgd@gmail.com>
2023-09-18 13:24:13 +0200

    fix: lint

--- psycop/projects/forced_admission_inpatient/model_training/retrain_pretrained_model.py
+++ psycop/projects/forced_admission_inpatient/model_training/retrain_pretrained_model.py
@@ -4,10 +4,10 @@ Required to allow the trainer_spawner to point towards a python script
 file, rather than an installed module.
 """
 import faulthandler
+import json
 import sys
 from pathlib import Path
 from typing import Union
-import json
 
 from omegaconf import DictConfig
 
@@ -21,14 +21,16 @@ from psycop.common.model_training.config_schemas.full_config import FullConfigSc
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 PRETRAINED_CFG_PATH = Path(
-            f"E:/shared_resources/forced_admissions_inpatient/models/only_text_with_sentence_transformer_and_tfidf_750_embeddings/pipeline_eval/puncturation-unbeset/hamosemassecuite/cfg.json",
-        )
+    "E:/shared_resources/forced_admissions_inpatient/models/only_text_with_sentence_transformer_and_tfidf_750_embeddings/pipeline_eval/puncturation-unbeset/hamosemassecuite/cfg.json",
+)
+
 
 def _get_cfg_from_json_file(path: Path) -> FullConfigSchema:
     # Loading the json instead of the .pkl makes us independent
     # of whether the imports in psycop-common model-training have changed
     # TODO: Note that this means assigning to the cfg property does nothing, since it's recomputed every time it's called
     return FullConfigSchema.parse_obj(json.loads(json.loads(path.read_text())))
+
 
 def main(cfg: Union[DictConfig, FullConfigSchema]) -> float:
     """Main."""
