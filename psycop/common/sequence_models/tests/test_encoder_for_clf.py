e60f99282500a4edcc13c3d7270c9eb4f3171b86
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2023-10-09 15:57:52 +0200

    chore: remove duplicate fixture

--- psycop/common/sequence_models/tests/test_encoder_for_clf.py
+++ psycop/common/sequence_models/tests/test_encoder_for_clf.py
@@ -15,41 +15,7 @@ from psycop.common.sequence_models import (
 
 
 @pytest.fixture()
-def patients() -> list[Patient]:
-    """
-    Returns a list of patient objects
-    """
-
-    e1 = TemporalEvent(
-        timestamp=datetime(2021, 1, 1),
-        value="d1",
-        source_type="diagnosis",
-        source_subtype="",
-    )
-    e2 = TemporalEvent(
-        timestamp=datetime(2021, 1, 3),
-        value="d2",
-        source_type="diagnosis",
-        source_subtype="",
-    )
-
-    patient1 = Patient(
-        patient_id=1,
-        date_of_birth=datetime(1990, 1, 1),
-    )
-    patient1.add_events([e1, e2])
-
-    patient2 = Patient(
-        patient_id=2,
-        date_of_birth=datetime(1993, 3, 1),
-    )
-    patient2.add_events([e1, e2, e2, e1])
-
-    return [patient1, patient2]
-
-
-@pytest.fixture()
-def patient_dataset(patients: list) -> PatientDatasetWithLabels:
+def patient_dataset_with_labels(patients: list) -> PatientDatasetWithLabels:
     return PatientDatasetWithLabels(patients, labels=[0, 1])
 
 
@@ -83,7 +49,7 @@ def aggregation_module() -> AveragePooler:
 
 
 def test_encoder_for_clf_(
-    patient_dataset: PatientDatasetWithLabels,
+    patient_dataset_with_labels: PatientDatasetWithLabels,
     embedding_module: BEHRTEmbedder,
     encoder_module: nn.Module,
     aggregation_module: AggregationModule,
@@ -98,7 +64,7 @@ def test_encoder_for_clf_(
     )
 
     dataloader = DataLoader(
-        patient_dataset,
+        patient_dataset_with_labels,
         batch_size=32,
         shuffle=True,
         collate_fn=clf.collate_fn,  # type: ignore
