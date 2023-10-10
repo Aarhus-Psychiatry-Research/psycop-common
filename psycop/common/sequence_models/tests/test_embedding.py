81531f513a7b432d577abf6a5d9245e413928dcc
frillecode <frihae25@gmail.com>
2023-10-10 15:55:00 +0200

    tests: test diagnosis mapping

--- psycop/common/sequence_models/tests/test_embedding.py
+++ psycop/common/sequence_models/tests/test_embedding.py
@@ -1,8 +1,11 @@
 import pytest
 import torch
 
+import datetime as dt
+
 from psycop.common.sequence_models.embedders.BEHRT_embedders import BEHRTEmbedder
 from psycop.common.sequence_models.embedders.interface import Embedder
+from psycop.common.data_structures import Patient, TemporalEvent
 
 from .conftest import patients  # noqa: F401 # type: ignore
 
@@ -27,3 +30,63 @@ def test_embeddings(patients: list, embedding_module: Embedder):  # noqa: F811
 
     # forward
     embedding_module(inputs_ids)
+
+
+@pytest.mark.parametrize(
+    "embedding_module",
+    [BEHRTEmbedder(d_model=384, dropout_prob=0.1, max_sequence_length=128)],
+)
+def test_diagnosis_mapping(
+    patients: list, embedding_module: BEHRTEmbedder
+):  # noqa: F811
+    """
+    Test mapping of diagnosis from ICD10 to caliber
+    """
+
+    patient = Patient(
+        patient_id=11,
+        date_of_birth=dt.datetime(year=1990, month=1, day=1),
+    )
+
+    temporal_events = [
+        TemporalEvent(
+            timestamp=dt.datetime(2021, 1, 1),
+            value="A00",
+            source_type="diagnosis",
+            source_subtype="test_name",
+        ),
+        TemporalEvent(
+            timestamp=dt.datetime(2021, 1, 3),
+            value="A30",
+            source_type="diagnosis",
+            source_subtype="test_name",
+        ),
+        TemporalEvent(
+            timestamp=dt.datetime(2021, 1, 3),
+            value="I65",
+            source_type="diagnosis",
+            source_subtype="test_name",
+        ),
+    ]
+
+    patient.add_events(temporal_events)
+
+    patients.append(patient)
+
+    patient_events: list[tuple[Patient, TemporalEvent]] = [
+        (p, e)
+        for p in patients
+        for e in embedding_module.filter_events(p.temporal_events)
+    ]
+    diagnosis_codes: list[str] = [e.value for p, e in patient_events]  # type: ignore
+
+    # map diagnosis codes
+    mapped_diagnosis_codes = embedding_module.get_mapped_diagnosis_codes(
+        diagnosis_codes=diagnosis_codes,
+    )
+
+    assert mapped_diagnosis_codes == [
+        "Bacterial Diseases (excl TB)",
+        "Bacterial Diseases (excl TB)",
+        "Transient ischaemic attack",
+    ]
