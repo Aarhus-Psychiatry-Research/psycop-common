50aa41af4372b068f598947785bec6c5cb2b16ea
Martin Bernstorff <martinbernstorff@gmail.com>
2023-10-18 13:01:30 +0000

    misc.

--- psycop/common/data_structures/patient_slice.py
+++ psycop/common/data_structures/patient_slice.py
@@ -2,14 +2,17 @@ import datetime as dt
 from collections.abc import Sequence
 from dataclasses import dataclass
 
-from psycop.common.data_structures.patient import Patient
 from psycop.common.data_structures.static_feature import StaticFeature
 from psycop.common.data_structures.temporal_event import TemporalEvent
+from psycop.common.data_structures.patient import Patient
+from psycop.common.feature_generation.sequences.cohort_definer_to_prediction_times import (
+    PATIENT_ID,
+)
 
 
 @dataclass(frozen=True)
 class PatientSlice:
-    patient: Patient
+    patient_id: PATIENT_ID
     date_of_birth: dt.datetime
     _temporal_events: Sequence[TemporalEvent]
     _static_features: Sequence[StaticFeature]
