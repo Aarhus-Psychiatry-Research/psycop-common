df8e8fd67d05329afbd08cc56947f4ca7d46bd2a
HLasse <lasseh0310@gmail.com>
2023-10-26 12:58:17 +0200

    add visit counts to hospital groupings

--- psycop/common/data_inspection/visits_by_hospital_units/check_hospital_units.py
+++ psycop/common/data_inspection/visits_by_hospital_units/check_hospital_units.py
@@ -15,9 +15,10 @@ def load_shak_to_location_mapping() -> pl.DataFrame:
         pl.col("shak_6").cast(str),
     )
 
+
 def shak_codes_to_drop() -> list[str]:
-    """Dropping Børne og ungdomspsykiatrisk afdeling/center as we don't use 
-    data from child psychiatry. Dropping central visitation as it is just a 
+    """Dropping Børne og ungdomspsykiatrisk afdeling/center as we don't use
+    data from child psychiatry. Dropping central visitation as it is just a
     place of administration"""
     return ["660011", "660020", "660021"]
 
@@ -40,17 +41,28 @@ if __name__ == "__main__":
     # number of  visits at each location
     count_and_sort_values_of_series(df["department"])
     count_and_sort_values_of_series(df["unit"])
+    count_and_sort_values_of_series(df["grouping"])
 
-    # number of unique locations (cities) visited by patient
-    df.groupby("dw_ek_borger").agg(
-        pl.col("unit").n_unique().alias("n_unique_locations"),
-    )["n_unique_locations"].value_counts()
+    # number of unique groupings visited by patient
+    n_locations_and_visits_by_patient = df.groupby("dw_ek_borger").agg(
+        pl.col("grouping").n_unique().alias("n_unique_locations"),
+        pl.col("grouping").count().alias("n_visits"),
+    )
 
+    n_locations_and_visits_by_patient.groupby("n_unique_locations").agg(
+        pl.col("n_visits").count().alias("n_patients"),
+        pl.col("n_visits").sum().alias("n_visits"),
+    ).sort("n_unique_locations")
+
+    df.groupby("grouping").agg(
+        pl.col("dw_ek_borger").unique().count().alias("n_patients"),
+        pl.col("dw_ek_borger").count().alias("n_visits"),
+    ).sort("n_patients", descending=True)
 
 
 # vest = herning, holstebro, gødstrup
 # midt = silkeborg, viborg, skive
-# øst = horsens, aarhus, risskov
-# nord = randers
+# øst = horsens, aarhus, risskov, randers
 
 
+# tilføj antal besøg ved unique pateint ats visit
