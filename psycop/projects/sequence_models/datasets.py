17c463a2aa26e040e1710146910e1f82c1013d59
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2023-10-26 18:00:30 +0200

    fix: removed todo comment

--- psycop/projects/sequence_models/datasets.py
+++ psycop/projects/sequence_models/datasets.py
@@ -5,21 +5,27 @@ datasets for training and evaluation of sequence models
 import datetime as dt
 
 from psycop.common.feature_generation.loaders.raw.load_ids import SplitName
-from psycop.common.feature_generation.sequences.cohort_definer_to_prediction_times import \
-    CohortToPredictionTimes
+from psycop.common.feature_generation.sequences.cohort_definer_to_prediction_times import (
+    CohortToPredictionTimes,
+)
 from psycop.common.feature_generation.sequences.patient_loaders import (
-    DiagnosisLoader, PatientLoader)
-from psycop.common.sequence_models.dataset import (PatientSliceDataset,
-                                                   PatientSlicesWithLabels)
+    DiagnosisLoader,
+    PatientLoader,
+)
+from psycop.common.sequence_models.dataset import (
+    PatientSliceDataset,
+    PatientSlicesWithLabels,
+)
 from psycop.common.sequence_models.registry import Registry
-from psycop.projects.t2d.feature_generation.cohort_definition.t2d_cohort_definer import \
-    T2DCohortDefiner
+from psycop.projects.t2d.feature_generation.cohort_definition.t2d_cohort_definer import (
+    T2DCohortDefiner,
+)
 
 
 @Registry.datasets.register("patient_slice_dataset")
 def create_patient_slice_dataset(
     min_n_visits: int,
-    split_name: str,  # TODO: @martin hvordan ville du type hinte det her?
+    split_name: str,
 ) -> PatientSliceDataset:
     train_patients = PatientLoader.get_split(
         event_loaders=[
