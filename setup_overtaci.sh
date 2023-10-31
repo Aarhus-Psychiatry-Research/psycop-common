df5bb2d0c0e5d5b94a71bc84ca106c3f2754ea84
Kenneth <Kennethcenevoldsen@gmail.com>
2023-10-31 11:09:49 +0100

    Added setup for overtaci installation

--- setup_overtaci.sh
+++ setup_overtaci.sh
@@ -2,3 +2,6 @@
 
 pip install -r requirements.txt -r dev-requirements.txt -r gpu-requirements.txt
 conda install pytorch=2.1.0 pytorch-cuda=12.1 -c https://exrhel0371.it.rm.dk/api/repo/pytorch -c https://exrhel0371.it.rm.dk/api/repo/nvidia -c https://exrhel0371.it.rm.dk/api/repo/anaconda --override-channels --insecure -y
+
+# test environment
+python -c "import torch; t=torch.tensor(1); t.to(torch.device('cuda'))"
