d72c9b6b00e0c8a40120446af67315b2889d1129
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-01 13:52:53 +0000

    base-interfaces

--- psycop/common/model_training_v2/metrics/binary_metrics/base.py
+++ psycop/common/model_training_v2/metrics/binary_metrics/base.py
@@ -1,8 +1,10 @@
 from typing import Protocol
 
+import polars as pl
+
 from ...presplit_preprocessing.polars_frame import PolarsFrame
 
 
 class BinaryMetric(Protocol):
-    def __call__(self, y_true: PolarsFrame, y_pred: PolarsFrame) -> float:
+    def __call__(self, y_true: PolarsFrame, y_pred: pl.Series) -> float:
         ...
