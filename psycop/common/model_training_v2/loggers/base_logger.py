7e07df2ee49bfbe9a7a25589875110f823a8dd95
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-02 12:59:13 +0000

    add_age_filter

--- psycop/common/model_training_v2/loggers/base_logger.py
+++ psycop/common/model_training_v2/loggers/base_logger.py
@@ -8,7 +8,7 @@ class BaselineLogger(Protocol):
     def warn(self, message: str) -> None:
         ...
 
-    def error(self, message: str) -> None:
+    def fail(self, message: str) -> None:
         ...
 
     def log_metric(self, name: str, value: float) -> None:
