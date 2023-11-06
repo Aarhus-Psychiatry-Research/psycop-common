87af0b497a30d2d4a8db59b9062f3230b53003c3
Lasse Hansen <lasseh0310@gmail.com>
2023-11-06 09:46:01 +0100

    implemented binary classification pipeline

--- psycop/common/model_training_v2/classifier_pipelines/binary_classification_pipeline.py
+++ psycop/common/model_training_v2/classifier_pipelines/binary_classification_pipeline.py
@@ -6,14 +6,17 @@ from psycop.common.model_training_v2.classifier_pipelines.model_step import Mode
 from psycop.common.model_training_v2.presplit_preprocessing.polars_frame import (
     PolarsFrame,
 )
+from sklearn.pipeline import Pipeline
 
 
 class BinaryClassificationPipeline:
     def __init__(self, steps: Sequence[ModelStep]):
-        ...
+        self.pipe = Pipeline(steps=steps)
 
-    def fit(self, X: PolarsFrame, y: PolarsFrame) -> None:
-        ...
+    def fit(self, X: PolarsFrame, y: pl.Series) -> None:
+        self.pipe.fit(X, y)
 
     def predict_proba(self, X: PolarsFrame) -> pl.Series:
-        ...
+        """Returns the predicted probabilities of the `1`
+        class"""
+        return self.pipe.predict_proba(X)[:, 1]
