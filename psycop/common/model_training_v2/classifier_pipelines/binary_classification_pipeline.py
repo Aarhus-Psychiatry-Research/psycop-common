e942907958a6cda5985bf4702649ebb7cb4c1758
Lasse <lasseh0310@gmail.com>
2023-11-06 09:00:46 +0000

    fix: collect lazyframe and return pl.series

--- psycop/common/model_training_v2/classifier_pipelines/binary_classification_pipeline.py
+++ psycop/common/model_training_v2/classifier_pipelines/binary_classification_pipeline.py
@@ -1,12 +1,12 @@
 from collections.abc import Sequence
 
 import polars as pl
+from sklearn.pipeline import Pipeline
 
 from psycop.common.model_training_v2.classifier_pipelines.model_step import ModelStep
 from psycop.common.model_training_v2.presplit_preprocessing.polars_frame import (
     PolarsFrame,
 )
-from sklearn.pipeline import Pipeline
 
 
 class BinaryClassificationPipeline:
@@ -14,9 +14,14 @@ class BinaryClassificationPipeline:
         self.pipe = Pipeline(steps=steps)
 
     def fit(self, X: PolarsFrame, y: pl.Series) -> None:
-        self.pipe.fit(X, y)
+        if isinstance(X, pl.LazyFrame):
+            X = X.collect()
+        self.pipe.fit(X.to_pandas(), y)
 
     def predict_proba(self, X: PolarsFrame) -> pl.Series:
         """Returns the predicted probabilities of the `1`
         class"""
-        return self.pipe.predict_proba(X)[:, 1]
+        if isinstance(X, pl.LazyFrame):
+            X = X.collect()
+        pred_probs = self.pipe.predict_proba(X.to_pandas())[:, 1]
+        return pl.Series("y_hat_probs", pred_probs)
