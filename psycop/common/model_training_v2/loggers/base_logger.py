0b59e910636cccb56f243dea749b7b6396299d51
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-07 10:53:06 +0100

    feat: add logger.info() (#382)
    - Squash changes from parent branch
    - feat: add logger.info

--- psycop/common/model_training_v2/loggers/base_logger.py
+++ psycop/common/model_training_v2/loggers/base_logger.py
@@ -7,6 +7,9 @@ from psycop.common.model_training_v2.metrics.base_metric import CalculatedMetric
 
 
 class BaselineLogger(Protocol):
+    def info(self, message: str) -> None:
+        ...
+
     def good(self, message: str) -> None:
         ...
 
@@ -26,6 +29,9 @@ class BaselineLogger(Protocol):
 class TerminalLogger(BaselineLogger):
     def __init__(self) -> None:
         self._l = wasabi.Printer(timestamp=True)
+
+    def info(self, message: str) -> None:
+        self._l.info(message)
 
     def good(self, message: str) -> None:
         self._l.good(message)
