ca60ecdcaa3134442e4fa346f88d0b59a2663e24
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-07 11:55:03 +0000

    style: auto-fixes from pre-commit

--- psycop/common/model_training_v2/test_pipeline.py
+++ psycop/common/model_training_v2/test_pipeline.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-import polars as pl
-
 from psycop.common.model_training_v2.classifier_pipelines.binary_classification_pipeline import (
     BinaryClassificationPipeline,
 )
@@ -9,7 +7,6 @@ from psycop.common.model_training_v2.classifier_pipelines.estimator_steps.xgboos
     xgboost_classifier_step,
 )
 from psycop.common.model_training_v2.loggers.base_logger import (
-    BaselineLogger,
     TerminalLogger,
 )
 from psycop.common.model_training_v2.metrics.binary_metrics.binary_auroc import (
@@ -32,17 +29,19 @@ from psycop.common.test_utils.str_to_df import str_to_pl_df
 
 
 def test_v2_train_model_pipeline():
-    training_data = str_to_pl_df("""pred_time_uuid,pred_1,pred_age,outcome
+    training_data = str_to_pl_df(
+        """pred_time_uuid,pred_1,pred_age,outcome
                                      1,1,1,1
                                      2,1,1,99
                                      3,1,1,99
                                      4,0,0,99
                                      5,0,0,99
                                      6,0,0,99
-                                     """)
+                                     """,
+    )
     logger = TerminalLogger()
-    
-    cfg = BaselineSchema(
+
+    BaselineSchema(
         experiment_path=Path(""),
         logger=logger,
         training_method=SplitTrainer(
@@ -50,8 +49,13 @@ def test_v2_train_model_pipeline():
             training_outcome_col_name="outcome",
             validation_data=training_data,
             validation_outcome_col_name="outcome",
-            preprocessing_pipeline=BaselinePreprocessingPipeline([AgeFilter(min_age=4, max_age=99, age_col_name="pred_age")]),
-            problem_type=BinaryClassification(pipe=BinaryClassificationPipeline(steps=[xgboost_classifier_step()]), main_metric=BinaryAUROC()),
+            preprocessing_pipeline=BaselinePreprocessingPipeline(
+                [AgeFilter(min_age=4, max_age=99, age_col_name="pred_age")],
+            ),
+            problem_type=BinaryClassification(
+                pipe=BinaryClassificationPipeline(steps=[xgboost_classifier_step()]),
+                main_metric=BinaryAUROC(),
+            ),
             logger=logger,
         ),
     )
