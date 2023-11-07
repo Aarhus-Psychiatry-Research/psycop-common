ebbd5930075599512bf128acf85d805380eb2337
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-07 10:07:38 +0000

    refactor: split out splittrainer

--- psycop/common/model_training_v2/training_method/split_trainer.py
+++ psycop/common/model_training_v2/training_method/split_trainer.py
@@ -35,18 +35,20 @@ class SplitTrainer(TrainingMethod):
         training_data_preprocessed = self.preprocessing_pipeline.apply(
             data=self.training_data,
         )
-        self.preprocessing_pipeline.apply(
+        validation_data = self.preprocessing_pipeline.apply(
             data=self.validation_data,
         )
+
         self.problem_type.train(
             x=training_data_preprocessed.drop(self.training_outcome_col_name),
             y=training_data_preprocessed.select(self.training_outcome_col_name),
         )
+
         result = self.problem_type.evaluate(
-            x=self.validation_data.drop(self.validation_outcome_col_name),
-            y=self.validation_data.select(self.validation_outcome_col_name),
+            x=validation_data.drop(self.validation_outcome_col_name),
+            y=validation_data.select(self.validation_outcome_col_name),
         )
 
-        self.logger.log_metric(name="metric", value=result.main_metric.value)
+        self.logger.log_metric(result.metric)
 
         return result
