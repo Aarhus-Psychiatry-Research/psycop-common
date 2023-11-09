a1fdc85c0bac6cbd16a2ba025cab3d2427fb5686
Jakob Grøhn <bokajgd@gmail.com>
2023-11-09 17:40:55 +0100

    test: filter cols by lookbehind combination filter

--- psycop/common/model_training_v2/trainer/preprocessing/steps/test_v2_filters.py
+++ psycop/common/model_training_v2/trainer/preprocessing/steps/test_v2_filters.py
@@ -1,7 +1,9 @@
 import pytest
 
+from psycop.common.model_training_v2.loggers.base_logger import TerminalLogger
 from psycop.common.model_training_v2.trainer.preprocessing.steps.filters import (
     AgeFilter,
+    LookbehindCombinationFilter,
 )
 from psycop.common.test_utils.str_to_df import str_to_pl_df
 
@@ -17,13 +19,33 @@ from psycop.common.test_utils.str_to_df import str_to_pl_df
 )
 def test_age_filter(min_age: int, max_age: int, n_remaining: int):
     df = str_to_pl_df(
-        """age,
-    1,
-    2,
-    3,
+        """age,pred_age,
+    1, 2,
+    2, 3,
+    3, 4,
     """,
     )
 
     result = AgeFilter(min_age=min_age, max_age=max_age, age_col_name="age").apply(df)
 
     assert len(result) == n_remaining
+
+
+def test_lookbehind_combination_filter():
+    df = str_to_pl_df(
+        """pred_age,pred_age_within_2_days,pred_age_within_3_days,pred_diagnosis_within_4_days
+        3,4,3,2
+        3,4,3,3
+        4,3,4,1
+        """,
+    )
+
+    logger = TerminalLogger()
+
+    result = LookbehindCombinationFilter(
+        lookbehind_combination={2, 3},
+        pred_col_prefix="pred_",
+        logger=logger,
+    ).apply(df)
+
+    assert len(result.columns) == 3
