54c75cf867b59332bef77e13fa4f48a8245be3d0
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-10 14:20:22 +0000

    misc.

--- psycop/common/model_training_v2/hyperparameter_suggester/suggesters/test_suggesters.py
+++ psycop/common/model_training_v2/hyperparameter_suggester/suggesters/test_suggesters.py
@@ -10,6 +10,7 @@ from psycop.common.model_training_v2.config.populate_registry import (
 from psycop.common.model_training_v2.hyperparameter_suggester.suggesters.logistic_regression_suggester import (
     FloatSpace,
     LogisticRegressionSuggester,
+    Suggester,
 )
 
 
@@ -17,16 +18,25 @@ def float_space_for_test() -> FloatSpace:
     return FloatSpace(low=0, high=1, logarithmic=False)
 
 
-def test_logistic_regression_suggester():
+def suggester_tester(suggester: Suggester) -> dict[str, Any]:
     sampler = optuna.samplers.RandomSampler()
+
     with StorageSupplier("inmemory") as storage:
         study = optuna.create_study(storage=storage, sampler=sampler)
         trial = optuna.Trial(study, study._storage.create_new_trial(study._study_id))  # type: ignore
-        result = LogisticRegressionSuggester(
-            C=float_space_for_test(), l1_ratio=float_space_for_test()
-        ).suggest_hyperparameters(trial=trial)
+        result = suggester.suggest_hyperparameters(trial=trial)
 
         populate_baseline_registry()
         cfg = BaselineRegistry.resolve(result)
 
-        # TODO: Refactor this shit
+    return cfg
+
+
+def test_logistic_regression_suggester():
+    suggester_tester(
+        suggester=LogisticRegressionSuggester(
+            C=float_space_for_test(), l1_ratio=float_space_for_test()
+        )
+    )
+
+    # XXX: Ensure this tests more than resolution. E.g. that the return value is meaningful.
