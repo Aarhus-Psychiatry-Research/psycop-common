2caf00dbe9e623019d2b68305fc3078e7d876283
sarakolding <sarakolding@live.dk>
2023-11-13 10:11:01 +0000

    stash

--- psycop/common/model_training_v2/trainer/cross_validator.py
+++ psycop/common/model_training_v2/trainer/cross_validator.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.model_selection import StratifiedGroupKFold
 
 from psycop.common.model_training_v2.config.baseline_registry import BaselineRegistry
@@ -20,7 +21,7 @@ class CrossValidatorTrainer(BaselineTrainer):
         training_data: BaselineDataLoader,  # make into list?
         training_outcome_col_name: str,
         preprocessing_pipeline: PreprocessingPipeline,
-        task: BaselineTask,
+        task: BaselineTask,  # make more generalisable?
         logger: BaselineLogger,
         n_splits: int = 5,
         group_col_name: str = "dw_ek_borger",
@@ -38,43 +39,10 @@ class CrossValidatorTrainer(BaselineTrainer):
             data=self.training_data,
         )
 
-        # do we need to convert to matrix? or would that be too slow?
-        folds = StratifiedGroupKFold(n_splits=self.n_splits).split(
-            X=training_data_preprocessed.drop(self.training_outcome_col_name),
-            y=training_data_preprocessed.select(self.training_outcome_col_name),
-            groups=training_data_preprocessed.select(self.group_col_name),
+        StratifiedGroupKFold(n_splits=self.n_splits).split(
+            X=training_data_preprocessed.drop(
+                self.training_outcome_col_name,
+            ).to_pandas(),
+            y=pd.DataFrame(training_data_preprocessed),
+            groups=training_data_preprocessed[self.group_col_name],
         )
-
-        for _i, (train_idxs, val_idxs) in enumerate(folds):
-            X_train, y_train = (
-                training_data_preprocessed.loc[train_idxs],
-                training_data_preprocessed.loc[train_idxs],
-            )
-            pipe.fit(X_train, y_train)
-
-            y_pred = pipe.predict_proba(X_train)[:, 1]
-
-            roc_auc_score(y_train, y_pred)
-
-            oof_y_pred = pipe.predict_proba(X.loc[val_idxs])[  # type: ignore
-                :,
-                1,
-            ]
-
-            roc_auc_score(y.loc[val_idxs], oof_y_pred)
-
-            train_df.loc[val_idxs, "oof_y_hat"] = oof_y_pred
-
-        self.task.train(
-            x=training_data_preprocessed.drop(self.training_outcome_col_name),
-            y=training_data_preprocessed.select(self.training_outcome_col_name),
-        )
-        # cross-validation
-        result = self.task.evaluate(
-            x=validation_data_preprocessed.drop(self.validation_outcome_col_name),
-            y=validation_data_preprocessed.select(self.validation_outcome_col_name),
-        )
-
-        self.logger.log_metric(result.metric)
-
-        return result
