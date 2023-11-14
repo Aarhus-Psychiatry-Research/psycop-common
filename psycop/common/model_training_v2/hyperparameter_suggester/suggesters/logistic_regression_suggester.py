97189e0cf4e2faf71666ee15d447e5bf3f95e1a5
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-14 09:05:48 +0000

    misc.

--- psycop/common/model_training_v2/hyperparameter_suggester/suggesters/logistic_regression_suggester.py
+++ psycop/common/model_training_v2/hyperparameter_suggester/suggesters/logistic_regression_suggester.py
@@ -42,6 +42,9 @@ class LogisticRegressionSuggester(Suggester):
     # TODO: #424 Refactor so suggesters are co-located with their corresponding model steps.
     # Perhaps that makes it hard to decorate, though?
 
+    # TODO: Can suggesters take a mapping for each argument? 
+    # E.g. C can be a mapping which must contain low, high and log? How do we type-hint that? A nameddict?
+
     def __init__(
         self,
         C_low: float,
