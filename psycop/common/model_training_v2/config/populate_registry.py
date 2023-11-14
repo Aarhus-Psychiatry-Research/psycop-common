e2f3e0e07f241a4020b5b4af1bc6bda15c9ca75a
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-14 08:29:16 +0000

    style: auto-fixes from pre-commit

--- psycop/common/model_training_v2/config/populate_registry.py
+++ psycop/common/model_training_v2/config/populate_registry.py
@@ -8,5 +8,5 @@ def populate_baseline_registry() -> None:
     from ..loggers.base_logger import TerminalLogger  # noqa
     from ..trainer.task.pipeline_constructor import pipeline_constructor  # noqa
     from ..trainer.task.estimator_steps.logistic_regression import (
-        logistic_regression_step, # noqa
-    )  
+        logistic_regression_step,  # noqa
+    )
