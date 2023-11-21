d3400503e430530d681053ee5eee3cd4d8a9c5b1
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-21 10:21:49 +0000

    deps: fix import

--- psycop/projects/cvd/model_training/populate_cvd_registry.py
+++ psycop/projects/cvd/model_training/populate_cvd_registry.py
@@ -8,7 +8,7 @@ def populate_with_cvd_registry() -> None:
     from psycop.common.model_training_v2.trainer.preprocessing.steps.column_filters import (
         RegexColumnBlacklist,
     )
-    from psycop.common.model_training_v2.trainer.preprocessing.steps.col_filters import (
+    from psycop.common.model_training_v2.trainer.preprocessing.steps.column_filters import (
         TemporalColumnFilter,
     )
     from psycop.projects.cvd.model_training.preprocessing.bool_to_int import BoolToInt
