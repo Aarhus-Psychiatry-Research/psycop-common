bac5d6f1b17c2989f24c2f4abdf5f587a6cc081d
Lasse <lasseh0310@gmail.com>
2023-11-30 07:38:24 +0000

    lint

--- psycop/projects/scz_bp/feature_generation/inspect_feature_sets.py
+++ psycop/projects/scz_bp/feature_generation/inspect_feature_sets.py
@@ -1,16 +1,17 @@
 import polars as pl
+
 from psycop.common.global_utils.paths import OVARTACI_SHARED_DIR
 
 
-def load_scz_bp_feature_set(feature_set_name: str):
-    dir = (
+def load_scz_bp_feature_set(feature_set_name: str) -> pl.DataFrame:
+    feature_set_dir = (
         OVARTACI_SHARED_DIR
         / "scz_bp"
         / "initial_feature_set"
         / "flattened_datasets"
         / feature_set_name
     )
-    return pl.read_parquet(dir / "train.parquet")
+    return pl.read_parquet(feature_set_dir / "train.parquet")
 
 
 if __name__ == "__main__":
