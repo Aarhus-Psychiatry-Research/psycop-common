0b1d8f5140c8d6ccb2066ec0226770c92a9dc201
HLasse <lasseh0310@gmail.com>
2023-12-01 13:34:35 +0100

    plan for scz-bp-eval

---
+++ psycop/projects/scz_bp/evaluation/model_selection/scz_bp_evaluation.py
@@ -0,0 +1,23 @@
+# Set path to BaselineSchema for the run
+
+## Load dataset with predictions after training
+## Load validation dataset
+## 1:1 join metadata cols to predictions
+
+# Plot performance by
+
+## Age
+## Sex
+## Calendar time
+## Diagnosis type (scz or bp)
+## Time to event
+## Time from first visit
+
+# Table of performance (sens, spec, ppv, f1) by threshold
+
+## Confusion matrix at specified threshold
+
+# Plot feature importance
+
+
+
