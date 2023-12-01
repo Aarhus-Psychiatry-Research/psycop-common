c3d3dda525166724d466d22b290f9b7c7e3ca8c7
Martin Bernstorff <martinbernstorff@gmail.com>
2023-12-01 08:51:30 +0000

    misc.

--- automation/windows.py
+++ automation/windows.py
@@ -8,15 +8,14 @@ from typing import Optional
 from invoke import Context, Result, task
 
 NOT_WINDOWS = platform.system() != "Windows"
-from .logger import msg_type
 
 
 def on_ovartaci() -> bool:
     import platform
 
     if platform.node() == "RMAPPS1279":
-        print(f"\n{msg_type.GOOD} On Ovartaci")
+        print("On Ovartaci")
         return True
 
-    print(f"\n{msg_type.GOOD} Not on Ovartaci")
+    print("Not on Ovartaci")
     return False
