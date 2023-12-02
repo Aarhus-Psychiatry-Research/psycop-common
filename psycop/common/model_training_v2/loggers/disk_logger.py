37266946711e71568bb23f9cbf4229bb5b80e54e
Lasse <lasseh0310@gmail.com>
2023-12-02 10:24:55 +0000

    add test

--- psycop/common/model_training_v2/loggers/disk_logger.py
+++ psycop/common/model_training_v2/loggers/disk_logger.py
@@ -9,10 +9,10 @@ from psycop.common.model_training_v2.trainer.task.base_metric import CalculatedM
 
 
 @BaselineRegistry.loggers.register("disk_logger")
-class TerminalLogger(BaselineLogger):
+class DiskLogger(BaselineLogger):
     def __init__(self, experiment_path: str) -> None:
         self.experiment_path = Path(experiment_path)
-        self.logger_path = self.experiment_path / "logs.log"
+        self.log_path = self.experiment_path / "logs.log"
         self.cfg_log_path = self.experiment_path / "config.json"
 
         self._l = self._setup_logging_module()
@@ -38,14 +38,21 @@ class TerminalLogger(BaselineLogger):
             json.dump(config, f)
 
     def _setup_logging_module(self) -> logging.Logger:
-        logging.basicConfig(
-            filename=self.logger_path,
-            level=logging.DEBUG,
-            encoding="utf-8",
-            format="%(asctime)s %(levelname)s %(name)s %(message)s",
+        logger = logging.getLogger("DiskLogger")
+        logger.setLevel(logging.DEBUG)
+
+        self.log_path.parent.mkdir(exist_ok=True, parents=True)
+
+        # Create file handler
+        fh = logging.FileHandler(self.log_path, mode="a", encoding="utf-8")
+        formatter = logging.Formatter(
+            "%(asctime)s %(levelname)s %(name)s %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
         )
-        self.logger_path.mkdir(exist_ok=True, parents=True)
+        fh.setFormatter(formatter)
 
-        logging.info("Disk logger initialized")
-        return logging.getLogger()
+        # Add the handler to the logger
+        logger.addHandler(fh)
+
+        logger.info("Disk logger initialized")
+        return logger
