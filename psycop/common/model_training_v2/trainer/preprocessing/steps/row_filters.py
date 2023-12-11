bcc1edfadf0c009826cc2a848e6c95e2cdb55fc9
sarakolding <sarakolding@live.dk>
2023-12-11 12:34:18 +0000

    first stab

--- psycop/common/model_training_v2/trainer/preprocessing/steps/row_filters.py
+++ psycop/common/model_training_v2/trainer/preprocessing/steps/row_filters.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+from typing import Literal
+
 import polars as pl
 
 from psycop.common.model_training_v2.config.baseline_registry import BaselineRegistry
@@ -16,3 +19,29 @@ class AgeFilter(PresplitStep):
 
     def apply(self, input_df: PolarsFrame_T0) -> PolarsFrame_T0:
         return input_df.filter((self.age >= self.min_age) & (self.age <= self.max_age))
+
+
+@BaselineRegistry.preprocessing.register("window_filter")
+class WindowFilter(PresplitStep):
+    def __init__(self, n_days: int, direction: Literal["ahead", "behind"], timestamp_col_name: str):
+        self.n_days = timedelta(n_days)
+        self.direction = direction
+        self.timestamp_col_name = timestamp_col_name
+
+    def apply(self, input_df: PolarsFrame_T0) -> PolarsFrame_T0:
+
+        if self.direction == "ahead":
+            if isinstance(input_df, pl.DataFrame):
+                max_datetime = input_df.select(pl.col(self.timestamp_col_name)).max().item() - self.n_days
+            else:
+                max_datetime = input_df.select(pl.col(self.timestamp_col_name)).max().collect().item() - self.n_days
+            input_df = input_df.filter(pl.col(self.timestamp_col_name) < max_datetime)
+
+        elif self.direction == "behind":
+            if isinstance(input_df, pl.DataFrame):
+                min_datetime = input_df.select(pl.col(self.timestamp_col_name)).min().item() + self.n_days
+            else:
+                min_datetime = input_df.select(pl.col(self.timestamp_col_name)).min().collect().item() + self.n_days
+            input_df = input_df.filter(pl.col(self.timestamp_col_name) > min_datetime)
+
+        return input_df
