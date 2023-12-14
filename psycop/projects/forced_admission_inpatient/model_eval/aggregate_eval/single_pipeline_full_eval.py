db8f828fa7eb3b7061b5b7dd71487e7779c71bbb
bokajgd <bokajgd@gmail.com>
2023-12-14 14:34:56 +0100

    fix: add arg to aggr eval

--- psycop/projects/forced_admission_inpatient/model_eval/aggregate_eval/single_pipeline_full_eval.py
+++ psycop/projects/forced_admission_inpatient/model_eval/aggregate_eval/single_pipeline_full_eval.py
@@ -87,7 +87,10 @@ def fa_inpatient_main_manuscript_eval(
         run=pipeline,
         lookaheads_for_performance_eval=[30, 90, 180, 360],
     )
-    generate_feature_importance_table(pipeline_run=pipeline)
+    generate_feature_importance_table(
+        pipeline_run=pipeline,
+        vocab_filename="vocab_tfidf_psycop_train_all_sfis_preprocessed_sfi_type_all_sfis_ngram_range_12_max_df_09_min_df_2_max_features_750.parquet",
+    )
 
     return fa_inpatient_create_markdown_artifacts(pipeline=pipeline)
 
