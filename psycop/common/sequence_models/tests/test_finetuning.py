96393df0b66adfc312151a497566d715873f2741
Martin Bernstorff <martinbernstorff@gmail.com>
2023-12-14 13:01:13 +0000

    tests: skip matmul primitive

--- psycop/common/sequence_models/tests/test_finetuning.py
+++ psycop/common/sequence_models/tests/test_finetuning.py
@@ -1,12 +1,16 @@
 import datetime as dt
 from pathlib import Path
 
+import pytest
+
 from psycop.common.data_structures.patient import Patient, PatientSlice
 from psycop.common.data_structures.prediction_time import PredictionTime
 from psycop.common.data_structures.temporal_event import TemporalEvent
 from psycop.common.sequence_models.dataset import PatientSlicesWithLabels
 from psycop.common.sequence_models.registry import Registry
 from psycop.common.sequence_models.train import train
+
+from .test_encoder_for_clf import arm_within_docker
 
 
 @Registry.datasets.register("fake_patient_slices_with_labels")
@@ -40,6 +44,10 @@ def create_fake_patient_slices_with_labels() -> PatientSlicesWithLabels:
     return patient_slices
 
 
+@pytest.mark.skipif(
+    arm_within_docker(),
+    reason="Skipping test on ARM within docker. Some tests fail, unknown reason, see https://github.com/Aarhus-Psychiatry-Research/psycop-common/issues/348",
+)
 def test_finetune():
     config_path = Path(__file__).parent / "test_finetuning.cfg"
     train(config_path)
