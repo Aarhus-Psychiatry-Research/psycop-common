d2931a7cf792cc8a15028e29b59b5684337c730c
Martin Bernstorff <martinbernstorff@gmail.com>
2023-12-15 12:25:23 +0000

    misc. refactor

--- psycop/common/sequence_models/train.py
+++ psycop/common/sequence_models/train.py
@@ -45,14 +45,14 @@ def train(config_path: Path | None = None) -> None:
         config_path: path to config file if none loads default
     """
     config_dict = load_config(config_path)
-    config = parse_config(config_dict)
+    cfg = parse_config(config_dict)
 
     # Setup the logger and pass it to the TrainingConfig
-    logger_factory = config.logger_factory
+    logger_factory = cfg.logger_factory
     if logger_factory is not None:
-        logger = config.logger_factory.get_logger()  # type: ignore
-        config.training.trainer.logger = logger
-        config.training.trainer.Config.allow_mutation = False
+        logger = cfg.logger_factory.get_logger()  # type: ignore
+        cfg.training.trainer.logger = logger
+        cfg.training.trainer.Config.allow_mutation = False
 
         # update config
         log.info("Updating Config")
@@ -60,39 +60,37 @@ def train(config_path: Path | None = None) -> None:
         logger.log_hyperparams(flat_config)
 
     # Load and filter dataset
-    filter_fn = config.model_and_dataset.model.filter_and_reformat
+    filter_fn = cfg.model_and_dataset.model.filter_and_reformat
 
     log.info("Preparing train")
-    training_dataset = config.model_and_dataset.training_dataset.get_patient_slices()
+    training_dataset = cfg.model_and_dataset.training_dataset.get_patient_slices()
     training_dataset.filter_patients(filter_fn)
 
     log.info("Preparing validation")
-    validation_dataset = (
-        config.model_and_dataset.validation_dataset.get_patient_slices()
-    )
+    validation_dataset = cfg.model_and_dataset.validation_dataset.get_patient_slices()
     validation_dataset.filter_patients(filter_fn)
 
     log.info("Creating dataloaders")
-    model = config.model_and_dataset.model
+    model = cfg.model_and_dataset.model
     train_loader = DataLoader(
         training_dataset,
-        batch_size=config.training.batch_size,
+        batch_size=cfg.training.batch_size,
         shuffle=True,
         collate_fn=model.collate_fn,
-        num_workers=config.training.num_workers_for_dataloader,
+        num_workers=cfg.training.num_workers_for_dataloader,
         persistent_workers=True,
     )
     val_loader = DataLoader(
         validation_dataset,
-        batch_size=config.training.batch_size,
+        batch_size=cfg.training.batch_size,
         shuffle=False,
         collate_fn=model.collate_fn,
-        num_workers=config.training.num_workers_for_dataloader,
+        num_workers=cfg.training.num_workers_for_dataloader,
         persistent_workers=True,
     )
 
     log.info("Initalizing trainer")
-    trainer = pl.Trainer(**training_cfg.trainer.to_dict())
+    trainer = pl.Trainer(**cfg.training.trainer.to_dict())
 
     log.info("Starting training")
     torch.set_float32_matmul_precision("medium")
