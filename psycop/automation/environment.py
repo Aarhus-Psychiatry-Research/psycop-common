4967566f90e227507b398272e473554405a3b967
Martin Bernstorff <ryqiem@gmail.com>
2023-12-15 09:44:11 +0100

    fix: pytorch cuda test

--- psycop/automation/environment.py
+++ psycop/automation/environment.py
@@ -1,5 +1,7 @@
 import platform
 
+from invoke import Context, task
+
 NOT_WINDOWS = platform.system() != "Windows"
 
 
@@ -12,3 +14,12 @@ def on_ovartaci() -> bool:
 
     print("Not on Ovartaci")
     return False
+
+
+def test_pytorch_cuda(c: Context):
+    python_command = "import torch; t=torch.tensor(1); t.to(torch.device('cuda'))"
+    c.run(
+        f'python -c "{python_command}"',
+        pty=NOT_WINDOWS,
+    )
+    print("Pytorch CUDA works!")
--- tasks.py
+++ tasks.py
@@ -21,7 +21,7 @@ from pathlib import Path
 
 from invoke import Context, Result, task
 
-from psycop.automation.environment import NOT_WINDOWS, on_ovartaci
+from psycop.automation.environment import NOT_WINDOWS, test_pytorch_cuda, on_ovartaci
 from psycop.automation.git import (
     add_and_commit,
     filetype_modified_since_main,
@@ -43,10 +43,8 @@ def install_requirements(c: Context):
             "conda install --force-reinstall pytorch=2.1.0 pytorch-cuda=12.1 -c https://exrhel0371.it.rm.dk/api/repo/pytorch -c https://exrhel0371.it.rm.dk/api/repo/nvidia -c https://exrhel0371.it.rm.dk/api/repo/anaconda --override-channels --insecure -y",
             pty=NOT_WINDOWS,
         )
+        test_pytorch_cuda(c)
 
-        # Test that cuda works
-    c.run(r"python -c \"import torch; t=torch.tensor(1); t.to(torch.device('cuda'))\"")
-    print(f"{msg_type.GOOD} Pytorch CUDA works!")
     print(f"{msg_type.GOOD} Newest version of all requirements installed!")
 
 
