5a5b25161649a803e3433804f807d441719c4895
Lasse <lasseh0310@gmail.com>
2024-01-04 09:09:57 +0100

    lint and skip tests for files requiring synthcity

--- psycop/projects/scz_bp/model_training/estimator_steps/test_synth_data_augmentation.py
+++ psycop/projects/scz_bp/model_training/estimator_steps/test_synth_data_augmentation.py
@@ -6,10 +6,12 @@ from imblearn.pipeline import Pipeline
 from sklearn.datasets import make_classification
 from xgboost import XGBClassifier
 
+if sys.modules.get("synthcity") is None:
+    pytest.skip("requires the synthcity library", allow_module_level=True)
+
 from psycop.projects.scz_bp.model_training.estimator_steps.synth_data_augmentation import (
     SyntheticDataAugmentation,
 )
-
 
 
 @pytest.fixture()
@@ -24,18 +26,14 @@ def sample_data() -> tuple[pd.DataFrame, pd.Series]:  # type: ignore
     )
     return pd.DataFrame(X), pd.Series(y, name="target")
 
-@pytest.mark.skipif(
-    "synthcity" not in sys.modules, reason="requires the synthcity library"
-)
+
 def test_initialization():
     augmenter = SyntheticDataAugmentation("ddpm", {"n_iter": 10}, prop_augmented=0.5)
     assert augmenter.model_name == "ddpm"
     assert "n_iter" in augmenter.model_params
     assert augmenter.prop_augmented == 0.5
 
-@pytest.mark.skipif(
-    "synthcity" not in sys.modules, reason="requires the synthcity library"
-)
+
 def test_prop_augmented(sample_data: tuple[pd.DataFrame, pd.Series]):  # type: ignore
     X, y = sample_data
     aug = SyntheticDataAugmentation(
@@ -47,9 +45,7 @@ def test_prop_augmented(sample_data: tuple[pd.DataFrame, pd.Series]):  # type: i
     assert len(X_res) == int(1.5 * len(X))  # check if 50% more samples are added
     assert len(y_res) == int(1.5 * len(y))
 
-@pytest.mark.skipif(
-    "synthcity" not in sys.modules, reason="requires the synthcity library"
-)
+
 def test_minority_strategy(sample_data: tuple[pd.DataFrame, pd.Series]):  # type: ignore
     X, y = sample_data
     # should only add cases of the minority (assumed to be 1) class
@@ -65,9 +61,7 @@ def test_minority_strategy(sample_data: tuple[pd.DataFrame, pd.Series]):  # type
 
     assert n_minority_in_X_target_aug == n_minority_in_X + 0.5 * X.shape[0]
 
-@pytest.mark.skipif(
-    "synthcity" not in sys.modules, reason="requires the synthcity library"
-)
+
 def test_synth_data_augmentation_in_pipeline(
     sample_data: tuple[pd.DataFrame, pd.Series],  # type: ignore
 ):
