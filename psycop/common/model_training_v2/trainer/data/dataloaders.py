3ead1c6e90731c8c0fc5e8a0acbafb3ced6d89d0
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-09 09:04:13 +0000

    style: auto-fixes from pre-commit

--- psycop/common/model_training_v2/trainer/data/dataloaders.py
+++ psycop/common/model_training_v2/trainer/data/dataloaders.py
@@ -2,7 +2,6 @@ from collections.abc import Sequence
 from pathlib import Path
 
 import polars as pl
-from iterpy import Iter
 
 from psycop.common.model_training_v2.config.baseline_registry import BaselineRegistry
 from psycop.common.model_training_v2.trainer.base_dataloader import BaselineDataLoader
