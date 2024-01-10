00cbb7dc9fe7f4339c1d7d920668d31878def6de
Lasse <lasseh0310@gmail.com>
2024-01-10 08:37:08 +0000

    clean up

--- psycop/projects/scz_bp/model_training/scz_bp_registry.py
+++
@@ -1,5 +0,0 @@
-from psycop.common.model_training_v2.config.baseline_registry import BaselineRegistry
-
-
-class SczBpRegistry(BaselineRegistry):
-    """Registry for scz_bp project which includes project-specific steps"""
