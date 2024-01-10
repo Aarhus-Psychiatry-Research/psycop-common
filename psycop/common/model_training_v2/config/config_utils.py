a821915b3b7325e9a5c1de722bf45bc80ca70046
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-10 09:34:53 +0000

    feat: fill defaults from function signatures into .cfg (issue #426)
    Fixes #426

--- psycop/common/model_training_v2/config/config_utils.py
+++ psycop/common/model_training_v2/config/config_utils.py
@@ -14,15 +14,21 @@ from psycop.common.model_training_v2.config.populate_registry import (
 populate_baseline_registry()
 
 
+def resolve_and_fill_config(config_path: Path) -> dict[str, Any]:
+    cfg = Config().from_disk(config_path)
+
+    # Fill with defaults and write to disk if relevant
+    filled = BaselineRegistry.fill(cfg, validate=False)
+    resolved = BaselineRegistry.resolve(filled)
+
+    # Writing to disk happens after resolution, to ensure that the
+    # config is valid
+    if cfg != filled:
+        filled.to_disk(config_path)
+    return resolved
+
+
 def load_baseline_config(config_path: Path) -> BaselineSchema:
     """Loads the baseline config from disk and resolves it."""
-    cfg = Config().from_disk(config_path)
-    resolved = BaselineRegistry.resolve(cfg)
+    resolved = resolve_and_fill_config(config_path)
     return BaselineSchema(**resolved)
-
-
-def load_hyperparam_config(config_path: Path) -> dict[str, Any]:
-    """Loads the baseline config from disk and resolves it."""
-    cfg = Config().from_disk(config_path)
-    resolved = BaselineRegistry.resolve(cfg)
-    return resolved
