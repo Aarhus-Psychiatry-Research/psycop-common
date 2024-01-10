c264b0be896b927b4e15018e22b0418efe0a179b
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-10 13:34:03 +0000

    misc.

--- psycop/common/feature_generation/sequences/prediction_time_collater.py
+++ psycop/common/feature_generation/sequences/prediction_time_collater.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from typing import Protocol, runtime_checkable
 
 from ...cohort_definition import CohortDefiner
+from ...model_training_v2.trainer.data.data_filters.geography import RegionalFilter
+from ...model_training_v2.trainer.preprocessing.step import PresplitStep
 from ...sequence_models.dataset import PredictionTimeDataset
 from ...sequence_models.registry import Registry
 from .patient_loader import PatientLoader
@@ -20,6 +22,7 @@ class BasePredictionTimeCollater(Protocol):
 class PredictionTimeCollater(BasePredictionTimeCollater):
     patient_loader: PatientLoader
     cohort_definer: CohortDefiner
+    split_filter: PresplitStep
     lookbehind_days: int
     lookahead_days: int
 
@@ -29,6 +32,7 @@ class PredictionTimeCollater(BasePredictionTimeCollater):
         prediction_times = PredictionTimesFromCohort(
             cohort_definer=self.cohort_definer,
             patients=self.patient_loader.get_patients(),
+            split_filter=self.split_filter,
         ).create_prediction_times(
             lookbehind=dt.timedelta(days=self.lookbehind_days),
             lookahead=dt.timedelta(days=self.lookahead_days),
