bdcbecdd2830e3ce713b2bcc9abcd6d47322062f
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-10 12:43:34 +0000

    misc.

--- psycop/common/feature_generation/sequences/prediction_times_from_cohort.py
+++ psycop/common/feature_generation/sequences/prediction_times_from_cohort.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
     prediction_times = PredictionTimesFromCohort(
         cohort_definer=T2DCohortDefiner(),
         patients=patients,
-        split_filter=RegionalFilter(),
+        split_filter=RegionalFilter(),  # TODO: Awaiting #649
     ).create_prediction_times(
         lookbehind=dt.timedelta(days=365),
         lookahead=dt.timedelta(days=365),
