cce16e2504c5d88f8f361fbdbbd638a58ecf0229
Lasse <lasseh0310@gmail.com>
2024-01-16 14:35:22 +0000

    misc

--- psycop/common/model_training_v2/config/baseline_registry.py
+++ psycop/common/model_training_v2/config/baseline_registry.py
@@ -29,7 +29,7 @@ class BaselineRegistry(RegistryWithDict):
     metrics = catalogue.create("psycop", "metrics")
 
     # TODO: https://github.com/Aarhus-Psychiatry-Research/psycop-common/issues/440 Move the populate registry function to the baseline registry, to make it easier to add new imports to the function
-    suggesters = catalogue.create("psycop", "suggester")
+    suggesters = catalogue.create("psycop", "suggesters")
 
     def to_dict(self) -> dict[str, catalogue.Registry]:
         return {
--- psycop/common/model_training_v2/hyperparameter_suggester/suggesters/test_suggesters.py
+++ psycop/common/model_training_v2/hyperparameter_suggester/suggesters/test_suggesters.py
@@ -11,7 +11,7 @@ from psycop.common.model_training_v2.config.populate_registry import (
 from psycop.common.model_training_v2.hyperparameter_suggester.suggesters.base_suggester import (
     Suggester,
 )
-from psycop.common.model_training_v2.hyperparameter_suggester.suggesters.logistic_regression_suggester import (
+from psycop.common.model_training_v2.hyperparameter_suggester.suggesters.suggester_spaces import (
     FloatSpace,
 )
 
