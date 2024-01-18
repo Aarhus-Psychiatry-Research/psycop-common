3d53adf7773725f3f4dabaa13e84d9e6de59f1a2
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-18 10:49:51 +0000

    misc.

--- psycop/common/model_training_v2/trainer/task/multilabel_classification/multilabel_classification_pipeline.py
+++ psycop/common/model_training_v2/trainer/task/multilabel_classification/multilabel_classification_pipeline.py
@@ -6,8 +6,10 @@ from psycop.common.model_training_v2.trainer.task.model_step import (
 )
 from psycop.common.types.polarsframe import PolarsFrame
 
+from ..base_pipeline import BasePipeline
 
-class MultilabelClassificationPipeline(Protocol):
+
+class MultilabelClassificationPipeline(BasePipeline, Protocol):
     def __init__(self, steps: Sequence[ModelStep]) -> None:
         ...
 
