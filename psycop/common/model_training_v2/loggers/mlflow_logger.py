b4bcda7bb488f99036f5685a0bd1be31f6f1c0ce
HLasse <lasseh0310@gmail.com>
2024-01-24 09:30:58 +0100

    misc

--- psycop/common/model_training_v2/loggers/mlflow_logger.py
+++ psycop/common/model_training_v2/loggers/mlflow_logger.py
@@ -26,18 +26,15 @@ class MLFlowLogger(BaselineLogger):
         self,
         experiment_name: str,
         tracking_uri: str = "http://exrhel0371.it.rm.dk:5050",
-        start_on_init: bool = True,
     ) -> None:
         mlflow.set_tracking_uri(tracking_uri)
 
         # Start a new run. End a run if it already exists within the process.
-        if start_on_init:
-            with contextlib.suppress(mlflow.MlflowException):
-                mlflow.end_run()
-            mlflow.start_run()
-
+        if mlflow.active_run() is not None:
+            mlflow.end_run()
         self.mlflow_experiment = mlflow.set_experiment(experiment_name=experiment_name)
         self.experiment_id = self.mlflow_experiment.experiment_id
+
         self._log_str = ""
 
     def _append_log_str(self, prefix: str, message: str):
