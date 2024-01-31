4df2dec3afb47b4b3ef363392a8f35b19a4684a8
Martin Bernstorff <ryqiem@gmail.com>
2024-01-31 14:55:39 +0100

    feat: joblib based hyperparameter search

--- psycop/projects/cvd/model_training/hyperparam_tune_cvd_model.py
+++ psycop/projects/cvd/model_training/hyperparam_tune_cvd_model.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 
 from psycop.common.model_training_v2.config.populate_registry import populate_baseline_registry
-from psycop.projects.cvd.model_training.populate_cvd_registry import populate_with_cvd_registry
-
-from ....common.model_training_v2.hyperparameter_suggester.optuna_hyperparameter_search import (
+from psycop.common.model_training_v2.hyperparameter_suggester.optuna_hyperparameter_search import (
     OptunaHyperParameterOptimization,
 )
+from psycop.projects.cvd.model_training.populate_cvd_registry import populate_with_cvd_registry
 
 if __name__ == "__main__":
     populate_baseline_registry()
     populate_with_cvd_registry()
     OptunaHyperParameterOptimization().from_file(
-        (Path(__file__).parent / "cvd_baseline.cfg"), n_trials=1000, n_jobs=20, direction="minimize"
+        (Path(__file__).parent / "cvd_baseline.cfg"),
+        study_name="test_joblib",
+        n_trials=1000,
+        n_jobs=30,
+        direction="maximize",
+        catch=(Exception,),
     )
