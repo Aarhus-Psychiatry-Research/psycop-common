040a560db74c9dd13091cf564fcb18733bfc7ebf
sarakolding <sarakolding@live.dk>
2024-02-13 15:38:07 +0100

    misc

--- psycop/projects/restraint/training/populate_restraint_registry.py
+++ psycop/projects/restraint/training/populate_restraint_registry.py
@@ -12,6 +12,7 @@ def populate_with_restraint_registry() -> None:
     from psycop.common.model_training_v2.trainer.preprocessing.steps.cell_transformers import (
         BoolToInt,
     )
+    from psycop.common.model_training_v2.trainer.preprocessing.steps.row_filter_split import RegionalFilter
 
 
 populate_with_restraint_registry()
