671f876c20e260a13d77ac9bd68c825da478e18d
erikperfalk <erik.perfalk@gmail.com>
2024-02-22 10:41:36 +0100

    lint

--- psycop/projects/clozapine/feature_generation/cohort_definition/outcome_specification/text_clozapine_outcome.py
+++ psycop/projects/clozapine/feature_generation/cohort_definition/outcome_specification/text_clozapine_outcome.py
@@ -114,7 +114,7 @@ def extract_clozapine_from_free_text() -> pl.DataFrame:
                 row_df = schizophrenia_text_medicin_df[index, :]
 
                 row_df = row_df.with_columns(matched_word=pl.lit(value))
-               
+
                 row_df = row_df.with_columns(fuzz_ratio=pl.lit(int(similarity_score)))  # pyright: reportUnboundVariable=none
 
                 # Concatenate the current row to the filtered DataFrame
