19541587c9af097aa16d8b92ef5436d7a7f7b68a
Lasse <lasseh0310@gmail.com>
2024-02-28 09:04:22 +0100

    hparam config

---
+++ psycop/projects/scz_bp/model_training/hparam_search_scz_bp.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from psycop.common.model_training_v2.config.populate_registry import populate_baseline_registry
+from psycop.common.model_training_v2.hyperparameter_suggester.optuna_hyperparameter_search import (
+    OptunaHyperParameterOptimization,
+)
+
+if __name__ == "__main__":
+    populate_baseline_registry()
+    OptunaHyperParameterOptimization().from_file(
+        (Path(__file__).parent / "config" / "scz_bp_hparam.cfg"),
+        study_name="sczbp_3_year_lookahead",
+        n_trials=500,
+        n_jobs=30,
+        direction="maximize",
+        catch=(Exception,),
+    )
