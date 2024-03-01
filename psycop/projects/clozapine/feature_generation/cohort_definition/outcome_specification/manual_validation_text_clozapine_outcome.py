3e43bff6818c52008899207c0fc5b00232b511b8
erikperfalk <erik.perfalk@gmail.com>
2024-03-01 15:00:27 +0100

    added manual text outcome validation function

---
+++ psycop/projects/clozapine/feature_generation/cohort_definition/outcome_specification/manual_validation_text_clozapine_outcome.py
@@ -0,0 +1,216 @@
+from pathlib import Path
+
+import pandas as pd
+
+from psycop.common.feature_generation.utils import write_df_to_file
+
+
+def process_df_from_disk():
+    raw_text_df = pd.read_parquet(
+        "E:/shared_resources/clozapine/text_outcome/raw_text_outcome_clozapine_v2.parquet"
+    )
+
+    validated_text_outcome_clozapine = pd.read_parquet(
+        "E:/shared_resources/clozapine/text_outcome/validated_text_outcome_clozapine_v17.parquet"
+    )
+
+    unsure_text_outcome_clozapine = pd.read_parquet(
+        "E:/shared_resources/clozapine/text_outcome/unsure_text_outcome_clozapine_v13.parquet"
+    )
+
+    rows_no_text_on_matched_word = pd.read_parquet(
+        "E:/shared_resources/clozapine/text_outcome/rows_no_text_on_matched_word_v1.parquet"
+    )
+
+    no_text_outcome_df = pd.read_parquet(
+        "E:/shared_resources/clozapine/text_outcome/no_text_outcome_clozapine_v15.parquet"
+    )
+
+    # remove rows from already checked cpr/rows -
+    #  Condition for 'rows_no_text_on_matched_word' - specific rows
+    condition_no_text_matched_words = (
+        raw_text_df[["dw_ek_borger", "timestamp"]]
+        .isin(rows_no_text_on_matched_word[["dw_ek_borger", "timestamp"]].to_dict(orient="list"))
+        .all(axis=1)
+    )
+
+    condition_no_text = (
+        raw_text_df[["dw_ek_borger", "timestamp"]]
+        .isin(no_text_outcome_df[["dw_ek_borger", "timestamp"]].to_dict(orient="list"))
+        .all(axis=1)
+    )
+
+    # Condition for 'unsure_text_df' - specific rows
+    condition_unsure = (
+        raw_text_df[["dw_ek_borger", "timestamp"]]
+        .isin(unsure_text_outcome_clozapine[["dw_ek_borger", "timestamp"]].to_dict(orient="list"))
+        .all(axis=1)
+    )
+
+    # condition for "validated text outcome clozapine"- remove all rows for specific dw_ek_borger
+    condition_validated_text = (
+        raw_text_df[["dw_ek_borger"]]
+        .isin(validated_text_outcome_clozapine[["dw_ek_borger"]].to_dict(orient="list"))
+        .all(axis=1)
+    )
+
+    # Apply the conditions to filter rows in raw_text_df
+    raw_text_df = raw_text_df[
+        ~condition_no_text_matched_words
+        & ~condition_no_text
+        & ~condition_unsure
+        & ~condition_validated_text
+    ]
+
+    # sort and filter for timestamps more than 2013 raw_text_df for  reading
+    raw_text_df = raw_text_df[raw_text_df["timestamp"].dt.year > 2012]
+
+    sorted_df = raw_text_df.sort_values(by=["dw_ek_borger", "timestamp"])
+
+    unique_dw_ek_borger_count = sorted_df["dw_ek_borger"].nunique()
+    print(f"Number of unique dw_ek_borger left in sorted_df: {unique_dw_ek_borger_count}")
+
+    return (
+        sorted_df,
+        validated_text_outcome_clozapine,
+        unsure_text_outcome_clozapine,
+        no_text_outcome_df,
+    )
+
+
+def get_next_version_file_path(file_path):
+    base_path = file_path.parent
+    file_name = file_path.stem
+    extension = file_path.suffix
+
+    version = 1
+    while (new_file_path := base_path / f"{file_name}_v{version}{extension}").exists():
+        version += 1
+
+    return new_file_path
+
+
+def read_and_validate_text_for_clozapine_outcome(
+    sorted_df, validated_text_outcome_clozapine, unsure_text_outcome_clozapine, no_text_outcome_df
+):
+    no_text_version_count = 0
+    unsure_version_count = 0
+    validated_version_count = 0
+
+    # Iterate through sorted DataFrame
+    for index, row in sorted_df.iterrows():
+        if row["dw_ek_borger"] in validated_text_outcome_clozapine["dw_ek_borger"].values:
+            print(
+                f"Skipping row {index + 1} as dw_ek_borger {row['dw_ek_borger']} is already in validated_text_outcome_df."
+            )
+            continue
+
+        # Display the current row information
+        print(
+            f"\nRow {index + 1} - Fuzz Ratio: {row['fuzz_ratio']}, Matched Word: {row['matched_word']}, dw_ek_borger: {row['dw_ek_borger']}, timestamp: {row['timestamp']}, value: {row['value']}"
+        )
+
+        # Ask the user whether to add to validated_text_outcome_df, unsure, or move to the next row
+        choice = input(
+            "Do you want to add this text to validated_text_outcome_df? (y/n/unsure/skiprow): "
+        ).lower()
+
+        if choice == "y":
+            # Append the row to validated_text_outcome_df
+            validated_text_outcome_clozapine = validated_text_outcome_clozapine.append(
+                row, ignore_index=True
+            )
+            print("Added to validated_text_outcome_df.")
+
+            validated_version_count += 1
+            if validated_version_count % 5 == 0:
+                file_path = Path(
+                    "E:/shared_resources/clozapine/text_outcome/validated_text_outcome_clozapine.parquet"
+                )
+                new_file_path = get_next_version_file_path(file_path)
+                write_df_to_file(df=validated_text_outcome_clozapine, file_path=new_file_path)
+
+                print("saved a new version of validated_text_outcome_clozapine to disk.")
+
+        elif choice == "n":
+            # Add to no_text_outcome_df_unique
+            no_text_outcome_df = no_text_outcome_df.append(row, ignore_index=True)
+            print("Added to no_text_outcome_df.")
+
+            no_text_version_count += 1
+            if no_text_version_count % 20 == 0:
+                file_path = Path(
+                    "E:/shared_resources/clozapine/text_outcome/no_text_outcome_clozapine.parquet"
+                )
+                new_file_path = get_next_version_file_path(file_path)
+                write_df_to_file(df=no_text_outcome_df, file_path=new_file_path)
+                print("saved a new version of no_text_outcome_clozapine to disk.")
+
+        elif choice == "unsure":
+            # Add to unsure_text_outcome_clozapine
+            unsure_text_outcome_clozapine = unsure_text_outcome_clozapine.append(
+                row, ignore_index=True
+            )
+            print("Added to unsure_text_outcome_clozapine.")
+
+            unsure_version_count += 1
+            if unsure_version_count % 5 == 0:
+                file_path = Path(
+                    "E:/shared_resources/clozapine/text_outcome/unsure_text_outcome_clozapine.parquet"
+                )
+                new_file_path = get_next_version_file_path(file_path)
+                write_df_to_file(df=unsure_text_outcome_clozapine, file_path=new_file_path)
+                print("saved a new version of unsured_text_outcome_clozapine to disk.")
+
+        elif choice == "skip":
+            # Skip to the next row
+            print("Skipped to the next row.")
+
+    return (
+        sorted_df,
+        validated_text_outcome_clozapine,
+        unsure_text_outcome_clozapine,
+        no_text_outcome_df,
+    )
+
+
+if __name__ == "__main__":
+    (
+        sorted_df,
+        validated_text_outcome_clozapine,
+        unsure_text_outcome_clozapine,
+        no_text_outcome_df,
+    ) = process_df_from_disk()
+
+    (
+        sorted_df,
+        validated_text_outcome_clozapine,
+        unsure_text_outcome_clozapine,
+        no_text_outcome_df,
+    ) = read_and_validate_text_for_clozapine_outcome(
+        sorted_df,
+        validated_text_outcome_clozapine,
+        unsure_text_outcome_clozapine,
+        no_text_outcome_df,
+    )
+
+    # if completly finished, save all dfs
+
+    file_path = Path(
+        "E:/shared_resources/clozapine/text_outcome/validated_text_outcome_clozapine.parquet"
+    )
+    new_file_path = get_next_version_file_path(file_path)
+    write_df_to_file(df=validated_text_outcome_clozapine, file_path=new_file_path)
+    print("saved a new version of validated_text_outcome_clozapine to disk.")
+
+    file_path = Path("E:/shared_resources/clozapine/text_outcome/no_text_outcome_clozapine.parquet")
+    new_file_path = get_next_version_file_path(file_path)
+    write_df_to_file(df=no_text_outcome_df, file_path=new_file_path)
+    print("saved a new version of no_text_outcome_clozapine to disk.")
+
+    file_path = Path(
+        "E:/shared_resources/clozapine/text_outcome/unsure_text_outcome_clozapine.parquet"
+    )
+    new_file_path = get_next_version_file_path(file_path)
+    write_df_to_file(df=unsure_text_outcome_clozapine, file_path=new_file_path)
+    print("saved a new version of unsured_text_outcome_clozapine to disk.")
