3f409b12bd4ba572cc8923f243e6690c21890afd
sarakolding <sarakolding@live.dk>
2024-03-20 11:14:01 +0100

    first stab

--- psycop/projects/restraint/feature_generation/main.py
+++ psycop/projects/restraint/feature_generation/main.py
@@ -18,6 +18,7 @@ from psycop.common.feature_generation.application_modules.save_dataset_to_disk i
 )
 from psycop.projects.restraint.cohort.restraint_cohort_definer import RestraintCohortDefiner
 from psycop.projects.restraint.feature_generation.modules.specify_features import FeatureSpecifier
+from psycop.projects.restraint.feature_generation.modules.specify_text_features import TextFeatureSpecifier
 from psycop.projects.restraint.restraint_global_config import RESTRAINT_PROJECT_INFO
 
 log = logging.getLogger()
@@ -28,10 +29,12 @@ def main():
     dataset."""
     project_info = RESTRAINT_PROJECT_INFO
 
-    feature_specs = FeatureSpecifier(
-        project_info=project_info,
-        min_set_for_debug=False,  # Remember to set to False when generating full dataset
-    ).get_feature_specs()
+    # feature_specs = FeatureSpecifier(
+    #     project_info=project_info,
+    #     min_set_for_debug=False,  # Remember to set to False when generating full dataset
+    # ).get_feature_specs()
+
+    feature_specs = TextFeatureSpecifier(project_info=project_info, min_set_for_debug=True).get_text_feature_specs(note_types = ["aktuelt_psykisk", "all_relevant"], model_names = ["dfm-encoder-large", "dfm-encoder-large-v1-finetuned", "tfidf-500", "tfidf-1000"])
 
     flattened_df = create_flattened_dataset_tsflattener_v1(
         feature_specs=feature_specs,  # type: ignore
--- psycop/projects/restraint/feature_generation/modules/specify_text_features.py
+++ psycop/projects/restraint/feature_generation/modules/specify_text_features.py
@@ -1,94 +1,79 @@
 """Text-feature specification module."""
 import logging
+from typing import Union
+import pandas as pd
+import polars as pl
 
 import numpy as np
 from timeseriesflattener.v1.aggregation_fns import AggregationFunType, mean
 from timeseriesflattener.v1.df_transforms import df_with_multiple_values_to_named_dataframes
 from timeseriesflattener.v1.feature_specs.group_specs import PredictorGroupSpec
-from timeseriesflattener.v1.feature_specs.single_specs import PredictorSpec  # type: ignore
+from timeseriesflattener.v1.feature_specs.single_specs import PredictorSpec, StaticSpec, OutcomeSpec
 
 from psycop.common.feature_generation.application_modules.project_setup import ProjectInfo
 from psycop.common.feature_generation.loaders.raw.load_embedded_text import EmbeddedTextLoader
+from psycop.common.global_utils.paths import TEXT_EMBEDDINGS_DIR
+from psycop.projects.restraint.feature_generation.modules.specify_features import FeatureSpecifier
 
 log = logging.getLogger(__name__)
 
 
-class TextFeatureSpecifier:
+class TextFeatureSpecifier(FeatureSpecifier):
     """Specify features based on prediction time."""
-
-    def __init__(self, project_info: ProjectInfo, min_set_for_debug: bool = False):
-        self.min_set_for_debug = min_set_for_debug
-        self.project_info = project_info
 
     def _get_text_specs(
         self,
-        resolve_multiple: list[AggregationFunType],
-        interval_days: list[float],
-        embedded_text_filename: str,
-    ) -> list[PredictorSpec]:
-        log.info("-------- Generating tf-idf specs --------")
+        lookbehind_days: float,
+        embedded_text: pd.DataFrame,
+        spec_name: str,
+        prefix: str,
+    ) -> PredictorSpec:
 
-        TEXT_SFIS = [
-            "Observation af patient, Psykiatri",
-            "Samtale med behandlingssigte",
-            "Aktuelt psykisk",
-            "Aktuelt socialt, Psykiatri",
-            "Aftaler, Psykiatri",
-            "Aktuelt somatisk, Psykiatri",
-            "Objektivt psykisk",
-            "Kontaktårsag",
-            "Telefonnotat",
-            "Semistruktureret diagnostisk interview",
-            "Vurdering/konklusion",
-        ]
-
-        embedded_text = EmbeddedTextLoader.load_embedded_text(
-            filename=embedded_text_filename,
-            text_sfi_names=TEXT_SFIS,
-            include_sfi_name=False,
-            n_rows=None,
-        ).to_pandas()
-
-        embedded_text = df_with_multiple_values_to_named_dataframes(
-            df=embedded_text,
-            entity_id_col_name="dw_ek_borger",
-            timestamp_col_name="timestamp",
-            name_prefix="sent_",
+        return PredictorSpec(
+            timeseries_df=embedded_text,
+            feature_base_name=spec_name,
+            lookbehind_days=lookbehind_days,
+            aggregation_fn=mean,
+            fallback=np.nan,
+            prefix=prefix,
         )
 
-        tfidf_specs = PredictorGroupSpec(
-            named_dataframes=embedded_text,
-            lookbehind_days=interval_days,
-            aggregation_fns=resolve_multiple,
-            fallback=[np.nan],
-        ).create_combinations()
+    def _get_text_specs_by_type_and_model(
+        self, note_type: str, model_name: str, lookbehind_days: float, spec_name: str,
+    ) -> PredictorSpec:
+        filename = f"text_embeddings_{note_type}_{model_name}.parquet"
+        embedded_text = pd.read_parquet(TEXT_EMBEDDINGS_DIR / filename)
+        if "overskrift" in embedded_text.columns:
+            embedded_text = embedded_text.drop("overskrift")
+        
+        return self._get_text_specs(
+            embedded_text=embedded_text,
+            prefix=f"{note_type}_{model_name}_",
+            lookbehind_days=lookbehind_days,
+            spec_name=spec_name,
+        )
 
-        return tfidf_specs
-
-    def get_feature_specs(self) -> list[PredictorSpec]:
+    def get_text_feature_specs(self, note_types: list[str], model_names: list[str]) -> list[Union[StaticSpec, PredictorSpec, OutcomeSpec]]:
         """Generate text predictor spec list."""
         log.info("-------- Generating text predictor specs --------")
 
         if self.min_set_for_debug:
-            return self._get_text_specs(
-                resolve_multiple=[mean],
-                interval_days=[1],
-                embedded_text_filename="text_tfidf.parquet",
-            )  # type: ignore
+            return [self._get_text_specs_by_type_and_model(
+                lookbehind_days=10,
+                note_type="aktuelt_psykisk",
+                model_name="tfidf-500",
+                spec_name="test",
+            )]
 
-        resolve_multiple = [mean]
-        interval_days = [60.0, 365.0, 730.0]
+        text_features: list[PredictorSpec] = []
 
-        tfidf_features = self._get_text_specs(
-            resolve_multiple=resolve_multiple,
-            interval_days=interval_days,
-            embedded_text_filename="text_tfidf.parquet",
-        )
+        for note_type in note_types:
+            for model_name in model_names:
+                text_features += [self._get_text_specs_by_type_and_model(
+                    note_type=note_type,
+                    model_name=model_name,
+                    lookbehind_days=730,
+                    spec_name="test",
+                )]
 
-        self._get_text_specs(
-            resolve_multiple=resolve_multiple,
-            interval_days=interval_days,
-            embedded_text_filename="text_embeddings_paraphrase-multilingual-MiniLM-L12-v2.parquet",
-        )
-
-        return tfidf_features
+        return self._get_static_predictor_specs() + text_features + self._get_outcome_specs()
