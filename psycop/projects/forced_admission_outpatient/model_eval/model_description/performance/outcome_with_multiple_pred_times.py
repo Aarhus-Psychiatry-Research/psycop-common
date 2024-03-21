265947f079f8fcd8cfc65f8c01cc61843161450d
Jakob Grøhn <bokajgd@gmail.com>
2024-03-21 14:57:39 +0100

    feat: add wip plot code

--- psycop/projects/forced_admission_outpatient/model_eval/model_description/performance/outcome_with_multiple_pred_times.py
+++ psycop/projects/forced_admission_outpatient/model_eval/model_description/performance/outcome_with_multiple_pred_times.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pandas as pd
 import plotnine as pn
 from wasabi import Printer
 
+from psycop.common.model_evaluation.binary.global_performance.roc_auc import bootstrap_roc
 from psycop.common.model_training.training_output.dataclasses import EvalDataset
 from psycop.projects.forced_admission_outpatient.model_eval.config import FA_PN_THEME
 from psycop.projects.forced_admission_outpatient.utils.pipeline_objects import (
@@ -18,6 +20,7 @@ def _get_prediction_times_with_outcome_shared_by_n_other(
         {
             "id": eval_dataset.ids,
             "y": eval_dataset.y,
+            "y_hat_probs": eval_dataset.y_hat_probs,
             "pred_timestamps": eval_dataset.pred_timestamps,
             "outcome_timestamps": eval_dataset.outcome_timestamps,
         }
@@ -34,6 +37,38 @@ def _get_prediction_times_with_outcome_shared_by_n_other(
     filtered_df = df[df["outcome_uuid"].isin(outcome_uuid_counts[outcome_uuid_counts == n].index)]
 
     return filtered_df
+
+
+def _get_auc_and_time_to_event_for_cases_wtih_nn_pred_times_per_outcome(
+    run: ForcedAdmissionOutpatientPipelineRun, eval_dataset: EvalDataset, n: int
+):
+    df = _get_prediction_times_with_outcome_shared_by_n_other(eval_dataset, n)
+
+    df["time_to_event"] = (df["outcome_timestamps"] - df["pred_timestamps"]).dt.days
+
+    df["pred_time_order"] = df.groupby("outcome_uuid")["pred_timestamps"].rank(method="first")
+
+    for i in range(1, df["pred_time_order"].max() + 1):
+        df_subset = df[df["pred_time_order"] == i]
+
+        # Initialize lists for bootstrapped TPRs and FPRs
+        tprs_bootstrapped, aucs_bootstrapped, base_fpr = bootstrap_roc(
+            n_bootstraps=100, y=df.y, y_hat_probs=df.y_hat_probs
+        )
+
+        auc_mean = np.mean(aucs_bootstrapped)
+
+        plot = (
+            pn.ggplot(df_subset)
+            + FA_PN_THEME
+            + pn.geom_point(pn.aes(x="time_to_event", y=auc_mean), color="red")
+            + pn.labs(x="Time to event (days)", y="Density")
+            + pn.theme(legend_position="none")
+        )
+
+        plot_path = run.paper_outputs.paths.figures / "test.png"
+        plot.save(plot_path)
+
 
 def plot_distribution_of_n_pred_times_per_outcome(
     run: ForcedAdmissionOutpatientPipelineRun, eval_dataset: EvalDataset, max_n: int
@@ -54,7 +89,6 @@ def plot_distribution_of_n_pred_times_per_outcome(
     n_pred_times_counts = df["n_pred_times"].value_counts()
     filtered_df = df[df["n_pred_times"].isin(n_pred_times_counts[n_pred_times_counts > 4].index)]
     filtered_df = filtered_df.reset_index(drop=True)
-    #filtered_df["n_pred_times"] = filtered_df["n_pred_times"].astype(str)
 
     plot = (
         pn.ggplot(filtered_df)
@@ -80,4 +114,6 @@ if __name__ == "__main__":
     run = get_best_eval_pipeline()
     eval_dataset = run.pipeline_outputs.get_eval_dataset()
     max_n = 30
+
+    _get_auc_and_time_to_event_for_cases_wtih_nn_pred_times_per_outcome(run, eval_dataset, 1)
     plot_distribution_of_n_pred_times_per_outcome(run, eval_dataset, max_n)
