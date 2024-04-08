a5d4d3f6790c71298842eeed449302d6a8e80cdd
Jakob Grøhn <bokajgd@gmail.com>
2024-04-08 15:27:28 +0200

    feat: add pathchwork

--- psycop/projects/forced_admission_outpatient/model_eval/model_description/performance/outcome_with_multiple_pred_times.py
+++ psycop/projects/forced_admission_outpatient/model_eval/model_description/performance/outcome_with_multiple_pred_times.py
@@ -3,10 +3,7 @@ import plotnine as pn
 from wasabi import Printer
 
 from psycop.common.model_training.training_output.dataclasses import EvalDataset
-from psycop.projects.forced_admission_outpatient.model_eval.config import (
-    BEST_POS_RATE,
-    FA_PN_THEME,
-)
+from psycop.projects.forced_admission_outpatient.model_eval.config import BEST_POS_RATE, FA_PN_THEME
 from psycop.projects.forced_admission_outpatient.utils.pipeline_objects import (
     ForcedAdmissionOutpatientPipelineRun,
 )
@@ -46,8 +43,8 @@ def _get_prediction_times_with_outcome_shared_by_n_other(
 
 
 def _get_tpr_and_time_to_event_for_cases_wtih_n_pred_times_per_outcome(
-    run: ForcedAdmissionOutpatientPipelineRun, eval_dataset: EvalDataset, n: int
-):
+    eval_dataset: EvalDataset, n: int
+) -> pn.ggplot:
     df = _get_prediction_times_with_outcome_shared_by_n_other(eval_dataset, n)
 
     df["time_to_event"] = (df["outcome_timestamps"] - df["pred_timestamps"]).dt.days
@@ -57,44 +54,51 @@ def _get_tpr_and_time_to_event_for_cases_wtih_n_pred_times_per_outcome(
     )
 
     if df["outcome_uuid"].nunique() < 5:
-        
         return pn.ggplot()
-    
+
     # Add true positive rate for each group
-    df['tpr'] = ""
+    df["tpr"] = ""
 
     for i in range(1, df["pred_time_order"].max() + 1):
-
-        df.loc[df[df["pred_time_order"] == i].index, "tpr"] = (df[df["pred_time_order"] == i].y_pred.sum() / df[df["pred_time_order"] == i].y.sum()) * 100        
+        df.loc[df[df["pred_time_order"] == i].index, "tpr"] = (
+            df[df["pred_time_order"] == i].y_pred.sum() / df[df["pred_time_order"] == i].y.sum()
+        ) * 100
 
     # Change column dtypes
-    df['tpr'] = pd.to_numeric(df['tpr'])
-    df['pred_time_order'] = df['pred_time_order'].astype("category")
+    df["tpr"] = pd.to_numeric(df["tpr"])
+    df["pred_time_order"] = df["pred_time_order"].astype("category")
 
     # Create tpr for positining boxplot + violin plot away from each other:
-    df['tpr_boxplot'] = df['tpr'] + 1.2
-    df['tpr_violin'] = df['tpr'] - 0.6
+    df["tpr_boxplot"] = df["tpr"] + 1.2
+    df["tpr_violin"] = df["tpr"] - 0.6
 
     plot = (
         pn.ggplot(df)
-        + pn.scale_x_continuous(limits=((df['tpr'].max() + 5), max((df['tpr'].min() - 5),0)))
+        + pn.scale_x_continuous(limits=((df["tpr"].max() + 5), max((df["tpr"].min() - 5), 0)))
         + pn.scale_y_reverse()
         + FA_PN_THEME
         + pn.coord_flip()
         + pn.scale_fill_brewer(type="seq", palette="YlGn")  # type: ignore
-        + pn.geom_point(pn.aes(x="tpr", y="time_to_event", fill='pred_time_order'), alpha=0.9, size = 3)
+        + pn.geom_point(
+            pn.aes(x="tpr", y="time_to_event", fill="pred_time_order"), alpha=0.9, size=3
+        )
         + pn.labs(x="Accuracy (%)", y="Time to event (days)")
         + pn.theme(legend_position="none")
         + pn.geom_violin(
-            pn.aes(x="tpr_violin", y="time_to_event", fill='pred_time_order'), style="left", alpha=0.6, width=2, position = pn.position_dodge(),
+            pn.aes(x="tpr_violin", y="time_to_event", fill="pred_time_order"),
+            style="left",
+            alpha=0.6,
+            width=2,
+            position=pn.position_dodge(),
         )
         + pn.geom_boxplot(
-            pn.aes(x="tpr_boxplot", y="time_to_event", fill='pred_time_order'),
+            pn.aes(x="tpr_boxplot", y="time_to_event", fill="pred_time_order"),
             show_legend=False,
             width=1,
             alpha=0.3,
-            position = pn.position_dodge2()
+            position=pn.position_dodge2(),
         )
+        + pn.ggtitle(f"Outcomes with {n} prediction times")
     )
 
     return plot
@@ -137,6 +141,7 @@ def plot_distribution_of_n_pred_times_per_outcome(
 
 
 if __name__ == "__main__":
+    from psycop.common.model_evaluation.patchwork.patchwork_grid import create_patchwork_grid
     from psycop.projects.forced_admission_outpatient.model_eval.selected_runs import (
         get_best_eval_pipeline,
     )
@@ -145,9 +150,16 @@ if __name__ == "__main__":
     eval_dataset = run.pipeline_outputs.get_eval_dataset()
     max_n = 10
 
-    for n in range(1, max_n):
+    plots = [
+        _get_tpr_and_time_to_event_for_cases_wtih_n_pred_times_per_outcome(run, eval_dataset, n)
+        for n in range(1, max_n)
+    ]
 
-        plot = _get_tpr_and_time_to_event_for_cases_wtih_n_pred_times_per_outcome(run, eval_dataset, n)
+    grid_output_path = (
+        run.paper_outputs.paths.figures
+        / "fa_outpatient_performance_and_distribution_for_outcomes_with_multiple_pred_times.png"
+    )
 
-        plot_path = run.paper_outputs.paths.figures / f"fa_outpatient_performance_and_distribution_for_outcomes_with_{n}_pred_times.png"
-        plot.save(plot_path)
+    grid = create_patchwork_grid(plots=plots, single_plot_dimensions=(5, 5), n_in_row=2)
+    grid.savefig(grid_output_path)
+    msg.good(f"Saved figure to {grid_output_path}")
