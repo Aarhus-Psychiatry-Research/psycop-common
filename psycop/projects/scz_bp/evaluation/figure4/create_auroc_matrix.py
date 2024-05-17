c951fc71f1c2972fe9e4f2f2bd735115fd1c6330
sarakolding <sarakolding@live.dk>
2024-05-17 15:34:49 +0200

    misc

--- psycop/projects/scz_bp/evaluation/figure4/create_auroc_matrix.py
+++ psycop/projects/scz_bp/evaluation/figure4/create_auroc_matrix.py
@@ -1,78 +1,33 @@
 from pathlib import Path
 
-import pandas as pd
 import polars as pl
+import pandas as pd
 from confection import Config
-from sklearn.metrics import roc_auc_score
 
-from psycop.common.model_training.training_output.dataclasses import EvalDataset
+from psycop.common.model_evaluation.binary.performance_by_type.auroc_by_outcome import auroc_by_outcome
+from psycop.common.model_training_v2.config.populate_registry import populate_baseline_registry
 from psycop.projects.scz_bp.dataset_description.scz_bp_table_one import SczBpTableOne
-from psycop.projects.scz_bp.evaluation.scz_bp_run_evaluation_suite import (
-    scz_bp_get_eval_ds_from_best_run_in_experiment,
-)
 
 
-def scz_bp_merge_eval_dfs(eval_ds: EvalDataset) -> pl.DataFrame:
+def scz_bp_validation_outcomes() -> list[pl.DataFrame]:
     cfg = Config().from_disk(Path(__file__).parent / "eval_config.cfg")
     meta_df = SczBpTableOne(cfg).get_filtered_prediction_times()
-    meta_df_filtered = meta_df.with_columns(
-        pl.concat_str([pl.col("dw_ek_borger"), pl.col("timestamp")], separator="-").alias(
-            "pred_time_uuids"
-        )
-    )
 
-    meta_df_filtered.select(
-        "pred_time_uuid",
-        "meta_scz_diagnosis_within_0_to_1825_days_max_fallback_0",
-        "meta_bp_diagnosis_within_0_to_1825_days_max_fallback_0",
-        "outc_first_scz_or_bp_within_0_to_1825_days_max_fallback_0",
-    )
-
-    eval_df = eval_ds.to_polars().with_columns(
-        pl.concat_str([pl.col("ids"), pl.col("pred_timestamps")], separator="-").alias(
-            "pred_time_uuid"
-        )
-    )
-
-    joined_df = eval_df.join(meta_df_filtered, on="pred_time_uuids", how="left", validate="1:1")
-
-    return joined_df.rename(
+    meta_df = meta_df.rename(
         {
             "meta_scz_diagnosis_within_0_to_1825_days_max_fallback_0": "scz_diagnosis",
             "meta_bp_diagnosis_within_0_to_1825_days_max_fallback_0": "bp_diagnosis",
             "outc_first_scz_or_bp_within_0_to_1825_days_max_fallback_0": "first_diagnosis",
-        }
-    )
+        })
 
 
-def scz_bp_auroc_by_outcome(model2validation_mapping: list[tuple[str, str]]) -> pd.DataFrame:
-    auroc_df = pd.DataFrame(columns=["scz_diagnosis", "first_diagnosis", "bp_diagnosis"])
-    for model_name, validation_outcome in model2validation_mapping:
-        eval_df = scz_bp_merge_eval_dfs(
-            scz_bp_get_eval_ds_from_best_run_in_experiment(experiment_name=model_name)
-        )
-        eval_df = eval_df.filter(pl.col(validation_outcome).is_not_null()) #lasse 
+    meta_df = meta_df.with_columns(pl.concat_str([pl.col("dw_ek_borger"), pl.col("timestamp").dt.strftime('%Y-%m-%d-%H-%M-%S')], separator="-").alias("pred_time_uuid"))
 
-        auroc_df.loc[model_name, validation_outcome] = roc_auc_score(  # type: ignore
-            y_true=eval_df[validation_outcome], y_score=eval_df["y_hat_probs"]
-        )
-
-    return auroc_df
+    return [meta_df.select(["pred_time_uuid", "scz_diagnosis"]), meta_df.select(["pred_time_uuid", "first_diagnosis"]), meta_df.select(["pred_time_uuid", "bp_diagnosis"])]
 
 
 if __name__ == "__main__":
-    model2validation = [
-        ("sczbp/scz_only", "first_diagnosis"),
-        ("sczbp/scz_only", "scz_diagnosis"),
-        ("sczbp/scz_only", "bp_diagnosis"),
-        ("sczbp/structured_text_xgboost_ddpm", "first_diagnosis"),
-        ("sczbp/structured_text_xgboost_ddpm", "scz_diagnosis"),
-        ("sczbp/structured_text_xgboost_ddpm", "bp_diagnosis"),
-        ("sczbp/bp_only", "first_diagnosis"),
-        ("sczbp/bp_only", "scz_diagnosis"),
-        ("sczbp/bp_only", "bp_diagnosis"),
-    ]
-
-    m = scz_bp_auroc_by_outcome(model2validation_mapping=model2validation)  # type: ignore
+    populate_baseline_registry()
+    m = auroc_by_outcome(model_names=["sczbp/scz_only", "sczbp/structured_text_xgboost_ddpm", "sczbp/bp_only"], validation_outcomes=scz_bp_validation_outcomes())
 
     print(m)
