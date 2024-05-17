55aa3a1ad7380a4d42c349d4815a03941c02f1ba
HLasse <lasseh0310@gmail.com>
2024-05-17 11:35:41 +0200

    sczbp: hparam search individual models

---
+++ psycop/projects/scz_bp/model_training/hparam_search_individual_models.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from psycop.common.model_training_v2.config.populate_registry import populate_baseline_registry
+from psycop.common.model_training_v2.hyperparameter_suggester.optuna_hyperparameter_search import (
+    OptunaHyperParameterOptimization,
+)
+from psycop.projects.scz_bp.model_training.populate_scz_bp_registry import populate_scz_bp_registry
+
+if __name__ == "__main__":
+    populate_baseline_registry()
+
+    outcome_dirs = ["bp_hparam", "scz_hparam"]
+
+    n_trials = 150
+    n_jobs = 15
+    for outcome_dir in outcome_dirs:
+        for cfg_path in (Path(__file__).parent / "config" / "individual_outcomes" / outcome_dir).iterdir():
+            OptunaHyperParameterOptimization().from_file(
+                cfg_path,
+                study_name=cfg_path.stem,
+                n_trials=n_trials,
+                n_jobs=n_jobs,
+                direction="maximize",
+                catch=(Exception,),
+                custom_populate_registry_fn=populate_scz_bp_registry,
+            )
