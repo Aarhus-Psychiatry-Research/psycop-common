02b154f882c293a8a343a92c6a4556b9fbfecfd4
Martin Bernstorff <martinbernstorff@gmail.com>
2024-05-31 11:27:24 +0200

    update train_cvd_hyperparam.py

--- psycop/projects/cvd/model_training/train_cvd_hyperparam.py
+++ psycop/projects/cvd/model_training/train_cvd_hyperparam.py
@@ -3,15 +3,11 @@ from pathlib import Path
 
 import confection
 
-from psycop.common.model_training_v2.config.populate_registry import (
-    populate_baseline_registry,
-)
+from psycop.common.model_training_v2.config.populate_registry import populate_baseline_registry
 from psycop.common.model_training_v2.hyperparameter_suggester.optuna_hyperparameter_search import (
     OptunaHyperParameterOptimization,
 )
-from psycop.projects.cvd.model_training.populate_cvd_registry import (
-    populate_with_cvd_registry,
-)
+from psycop.projects.cvd.model_training.populate_cvd_registry import populate_with_cvd_registry
 
 if __name__ == "__main__":
     populate_baseline_registry()
