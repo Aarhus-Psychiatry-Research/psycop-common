d7e9d4a06678bc3775d967463bb00835d334322e
sarakolding <sarakolding@live.dk>
2024-06-04 16:05:54 +0200

    ups

--- psycop/projects/restraint/evaluation/confusion_matrix.py
+++ psycop/projects/restraint/evaluation/confusion_matrix.py
@@ -2,110 +2,94 @@ from pathlib import Path
 
 import pandas as pd
 import plotnine as pn
-
-from psycop.common.model_evaluation.confusion_matrix import confusion_matrix
+from psycop.common.global_utils.mlflow.mlflow_data_extraction import MlflowClientWrapper
+from psycop.common.model_evaluation.confusion_matrix.confusion_matrix import (
+    ConfusionMatrix,
+    get_confusion_matrix_cells_from_df,
+)
+from psycop.common.model_training.training_output.dataclasses import (
+    get_predictions_for_positive_rate,
+)
 from psycop.common.model_evaluation.confusion_matrix.confusion_matrix import ConfusionMatrix
 from psycop.common.test_utils.str_to_df import str_to_df
-from psycop.projects.restraint.model_evaluation.config import (
-    BEST_DEV_RUN,
-    COLOURS,
-    PN_THEME,
-    TABLES_PATH,
-)
-from psycop.projects.restraint.utils.best_runs import Run, df_to_eval_dataset
 
-
-def plotnine_confusion_matrix(matrix: ConfusionMatrix, x_title: str) -> pn.ggplot:
+def plotnine_confusion_matrix(matrix: ConfusionMatrix, title: str) -> pn.ggplot:
     df = str_to_df(
-        f"""true,pred,estimate
-+,+,{matrix.true_positives},
-+,-,{matrix.false_negatives},
--,+,{matrix.false_positives},
--,-,{matrix.true_negatives},
-" ",+,"PPV:\n{matrix.ppv:.3f}",
-" ",-,"NPV:\n{matrix.npv:.3f}",
--," ","Spec:\n{matrix.specificity:.3f}",
-+," ","Sens:\n{matrix.sensitivity:.3f}",
+        f"""true,pred,estimate,metric
++,+,{matrix.true_positives}," ",
++,-,{matrix.false_negatives}," ",
+-,+,{matrix.false_positives}," ",
+-,-,{matrix.true_negatives}," ",
+" ",+," ","PPV:\n{round(matrix.ppv*100, 1)}%",
+" ",-," ","NPV:\n{round(matrix.npv*100, 1)}%",
+-," "," ","Specificity:\n{round(matrix.specificity*100, 1)}%",
++," "," ","Sensitivity:\n{round(matrix.sensitivity*100, 1)}%",
 """
     )
 
-    """Create a confusion matrix and return a plotnine object."""
     df["true"] = pd.Categorical(df["true"], ["+", "-", " "])
     df["pred"] = pd.Categorical(df["pred"], ["+", "-", " "])
+    df["fill"] = ["1", "1", "1", "1", "2", "2", "2", "2"]
 
     p = (
-        pn.ggplot(df, pn.aes(x="true", y="pred", fill="estimate"))
-        + PN_THEME
-        + pn.geom_tile(pn.aes(width=0.95, height=0.95), fill=COLOURS["blue"])
-        + pn.geom_text(pn.aes(label="estimate"), size=20, color="White")
+        pn.ggplot(df, pn.aes(x="true", y="pred", fill="fill"))
+        + pn.geom_tile(pn.aes(width=0.95, height=0.95))
+        + pn.geom_text(pn.aes(label="metric"), size=20, color="white")
+        + pn.geom_text(pn.aes(label="estimate"), size=25, color="white", fontweight="bold")
         + pn.theme(
-            axis_line=pn.element_blank(),
-            axis_ticks=pn.element_blank(),
-            panel_grid_major=pn.element_blank(),
-            panel_grid_minor=pn.element_blank(),
-            panel_background=pn.element_blank(),
-            legend_position="none",
-        )
+                axis_line=pn.element_blank(),
+                axis_ticks=pn.element_blank(),
+                panel_grid_major=pn.element_blank(),
+                panel_grid_minor=pn.element_blank(),
+                panel_background=pn.element_blank(),
+                legend_position="none",
+                text=(pn.element_text(family="Times New Roman")),
+                axis_text_x=pn.element_text(size=20, weight="bold"),
+                axis_text_y=pn.element_text(size=20, weight="bold"),
+                axis_title=pn.element_text(size=22),
+                plot_title=pn.element_text(size=30, ha="center"),
+                dpi=300,
+            )
         + pn.scale_y_discrete(reverse=True)
-        + pn.labs(title=x_title, y="Predicted", x="Actual")
+        + pn.scale_fill_manual(values=["#B7C8B5", "#1E536D"])
+        + pn.labs(title=title, y="Predicted", x="Actual")
     )
 
     return p
 
 
-def confusion_matrix_metrics(
-    df: pd.DataFrame,
-) -> tuple[pd.DataFrame, pd.DataFrame, ConfusionMatrix]:
-    """
-    Creates a confusion matrix dataframe with PPV, NPV, SENS, and SPEC.
-    """
-    # Calculate the confusion matrix using sklearn
-    cm = confusion_matrix.get_confusion_matrix_cells_from_df(df)
+def confusion_matrix_model(
+    df: pd.DataFrame, positive_rate: float
+) -> ConfusionMatrix:
 
-    # Extract the TP, FP, TN, and FN values from the confusion matrix
+    df = df.rename(columns={"y": "true", "y_hat_prob": "pred"})
 
-    # Create the confusion matrix dataframe
     df = pd.DataFrame(
         {
-            "Actual Positive": [cm.true_positives, cm.false_negatives],
-            "Actual Negative": [cm.false_positives, cm.true_negatives],
-        },
-        index=["Predicted Positive", "Predicted Negative"],
-    )
-
-    # Create a separate dataframe for the metrics
-    metrics_df = pd.DataFrame(
-        {"PPV": [cm.ppv], "NPV": [cm.npv], "SENS": [cm.sensitivity], "SPEC": [cm.specificity]}
-    )
-
-    return df, metrics_df, cm
-
-
-def confusion_matrix_pipeline(run: Run, path: Path):
-    eval_ds = run.get_eval_dataset()
-
-    df_to_eval_dataset(eval_ds, custom_columns=None)  # type: ignore
-    df = pd.DataFrame(
-        {
-            "true": eval_ds["outcome_coercion_type_within_2_days"].replace(  # type: ignore
-                {1: 0, 2: 0, 3: 1}
-            ),
-            "pred": eval_ds.get_predictions_for_positive_rate(
-                desired_positive_rate=run.pos_rate, y_hat_probs_column="y_hat_prob"
+            "true": df["true"],
+            "pred": get_predictions_for_positive_rate(
+                desired_positive_rate=positive_rate,
+                y_hat_probs=df["pred"],  # type: ignore
             )[0],
         }
     )
+    confusion_matrix = get_confusion_matrix_cells_from_df(df=df)
 
-    conf_matrix, metrics_df, cm = confusion_matrix_metrics(df)
-
-    path.mkdir(parents=True, exist_ok=True)
-
-    # Save the df to a csv file
-    conf_matrix.to_csv(path / "confusion_matrix.csv")
-    metrics_df.to_csv(path / "confusion_matrix_metrics.csv")
-
-    plotnine_confusion_matrix(cm, "Confusion Matrix").save(path / "confusion_matrix.png")
-
+    return confusion_matrix
 
 if __name__ == "__main__":
-    confusion_matrix_pipeline(BEST_DEV_RUN, TABLES_PATH)
+    save_dir = Path(__file__).parent / "figures"
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    best_experiment = "restraint_text_hyper"
+    best_pos_rate = 0.05
+    eval_df = MlflowClientWrapper().get_best_run_from_experiment(
+            experiment_name=best_experiment, metric="all_oof_BinaryAUROC"
+        ).eval_frame().frame.to_pandas()
+
+    plotnine_confusion_matrix(
+            confusion_matrix_model(df=eval_df, positive_rate=best_pos_rate),
+            title="Performance Matrix",
+        ).save(save_dir)
+    
+
