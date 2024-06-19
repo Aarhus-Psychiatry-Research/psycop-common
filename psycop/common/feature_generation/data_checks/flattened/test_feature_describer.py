af979d8849a0818887eff0f08a7cc7e031a7ade6
Lasse <lasseh0310@gmail.com>
2024-06-19 10:20:40 +0200

    feat: feature description tsflattener v2

--- psycop/common/feature_generation/data_checks/flattened/test_feature_describer.py
+++ psycop/common/feature_generation/data_checks/flattened/test_feature_describer.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
 
+import numpy as np
+import polars as pl
 import pytest
+from polars.testing import assert_frame_equal
 
 from psycop.common.feature_generation.data_checks.flattened.feature_describer_tsflattener_v2 import (
     ParsedPredictorColumn,
+    generate_feature_description_df,
     parse_predictor_column_name,
+    tsflattener_v2_column_is_static,
 )
 
 
@@ -27,6 +32,7 @@ class PredictorColumnExample:
             fallback="nan",
             time_interval_start="0",
             time_interval_end="730",
+            time_interval_format="days",
             resolve_multiple_strategy="mean",
             is_static=False,
         ),
@@ -36,6 +42,7 @@ class PredictorColumnExample:
             fallback="nan",
             time_interval_start="N/A",
             time_interval_end="N/A",
+            time_interval_format="N/A",
             resolve_multiple_strategy="N/A",
             is_static=True,
         ),
@@ -43,3 +50,40 @@ class PredictorColumnExample:
 )
 def test_parse_predictor_column_name(predictor_column_example: PredictorColumnExample):
     parsed_col = parse_predictor_column_name(predictor_column_example.col_name)
+    assert parsed_col == predictor_column_example
+
+
+def test_tsflattener_v2_is_static():
+    assert (
+        tsflattener_v2_column_is_static("pred_value_1_within_0_to_730_days_mean_fallback_nan") is False
+    )
+    assert tsflattener_v2_column_is_static("pred_female_fallback_nan") is True
+
+
+def test_generate_feature_description_df():
+    df = pl.DataFrame(
+        {
+            "pred_female_fallback_nan": [0, 1, np.nan],
+            "outc_first_scz_or_bp_within_0_to_730_days_max_fallback_0": [0, 0, 1],
+            "pred_value_1_within_0_to_730_days_mean_fallback_nan": [1, 2, 3],
+        }
+    )
+
+    expected = pl.DataFrame(
+        {
+            "Feature name": ["female", "first_scz_or_bp", "value_1"],
+            "Lookbehind period": ["N/A", "0 to 730 days", "0 to 730 days"],
+            "Resolve multiple": ["N/A", "max", "mean"],
+            "Fallback strategy": ["nan", "0", "nan"],
+            "Static": [True, False, False],
+            "Proportion missing": [0.33, 0.0, 0.0],
+            "Mean": [0.5, 0.33, 2.0],
+            "N. unique": [3, 2, 3],
+            "Proportion using fallback": [0.33, 0.67, 0.0],
+        }
+    )
+
+    feature_description = generate_feature_description_df(
+        df, column_name_parser=parse_predictor_column_name
+    )
+    assert_frame_equal(feature_description, expected)
