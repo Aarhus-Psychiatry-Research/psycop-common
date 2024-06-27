a437ff35106b3c83f969091c64104511c07f3e1b
anddan <andrdani@rm.dk>
2024-06-27 11:23:17 +0200

    Removed call to deepcheck

--- psycop/common/feature_generation/application_modules/describe_flattened_dataset.py
+++ psycop/common/feature_generation/application_modules/describe_flattened_dataset.py
@@ -7,15 +7,11 @@ from typing import TYPE_CHECKING
 from timeseriesflattener.v1.feature_specs.single_specs import StaticSpec, TemporalSpec
 from timeseriesflattener.v1.flattened_dataset import PredictorSpec
 
-from psycop.common.feature_generation.data_checks.flattened.data_integrity import (
-    save_feature_set_integrity_checks_from_dir,
-)
 from psycop.common.feature_generation.data_checks.flattened.feature_describer import (
     save_feature_descriptive_stats_from_dir,
 )
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
     from pathlib import Path
 
     from psycop.common.feature_generation.application_modules.project_setup import ProjectInfo
@@ -24,11 +20,7 @@ log = logging.getLogger(__name__)
 
 
 def save_flattened_dataset_description_to_disk(
-    project_info: ProjectInfo,
-    feature_set_dir: Path,
-    feature_specs: list[TemporalSpec | StaticSpec],
-    splits: Iterable[str] = ("train", "val", "test"),
-    compare_splits: bool = True,
+    project_info: ProjectInfo, feature_set_dir: Path, feature_specs: list[TemporalSpec | StaticSpec]
 ):
     """Describe and check flattened dataset. Runs and saves train data integrity checks, split pair integrity checks, outcome integrity checks and a html table containing descriptive statistics for each feature.
 
@@ -51,12 +43,4 @@ def save_flattened_dataset_description_to_disk(
         feature_specs=[s for s in feature_specs if isinstance(s, (StaticSpec, PredictorSpec))],
         file_suffix=".parquet",
         prefixes_to_describe=set(project_info.prefix.__dict__.values()),
-    )
-
-    save_feature_set_integrity_checks_from_dir(
-        feature_set_dir=feature_set_dir,
-        splits=splits,
-        out_dir=data_integrity_checks_path,
-        dataset_format="parquet",
-        compare_splits=compare_splits,
     )
