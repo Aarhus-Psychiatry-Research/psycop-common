f7dce0e89ad507d59b6ecee6b536087967b0eb73
Martin Bernstorff <martinbernstorff@gmail.com>
2024-07-05 14:37:11 +0200

    cvd: update outcome (#976)
    <!--
    Reviews go much faster if the reviewer knows what to focus on! Help them
    out, e.g.:
    Reviewers can skip X, but should pay attention to Y.
    -->

--- psycop/projects/cvd/cohort_examination/test_label_by_outcome_type.py
+++ psycop/projects/cvd/cohort_examination/test_label_by_outcome_type.py
@@ -18,7 +18,6 @@ class Ex:
     [
         Ex(given="KFNG05A", then="PCI"),
         Ex(given="A:DI691", then="Stroke"),
-        Ex(given="A:DA2+:KFNW", then="CABG"),
         Ex(given="A:DI509#+:AZAC2#+:ZDW10#B:DE109#B:DI214", then="AMI"),
         Ex(given="A:DI639#+:AZAC2#B:DE780#B:DI109#B:DI489", then="Stroke"),
         Ex(given="A:DI21#+:KPEG", then="AMI", intention="If overlapping, take the most severe."),
