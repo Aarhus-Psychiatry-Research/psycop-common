6488d3ca29debf4db5e02bfeb84624b6cb3b2060
Martin Bernstorff <martinbernstorff@gmail.com>
2024-07-12 12:18:44 +0200

    update 2 files and delete 1 file

--- psycop/common/test_utils/test_outputs/sql_cache/psycop/common/model_training_v2/trainer/cross_validator_trainer/cached_folds/func_code.py
+++
@@ -1,9 +0,0 @@
-# first line: 19
-@shared_cache.cache()
-def cached_folds(
-    n_splits: int,
-    X: pd.DataFrame,
-    y: pd.DataFrame,
-    groups: pd.Series,  # type: ignore
-) -> list[tuple[ndarray[Any, Any], ndarray[Any, Any]]]:
-    return list(StratifiedGroupKFold(n_splits=n_splits).split(X=X, y=y, groups=groups))
