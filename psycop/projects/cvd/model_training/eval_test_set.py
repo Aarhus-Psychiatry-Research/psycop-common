9b1af34576665927d2264e32de31b1133e861b55
Martin Bernstorff <martinbernstorff@gmail.com>
2024-08-12 10:25:17 +0200

    feat: cvd test-set training (#994)
    @HLasse, would love a check of both:
    * The logic (am I missing anything for training on the test set?)
    * The new addition to PsycopConfig (allowing for a fluent interface,
    e.g. chained method calls)
    
    I've renamed the methods for easier comparison of the location strings.
    Since there are only 4 methods, I think the trade-off is OK, but let me
    know what you think 👍
    
    Thanks!

---
+++ psycop/projects/cvd/model_training/eval_test_set.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from psycop.common.model_training_v2.config.baseline_pipeline import train_baseline_model_from_cfg
+from psycop.common.model_training_v2.config.config_utils import PsycopConfig
+from psycop.common.model_training_v2.config.populate_registry import populate_baseline_registry
+from psycop.projects.cvd.model_training.populate_cvd_registry import populate_with_cvd_registry
+
+
+def test_set(cfg: PsycopConfig):
+    outcome_col_name = cfg.retrieve("trainer.outcome_col_name")
+    preprocessing_pipeline = cfg.retrieve("trainer.preprocessing_pipeline")
+
+    # Setup for test set
+    cfg = (
+        cfg.mut("logger.*.mlflow.experiment_name", "CVD, test-set")
+        .mut("trainer.@trainers", "split_trainer_separate_preprocessing")
+        .rem("trainer.outcome_col_name")
+        .rem("trainer.preprocessing_pipeline")
+    )
+
+    # Handle training set setup
+    cfg = (
+        cfg.add("trainer.training_outcome_col_name", outcome_col_name)
+        .add("trainer.training_preprocessing_pipeline", preprocessing_pipeline)
+        .mut(
+            "trainer.training_preprocessing_pipeline.*.split_filter.splits_to_keep",
+            ["train", "val"],
+        )
+    )
+
+    # Handle validation set setup
+    cfg = (
+        cfg.add(  # Handle validation dataset
+            "trainer.validation_data", cfg.retrieve("trainer.training_data")
+        )
+        .mut("trainer.validation_outcome_col_name", outcome_col_name)
+        .add("trainer.validation_preprocessing_pipeline", preprocessing_pipeline)
+        .mut("trainer.validation_preprocessing_pipeline.*.split_filter.splits_to_keep", ["test"])
+    )
+
+    train_baseline_model_from_cfg(cfg)
+
+
+if __name__ == "__main__":
+    populate_baseline_registry()
+    populate_with_cvd_registry()
+
+    test_set(PsycopConfig().from_disk(Path(__file__).parent / "cvd_baseline.cfg"))
--- psycop/projects/cvd/model_training/layers.py
+++ psycop/projects/cvd/model_training/layers.py
@@ -11,10 +11,10 @@ from psycop.projects.cvd.model_training.populate_cvd_registry import populate_wi
 def train_cvd_layers(cfg: PsycopConfig):
     for layer in range(1, 5):
         layer_cfg = copy.deepcopy(cfg)
-        layer_cfg.mutate("logger.*.mlflow.run_name", f"CVD layer {layer}, base")
+        layer_cfg.mut("logger.*.mlflow.run_name", f"CVD layer {layer}, base")
 
         layers = [str(i) for i in range(1, layer + 1)]
-        layer_cfg.mutate(
+        layer_cfg.mut(
             "trainer.preprocessing_pipeline.*",
             {"layer_selector": {"keep_matching": f".+_layer_({'|'.join(layers)}).+"}},
         )
@@ -27,7 +27,7 @@ def train_cvd_layers(cfg: PsycopConfig):
             layer_cfg.add(
                 "trainer.preprocessing_pipeline.*.aggregation_selector.keep_matching", aggs
             )
-            layer_cfg.mutate("logger.*.mlflow.run_name", f"CVD layer {layer}, (mean, min, mx)")
+            layer_cfg.mut("logger.*.mlflow.run_name", f"CVD layer {layer}, (mean, min, mx)")
 
             logging.info(f"Training model with {aggs}")
             train_baseline_model_from_cfg(cfg=layer_cfg)
