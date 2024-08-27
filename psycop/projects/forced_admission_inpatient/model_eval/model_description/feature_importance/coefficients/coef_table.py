ef818dbf7dc179af3d752894d6bacef8f5062eb2
bokajgd <bokajgd@gmail.com>
2024-08-27 13:51:49 +0200

    feat: std coefs

--- psycop/projects/forced_admission_inpatient/model_eval/model_description/feature_importance/coefficients/coef_table.py
+++ psycop/projects/forced_admission_inpatient/model_eval/model_description/feature_importance/coefficients/coef_table.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import polars as pl
+from sklearn.preprocessing import StandardScaler
 
 from psycop.common.global_utils.paths import OVARTACI_SHARED_DIR
 from psycop.common.model_training.data_loader.utils import (
@@ -21,24 +22,41 @@ def _load_vocabulary(vocab_filename: str) -> pd.DataFrame:
 
     return pd.read_parquet(vocab_filepath)
 
-
-def generate_feature_importance_table(
+def generate_feature_coefs_table(
     pipeline_run: ForcedAdmissionInpatientPipelineRun, vocab_filename: str
 ) -> pl.DataFrame:
+    
     pipeline = pipeline_run.pipeline_outputs.pipe
 
     # Import text model vocabulary
     vocab = _load_vocabulary(vocab_filename)
-
-    # Get feature importance scores
-    feature_importances = pipeline.named_steps["model"].feature_importances_
-
-    split_df = load_and_filter_split_from_cfg(
+    
+    val_df = load_and_filter_split_from_cfg(
         data_cfg=pipeline_run.inputs.cfg.data,
         pre_split_cfg=pipeline_run.inputs.cfg.preprocessing.pre_split,
         split="val",
     )
-    feature_names = [c for c in split_df.columns if "pred_" in c]
+
+    train_df = load_and_filter_split_from_cfg(
+        data_cfg=pipeline_run.inputs.cfg.data,
+        pre_split_cfg=pipeline_run.inputs.cfg.preprocessing.pre_split,
+        split="val",
+    )
+    
+    feature_names = [c for c in train_df.columns if "pred_" in c]
+
+        # Concatenate the train and validation DataFrames
+    combined_df = pd.concat([train_df, val_df], ignore_index=True)
+    
+    # Keep only the columns that are in feature_names
+    train_predictors = combined_df[feature_names]
+
+    # Standardize the predictors (X)
+    scaler = StandardScaler()
+    train_predictors_scaled = scaler.fit_transform(train_predictors)
+    
+    # Get the standard deviation of each feature from the fitted StandardScaler
+    feature_std_devs = scaler.scale_
 
     try:
         pipeline[0]["feature_selection"]  # type: ignore
@@ -48,16 +66,22 @@ def generate_feature_importance_table(
         )
         selected_feature_names = [feature_names[i] for i in feature_indices]
 
+        selected_feature_std_devs = [feature_std_devs[i] for i in feature_indices] # type: ignore
+
     except KeyError:
         selected_feature_names = feature_names  # type: ignore
+    
+    # Get standardised predictor coefficients
+    standard_coefs = pipeline.named_steps["model"].coef_ * selected_feature_std_devs  # type: ignore
 
     # Create a DataFrame to store the feature names and their corresponding gain
     feature_table = pl.DataFrame(
-        {"Feature Name": selected_feature_names, "Gain": feature_importances}
+        {"Feature Name": selected_feature_names, "Standardised coefficients": standard_coefs[0]}
     )
 
     # Sort the table by gain in descending order
-    feature_table = feature_table.sort("Gain", descending=True)
+    feature_table = feature_table.sort("Standardised coefficients", descending=True)
+
     # Get the top 100 features by gain
     top_100_features = feature_table.head(100).with_columns(
         pl.col("Gain").round(3),
@@ -82,9 +106,8 @@ def generate_feature_importance_table(
 
     return top_100_features
 
-
 if __name__ == "__main__":
-    top_100_features = generate_feature_importance_table(
+    top_100_features = generate_feature_coefs_table(
         pipeline_run=get_best_eval_pipeline(),
         vocab_filename="vocab_tfidf_psycop_train_all_sfis_preprocessed_sfi_type_all_sfis_ngram_range_12_max_df_09_min_df_2_max_features_750.parquet",
     )
