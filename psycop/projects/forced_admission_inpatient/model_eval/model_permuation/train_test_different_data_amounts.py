33a8d8097b0c994fece01cbab46c685d300b9ecf
Jakob Grøhn <bokajgd@gmail.com>
2024-09-06 11:41:23 +0200

    feat: train on different data fractions

--- psycop/projects/forced_admission_inpatient/model_eval/model_permuation/train_test_different_data_amounts.py
+++ psycop/projects/forced_admission_inpatient/model_eval/model_permuation/train_test_different_data_amounts.py
@@ -1,130 +1,124 @@
 """Experiments with models trained and evaluated using only part of the data"""
 
-from pathlib import Path
-
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy import stats
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import StratifiedGroupKFold
-from sklearn.pipeline import Pipeline
-from wasabi import Printer
 
-from psycop.common.model_training.config_schemas.full_config import FullConfigSchema
-from psycop.common.model_training.data_loader.utils import (
-    load_and_filter_split_from_cfg,
-)
 from psycop.common.model_training.preprocessing.post_split.pipeline import (
     create_post_split_pipeline,
 )
-from psycop.common.model_training.training.utils import create_eval_dataset
 from psycop.common.model_training.utils.col_name_inference import get_col_names
+from psycop.projects.forced_admission_inpatient.model_eval.model_description.performance.cross_validation_performance_table import (
+    crossvalidate,
+    load_data,
+)
 from psycop.projects.forced_admission_inpatient.utils.pipeline_objects import (
     ForcedAdmissionInpatientPipelineRun,
-    RunGroup,
 )
 
 
-def train_validate(
-    cfg: FullConfigSchema,
-    train: pd.DataFrame,
-    val: pd.DataFrame,
-    pipe: Pipeline,
-    outcome_col_name: str,
-    train_col_names: list[str],
-) -> EvalDataset:
-    """Train model on pre-defined train and validation split and return
-    evaluation dataset.
-
-    Args:
-        cfg (FullConfig): Config object
-        train: Training dataset
-        val: Validation dataset
-        pipe: Pipeline
-        outcome_col_name: Name of the outcome column
-        train_col_names: Names of the columns to use for training
-
-    Returns:
-        Evaluation dataset
-    """
-
-    X_train = train[train_col_names]
-    y_train = train[outcome_col_name]
-    X_val = val[train_col_names]
-
-    pipe.fit(X_train, y_train)
-
-    y_train_hat_prob = pipe.predict_proba(X_train)[:, 1]
-    y_val_hat_prob = pipe.predict_proba(X_val)[:, 1]
-
-    print(
-        f"Performance on train: {round(roc_auc_score(y_train, y_train_hat_prob), 3)}"  # type: ignore
-    )
-
-    df = val
-    df["y_hat_prob"] = y_val_hat_prob
-
-    return create_eval_dataset(
-        col_names=cfg.data.col_name, outcome_col_name=outcome_col_name, df=df
-    )
-
-
-def load_data(cfg: FullConfigSchema) -> tuple(pd.DataFrame, pd.DataFrame): # type: ignore
+def train_model_on_different_training_data_amounts(
+    run: ForcedAdmissionInpatientPipelineRun,
+    save: bool = True,
+    fractions: tuple[float] = (0.1, 0.25, 0.5, 0.75, 1.0),  # type: ignore
+) -> pd.DataFrame:
     """Train a single model and evaluate it."""
-    train_dataset = pd.concat(
-        [
-            load_and_filter_split_from_cfg(
-                data_cfg=cfg.data, pre_split_cfg=cfg.preprocessing.pre_split, split=split
-            )
-            for split in cfg.data.splits_for_training
-        ],
-        ignore_index=True,
-    )   
+    cfg = run.inputs.cfg
 
-    if cfg.data.splits_for_evaluation is not None:
+    dataset = load_data(cfg)
 
-        val_dataset = pd.concat(
-            [
-                load_and_filter_split_from_cfg(
-                    data_cfg=cfg.data, pre_split_cfg=cfg.preprocessing.pre_split, split=split
-                )
-                for split in cfg.data.splits_for_evaluation
-            ],
-            ignore_index=True,
-        )
-
-    else: 
-        val_dataset = load_and_filter_split_from_cfg(
-                    data_cfg=cfg.data, pre_split_cfg=cfg.preprocessing.pre_split, split='val'
-                )
-
-    return train_dataset, val_dataset
-
-
-def train_model_on_different_training_data_amounts(cfg: FullConfigSchema) -> tuple[float, list[float], list[float]]:
-    """Train a single model and evaluate it."""
-    train_dataset, val_dataset = load_data(cfg)
-
-    outcome_col_name_for_train, train_col_names = get_col_names(cfg, train_dataset)
+    outcome_col_name_for_train, train_col_names = get_col_names(cfg, dataset)
 
     pipe = create_post_split_pipeline(cfg)
 
-    eval_dataset = train_validate(
-        cfg=cfg,
-        train=train_dataset,
-        val=val_dataset,
-        pipe=pipe,
-        outcome_col_name=outcome_col_name_for_train,  # type: ignore
-        train_col_names=train_col_names,
+    roc_aucs = []
+    cfs = []
+    std_devs = []
+    oof_intervals = []
+    train_aurocs = []
+
+    # sample 10, 25, 50, 75, 100 percent of the data and train the model on it
+    for i in fractions:
+        dataset = dataset.sample(frac=i, random_state=42)
+
+        eval_dataset, oof_aucs, train_aucs = crossvalidate(
+            cfg=cfg,
+            train=dataset,
+            pipe=pipe,
+            outcome_col_name=outcome_col_name_for_train,  # type: ignore
+            train_col_names=train_col_names,
+        )
+        roc_auc = roc_auc_score(  # type: ignore
+            eval_dataset.y, eval_dataset.y_hat_probs
+        )
+
+        high_oof_auc = max(oof_aucs)
+        low_oof_auc = min(oof_aucs)
+        train_auroc = np.mean(train_aucs)  # type: ignore
+        mean_auroc = np.mean(oof_aucs)
+        std_dev_auroc = np.std(oof_aucs)  # type: ignore
+        std_error_auroc = std_dev_auroc / np.sqrt(len(oof_aucs))
+        dof = len(oof_aucs) - 1
+        conf_interval = stats.t.interval(0.95, dof, loc=mean_auroc, scale=std_error_auroc)
+
+        roc_aucs.append(roc_auc)
+        cfs.append(f"[{round(conf_interval[0],3)}:{round(conf_interval[1],3)}]")
+        std_devs.append(round(std_dev_auroc, 3))
+        oof_intervals.append(f"{low_oof_auc}-{high_oof_auc}")
+        train_aurocs.append(train_auroc)
+
+    df_dict = {
+        "Data fraction": list(fractions),
+        "Apparent AUROC score": train_aurocs,
+        "Internal AUROC score": roc_aucs,
+        "95 percent confidence interval": cfs,
+        "Standard deviation": std_devs,
+        "5-fold out-of-fold AUROC interval": oof_intervals,
+    }
+    df = pd.DataFrame(df_dict)
+
+    output_path = run.paper_outputs.paths.tables / "performance_by_training_fraction.xlsx"
+
+    if save:
+        df.to_excel(output_path, index=False)
+
+    return df
+
+
+def plot_performance_by_amount_of_training_data(run: ForcedAdmissionInpatientPipelineRun) -> None:
+    df = train_model_on_different_training_data_amounts(run=run, save=False)
+
+    # Plot a line graph of the performance by amount of training data
+    fig, ax = plt.subplots()
+    ax.plot(df["Data fraction"], df["Internal AUROC score"], label="Internal AUROC score")
+    ax.plot(df["Data fraction"], df["Apparent AUROC score"], label="Apparent AUROC score")
+    ax.fill_between(
+        df["Data fraction"],
+        df["Internal AUROC score"] - df["Standard deviation"],
+        df["Internal AUROC score"] + df["Standard deviation"],
+        alpha=0.2,
+    )
+    ax.fill_between(
+        df["Data fraction"],
+        df["Internal AUROC score"] - df["Standard deviation"],
+        df["Internal AUROC score"] + df["Standard deviation"],
+        alpha=0.2,
+    )
+    ax.set_xlabel("Data fraction")
+    ax.set_ylabel("AUROC score")
+
+    plt.legend()
+
+    # Save the plot
+    output_path = run.paper_outputs.paths.figures / "performance_by_training_fraction.png"
+    plt.show()
+
+
+if __name__ == "__main__":
+    from psycop.projects.forced_admission_inpatient.model_eval.selected_runs import (
+        get_best_eval_pipeline,
     )
 
-    roc_auc = roc_auc_score(  # type: ignore
-        eval_dataset.y, eval_dataset.y_hat_probs
-    )
-    return roc_auc  # type: ignore
-
-
-def plot_performance_by_amount_of_training_data(
-    models_to_train: pd.DataFrame, models_descriptions: str | None = None
-) -> pd.DataFrame:
-    return pd.DataFrame()
+    plot_performance_by_amount_of_training_data(run=get_best_eval_pipeline())
