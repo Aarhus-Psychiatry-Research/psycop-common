27d2d2209f63aa87ca1f8da596c00dd756745c2b
Martin Bernstorff <martinbernstorff@gmail.com>
2024-09-10 10:42:54 +0200

    feat: 2024-loaders (#1005)

---
+++ psycop/projects/t2d_extended/cohort_definition/outcome_specification/t2d_diagnoses.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+from psycop.common.feature_generation.loaders_2024.diagnoses import type_2_diabetes
+
+
+def get_first_type_2_diabetes_diagnosis() -> pd.DataFrame:
+    df = type_2_diabetes()
+
+    df_first_t2d_diag = (
+        df.sort_values("timestamp").groupby("dw_ek_borger").first().reset_index(drop=False)
+    )
+
+    return df_first_t2d_diag[["dw_ek_borger", "timestamp"]]
+
+
+if __name__ == "__main__":
+    df = get_first_type_2_diabetes_diagnosis()
