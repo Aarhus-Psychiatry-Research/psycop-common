5021feee7863fda448d14811d17af89a8e51b290
Martin Bernstorff <martinbernstorff@gmail.com>
2024-09-10 10:37:06 +0200

    update inspect_features.py and temporal_2021.py

--- psycop/projects/t2d_extended/inspect_features.py
+++ psycop/projects/t2d_extended/inspect_features.py
@@ -1,7 +1,6 @@
-import polars
+import polars as pl
 
 if __name__ == "__main__":
-    df = polars.read_parquet(
+    df = pl.read_parquet(
         "E:/shared_resources/t2d_extended/flattened_datasets/t2d_extended_feature_set/t2d_extended_feature_set.parquet"
     )
-    pass
