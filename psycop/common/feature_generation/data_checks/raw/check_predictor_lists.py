f938bf35278d3afc61ca4ef121097cc7ff85d5e6
Martin Bernstorff <martinbernstorff@gmail.com>
2024-09-13 08:42:40 +0200

    update check_predictor_lists.py and load_structured_sfi.py

--- psycop/common/feature_generation/data_checks/raw/check_predictor_lists.py
+++ psycop/common/feature_generation/data_checks/raw/check_predictor_lists.py
@@ -67,24 +67,6 @@ def check_df_conforms_to_feature_spec(
     return None
 
 
-def get_predictor_df_with_loader_fn(d: dict, n_rows: int) -> pd.DataFrame:  # type: ignore
-    """Get predictor_df from d.
-
-    Args:
-        d (dict): Dictionary with key predictor_df
-        n_rows (int): Number of rows to load
-
-    Returns:
-        pd.DataFrame: predictor_df
-    """
-
-    loader_fns_dict = data_loaders.get_all()
-
-    if "loader_kwargs" in d:
-        return loader_fns_dict[d["predictor_df"]](n_rows=n_rows, **d["loader_kwargs"])
-    return loader_fns_dict[d["predictor_df"]](n_rows=n_rows)
-
-
 def get_unique_predictor_dfs(
     predictor_dict_list: Sequence[dict],  # type: ignore
     required_keys: list[str],
@@ -117,79 +99,3 @@ def get_unique_predictor_dfs(
             unique_subset_dicts.append(predictor_dict)
 
     return unique_subset_dicts
-
-
-def check_feature_combinations_return_correct_dfs(
-    predictor_dict_list: Sequence[dict[str, Any]],
-    n_rows: int = 1_000,
-    required_columns: Sequence[str] | None = None,
-    subset_duplicates_columns: Sequence[str] | None = None,
-    expected_val_dtypes: Sequence[str] | None = None,
-):
-    """Test that all predictor_dfs in predictor_list return a valid df.
-
-    Args:
-        predictor_dict_list (list[dict[str, Union[str, float, int]]]): list of dictionaries
-            where the key predictor_df maps to a catalogue registered data loader
-            or is a valid dataframe.
-        n_rows (int): Number of rows to test. Defaults to 1_000.
-        required_columns (list[str]): list of required columns. Defaults to ["dw_ek_borger", "timestamp", "value"].
-        subset_duplicates_columns (list[str]): list of columns to subset on when
-            checking for duplicates. Defaults to ["dw_ek_borger", "timestamp"].
-        allowed_nan_value_prop (float): Allowed proportion of missing values. Defaults to 0.0.
-        expected_val_dtypes (list[str]): Expected value dtype. Defaults to ["float64", "int64"].
-    """
-
-    if required_columns is None:
-        required_columns = ["dw_ek_borger", "timestamp", "value"]
-
-    if subset_duplicates_columns is None:
-        subset_duplicates_columns = ["dw_ek_borger", "timestamp", "value"]
-
-    if expected_val_dtypes is None:
-        expected_val_dtypes = ["float64", "int64"]
-
-    msg = Printer(timestamp=True)
-
-    msg.info("Checking that feature combinations conform to correct formatting")
-
-    # Find all dicts that are unique on keys predictor_df and allowed_nan_value_prop
-    required_keys = ["predictor_df", "allowed_nan_value_prop"]
-
-    unique_subset_dicts = get_unique_predictor_dfs(
-        predictor_dict_list=predictor_dict_list, required_keys=required_keys
-    )
-
-    msg.info(f"Loading {n_rows} rows from each predictor_df")
-
-    failure_dicts = []
-
-    # Check each predictor df
-    for i, d in enumerate(unique_subset_dicts):
-        # Check that it returns a dataframe
-        try:
-            df = get_predictor_df_with_loader_fn(d=d, n_rows=n_rows)
-        except KeyError:
-            msg.warn(
-                f"{d['predictor_df']} does not appear to be a loader function in catalogue, assuming a well-formatted dataframe. Continuing."
-            )
-            continue
-
-        msg_prefix = f"{i+1}/{len(unique_subset_dicts)} {d['predictor_df']}:"
-
-        df_failures = check_df_conforms_to_feature_spec(
-            df=df,
-            required_columns=required_columns,
-            subset_duplicates_columns=subset_duplicates_columns,
-            expected_val_dtypes=expected_val_dtypes,
-            msg_prefix=msg_prefix,
-            arg_dict=d,
-        )
-
-        if df_failures:
-            failure_dicts.append(df_failures)
-
-    if not failure_dicts:
-        msg.good(f"Checked {len(unique_subset_dicts)} predictor_dfs, all returned appropriate dfs")
-    else:
-        raise ValueError(f"{failure_dicts}")
