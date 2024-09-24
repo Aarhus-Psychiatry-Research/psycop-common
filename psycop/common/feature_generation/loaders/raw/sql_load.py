e31542d1a68b32d65c5184b0f4eade6682675df6
bokajgd <bokajgd@gmail.com>
2024-09-24 11:46:27 +0200

    misc. fixes

--- psycop/common/feature_generation/loaders/raw/sql_load.py
+++ psycop/common/feature_generation/loaders/raw/sql_load.py
@@ -38,7 +38,7 @@ def sql_load(
         # From USR_PS_Forsk
         >>> view = "[FOR_SFI_fritekst_resultat_udfoert_i_psykiatrien_aendret_2011]"
         >>> sql = "SELECT * FROM [fct]." + view
-        >>> df = sql_load(sql, chunksize = None)
+        >>> df = sql_load(sql)
     """
     driver = "SQL Server"
     params = urllib.parse.quote(
