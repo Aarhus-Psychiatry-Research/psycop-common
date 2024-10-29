0b8b79a602e8e4c284c75d49a9f99177fb8d7565
anddan <andrdani@rm.dk>
2024-10-29 11:20:06 +0100

    polars changes to pandas before counting uniques

--- psycop/projects/AcuteSomaticAdmission/CohortDefinition/get_psychiatric_outpatient_visits.py
+++ psycop/projects/AcuteSomaticAdmission/CohortDefinition/get_psychiatric_outpatient_visits.py
@@ -27,7 +27,8 @@ def get_outpatient_visits_to_psychiatry() -> pl.DataFrame:
 
 if __name__ == "__main__":
     df_pl = get_outpatient_visits_to_psychiatry()
-    n_patients = df_pl["dw_ek_borger"].nunique()
+    df_pd = df_pl.to_pandas()
+    n_patients = df_pd["dw_ek_borger"].nunique()
     print(f"Antal unikke ID'er der har mindst én ambulant kontakt er: {n_patients}")
-    antal_kontakter = df_pl.shape[0]
+    antal_kontakter = df_pd.shape[0]
     print(f"Antal ambulante kontakter: {antal_kontakter}")
