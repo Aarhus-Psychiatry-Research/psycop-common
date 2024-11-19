99320b0c66c117c2c576aca33c4dd11249476231
bokajgd <bokajgd@gmail.com>
2024-11-19 12:34:47 +0100

    misc 2

--- psycop/projects/uti/feature_generation/visualise_outcomes.py
+++ psycop/projects/uti/feature_generation/visualise_outcomes.py
@@ -53,7 +53,7 @@ def plot_outcomes_by_year(df: pd.DataFrame, outcome_col_name: str = "out_value")
     plot = (
         pn.ggplot(df, pn.aes(x="year", fill="year"))
         + pn.geom_bar(fill="#1f77b4")  # Dark blue color
-        + pn.labs(title="UTI-outcomes pr. år", x="Year", y="Number of prediction times")
+        + pn.labs(title="UTI-outcomes pr. år", x="Year", y="Number of outcomes")
         + pn.theme_minimal()  # Modern theme
         + pn.theme(
             text=pn.element_text(size=12),
@@ -79,7 +79,27 @@ def plot_outcomes_by_shak_code(df: pd.DataFrame, outcome_col_name: str = "out_va
     plot = (
         pn.ggplot(df, pn.aes(x="shak_code", fill="shak_code"))
         + pn.geom_bar(fill="#1f77b4")  # Dark blue color
-        + pn.labs(title="UTI-udfald pr. afdeling", x="Shak code", y="Number of prediction times")
+        + pn.labs(title="UTI-udfald pr. afdeling", x="Shak code", y="Number of outomces")
+        + pn.theme_minimal()  # Modern theme
+        + pn.theme(
+            text=pn.element_text(size=12),
+            axis_title=pn.element_text(size=14),
+            title=pn.element_text(size=16),
+        )
+    )
+
+    return plot
+
+def plot_outcomes_gender(df: pd.DataFrame, outcome_col_name: str = "out_value") -> pn.ggplot:
+
+    # keep only outcomes that are not 0
+    df = df[df[outcome_col_name] != 0]
+
+    # plot the number of prediction times by shak code as a bar plot
+    plot = (
+        pn.ggplot(df, pn.aes(x="pred_adm_day_count", fill="pred_adm_day_count"))
+        + pn.geom_bar(fill="#1f77b4")  # Dark blue color
+        + pn.labs(title="UTI-outcome - admission day distribution", x="Day of admissions", y="Number of outcomes")
         + pn.theme_minimal()  # Modern theme
         + pn.theme(
             text=pn.element_text(size=12),
@@ -91,16 +111,39 @@ def plot_outcomes_by_shak_code(df: pd.DataFrame, outcome_col_name: str = "out_va
     return plot
 
 
+def plot_outcomes_adm_day(df: pd.DataFrame, outcome_col_name: str = "out_value") -> pn.ggplot:
+
+    # keep only outcomes that are not 0
+    df = df[df[outcome_col_name] != 0]
+
+    # plot the number of prediction times by shak code as a bar plot
+    plot = (
+        pn.ggplot(df, pn.aes(x="pred_sex_female_fallback_0", fill="pred_sex_female_fallback_0"))
+        + pn.geom_bar(fill="#1f77b4")  # Dark blue color
+        + pn.labs(title="UTI-outcome - sex distribution", x="Gender", y="Number of outcomes times")
+        + pn.theme_minimal()  # Modern theme
+        + pn.theme(
+            text=pn.element_text(size=12),
+            axis_title=pn.element_text(size=14),
+            title=pn.element_text(size=16),
+        )
+    )
+
+
+    return plot
+
 def uti_patchwork_data_plot(
-    df: pd.DataFrame, output_path: Path, name: str = "uti_data_pathwork", outcome_col_name='outc_uti_value_within_0_to_1_days_max_fallback_0',
+    df: pd.DataFrame, output_path: Path, name: str = "uti_data_pathwork", outcome_col_name: str = 'outc_uti_value_within_0_to_1_days_max_fallback_0',
 ) -> None:
-    pred_per_year = plot_prediction_times_by_year(df)
+
     outc_per_year = plot_outcomes_by_year(df,outcome_col_name)
     outc_per_shak = plot_outcomes_by_shak_code(df,outcome_col_name)
+    outc_gender = plot_outcomes_gender(df, outcome_col_name)
+    outc_adm_day = plot_outcomes_adm_day(df, outcome_col_name)
 
-    plots = [pred_per_year, outc_per_year, outc_per_shak]
+    plots = [outc_gender, outc_adm_day, outc_per_year, outc_per_shak]
 
-    grid = create_patchwork_grid(plots=plots, single_plot_dimensions=(5, 3), n_in_row=1)
+    grid = create_patchwork_grid(plots=plots, single_plot_dimensions=(5, 3), n_in_row=2)
     grid_output_path = output_path / f"{name}.png"
     grid.savefig(grid_output_path)
 
@@ -109,6 +152,9 @@ if __name__ == "__main__":
     full_definition = pd.read_parquet(
         data_path / "uti_outcomes_full_definition" / "uti_outcomes_full_definition.parquet"
     )
+
+    uti_patchwork_data_plot(full_definition, output_path, "full_definition")
+
     urine_samples = pd.read_parquet(
         data_path / "uti_outcomes_urine_samples" / "uti_outcomes_urine_samples.parquet"
     )
@@ -116,4 +162,3 @@ if __name__ == "__main__":
         data_path / "uti_outcomes_antibiotics" / "uti_outcomes_antibiotics.parquet"
     )
 
-    uti_patchwork_data_plot(full_definition, output_path, "full_definition")
