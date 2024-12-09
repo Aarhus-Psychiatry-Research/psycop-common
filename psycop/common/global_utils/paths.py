325819903f7dd4d864226d30ed5988fb4e5f4262
Jakob Grøhn <bokajgd@gmail.com>
2024-12-09 11:37:58 +0100

    fix: re-add dirs for scz proj

--- psycop/common/global_utils/paths.py
+++ psycop/common/global_utils/paths.py
@@ -3,4 +3,5 @@ from pathlib import Path
 PSYCOP_PKG_ROOT = Path(__file__).parent.parent
 OVARTACI_SHARED_DIR = Path(r"E:\shared_resources")
 TEXT_EMBEDDING_MODELS_DIR = OVARTACI_SHARED_DIR / "text_embedding_models"
+TEXT_EMBEDDINGS_DIR = OVARTACI_SHARED_DIR / "text_embeddings"
 RANDOM_SEED = 42
