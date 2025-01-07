d8f079cc26e9268f214b3bf1c8d63db014a95cdd
sarakolding <sarakolding@live.dk>
2025-01-07 13:17:44 +0000

    save fig

--- psycop/projects/fairness/example.py
+++ psycop/projects/fairness/example.py
@@ -7,8 +7,9 @@ from fairlearn.metrics import (
     false_negative_rate,
     false_positive_rate,
     selection_rate,
+    demographic_parity_ratio,
 )
-from sklearn.metrics import accuracy_score, precision_score
+from sklearn.metrics import accuracy_score, precision_score, roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
@@ -28,6 +29,9 @@ if __name__ == "__main__":
     classifier = DecisionTreeClassifier(min_samples_leaf=10, max_depth=4)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict_proba(X_test)[:, 1] >= 0.1
+
+    result = accuracy_score(y_test, y_pred)
+
     mf = MetricFrame(
         metrics=accuracy_score, y_true=y_test, y_pred=y_pred, sensitive_features=A_test
     )
@@ -41,9 +45,12 @@ if __name__ == "__main__":
     )
     mf.overall.item()
 
+    demographic_parity_ratio(y_test, y_pred, sensitive_features=A_test)
+
     metrics = {
+        "auroc": roc_auc_score,
         "accuracy": accuracy_score,
-        "precision": precision_score,
+        "positive predictive value": precision_score,
         "false positive rate": false_positive_rate,
         "false negative rate": false_negative_rate,
         "selection rate": selection_rate,
@@ -52,7 +59,8 @@ if __name__ == "__main__":
     metric_frame = MetricFrame(
         metrics=metrics, y_true=y_test, y_pred=y_pred, sensitive_features=A_test
     )
-    metric_frame.by_group.plot.bar(
+    bar_plot = metric_frame.by_group.plot.bar(
         subplots=True, layout=[3, 3], legend=False, figsize=[12, 8], title="Show all metrics"
     )
 
+    bar_plot[0][0].figure.savefig("bar_plot.png")
