319b3ade23ce7eb52af5c9689b2a755ee3f9449e
anddan <andrdani@rm.dk>
2025-01-07 09:14:58 +0100

    lint

--- psycop/projects/cvd/Missing_for_paper.py
+++ psycop/projects/cvd/Missing_for_paper.py
@@ -1,8 +1,10 @@
-import pandas as pd
-
 from pathlib import Path
 
-file_path = Path("E:/shared_resources/cvd/feature_set/flattened_datasets/cvd_feature_set/cvd_feature_set.parquet")
+import pandas as pd
+
+file_path = Path(
+    "E:/shared_resources/cvd/feature_set/flattened_datasets/cvd_feature_set/cvd_feature_set.parquet"
+)
 
 df = pd.read_parquet(file_path)
 
@@ -16,10 +18,12 @@ missing_proportions = {
 }
 
 # Create a new DataFrame with the results
-result_df = pd.DataFrame(list(missing_proportions.items()), columns=["Column", "Missing_Proportion"])
+result_df = pd.DataFrame(
+    list(missing_proportions.items()), columns=["Column", "Missing_Proportion"]
+)
 
 
-#save dataset to overtaci
+# save dataset to overtaci
 file_path = Path("E:/anddan/CVD/missing_proportions.xlsx")
 
 result_df.to_excel(file_path, index=False)
