48676fa641faedf1d67a01a80646f8b824e98019
sarakolding <sarakolding@live.dk>
2025-02-18 12:39:29 +0100

    add hyperparameter tuning cfgs

--- psycop/projects/restraint/training/restraint_split_tuning.py
+++ psycop/projects/restraint/training/restraint_split_tuning.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     populate_baseline_registry()
     populate_with_restraint_registry()
     OptunaHyperParameterOptimization().from_file(
-        (Path(__file__).parent / "configs" / "tuning" / "restraint_structured_mechanical_tuning_cont.cfg"),
-        study_name="restraint_structured_mechanical_tuning_v26",
-        n_trials=200,
+        (Path(__file__).parent / "configs" / "tuning" / "restraint_split_tuning.cfg"),
+        study_name="restraint_split_tuning",
+        n_trials=250,
         n_jobs=10,
         direction="maximize",
         catch=(),  # type: ignore
