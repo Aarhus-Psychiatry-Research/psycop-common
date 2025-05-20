bdb38a467bd72b5ca47845cee02e46a197b2a4eb
sarakolding <sarakolding@live.dk>
2025-05-20 16:24:17 +0200

    add binary ppv

--- psycop/projects/restraint/training/restraint_mechanical_tuning.py
+++ psycop/projects/restraint/training/restraint_mechanical_tuning.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     populate_with_restraint_registry()
     OptunaHyperParameterOptimization().from_file(
         (Path(__file__).parent / "configs" / "tuning" / "restraint_mechanical_tuning.cfg"),
-        study_name="restraint_mechanical_tuning_v2",
+        study_name="restraint_mechanical_tuning_v3",
         n_trials=250,
         n_jobs=10,
         direction="maximize",
