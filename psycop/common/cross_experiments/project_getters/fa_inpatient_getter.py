fcd92bbea9a199a7986893acf4395fa09b3ce4ed
Jakob Grøhn Damgaard <bokajgd@gmail.com>
2025-12-09 16:34:57 +0100

    add fao getter

---
+++ psycop/common/cross_experiments/project_getters/fa_inpatient_getter.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import pandas as pd
+from confection import Config
+
+from psycop.common.cross_experiments.getter import Getter
+from psycop.common.model_training_v2.config.config_utils import PsycopConfig
+
+
+class ForcedAdmissionsInpatientGetter(Getter):
+    predicted_positive_rate: float = 0.05
+
+    @staticmethod
+    def get_eval_df() -> pd.DataFrame:
+        eval_df_path = "E:/shared_resources/forced_admission_inpatient/models/full_model_with_text_features_train_val/chuddahs-caterwauls-eval-on-test/abrasiometerintergradient-eval-on-test/evaluation_dataset.parquet"
+
+        return pd.read_parquet(eval_df_path)
+
+    @staticmethod
+    def get_feature_set_df() -> pd.DataFrame:
+        feature_set_df_path = "E:/shared_resources/forced_admission_inpatient/flattened_datasets/full_feature_set_with_sentence_transformers_and_tfidf_750/"
+
+        split_df_paths = Path(feature_set_df_path).glob("*.parquet")
+        df_list = [pd.read_parquet(file) for file in split_df_paths]
+
+        feature_set_df = pd.concat(df_list, ignore_index=True)
+
+        return feature_set_df
+
+    @staticmethod
+    def get_cfg() -> PsycopConfig:
+        config_path = "E:/shared_resources/forced_admission_inpatient/models/full_model_with_text_features_train_val/chuddahs-caterwauls-eval-on-test/abrasiometerintergradient-eval-on-test/cfg.json"
+
+        # read and return config
+        return PsycopConfig(Config().from_disk(path=Path(config_path)))
+
+
+if __name__ == "__main__":
+    getter = ForcedAdmissionsInpatientGetter()
+    print(getter.get_cfg())
+    print(getter.get_eval_df().head())
+    print(getter.get_feature_set_df().head())
