984b587dd9bc4e162d7de4e90113831c90c77d3e
bokajgd <bokajgd@gmail.com>
2025-12-18 12:06:04 +0100

    misc updates

--- psycop/projects/forced_admission_outpatient/training/hyperparam.py
+++ psycop/projects/forced_admission_outpatient/training/hyperparam.py
@@ -6,14 +6,12 @@ from psycop.common.model_training_v2.config.populate_registry import populate_ba
 from psycop.common.model_training_v2.hyperparameter_suggester.optuna_hyperparameter_search import (
     OptunaHyperParameterOptimization,
 )
+from psycop.projects.forced_admission_outpatient.training.populate_fao_registry import (
+    populate_with_fao_registry,
+)
 
 
 def hyperparameter_search(cfg: PsycopConfig, run_name: str):
-    cfg.mut(
-        "trainer.task.task_pipe.sklearn_pipe.*.model",
-        {"@estimator_steps_suggesters": "xgboost_suggester"},
-    )
-
     cfg.mut("logger.*.mlflow.experiment_name", f"{run_name}")
 
     cfg.mut(
@@ -25,7 +23,7 @@ def hyperparameter_search(cfg: PsycopConfig, run_name: str):
         cfg,
         study_name=cfg.retrieve("logger.*.mlflow.experiment_name") + "_",
         n_trials=100,
-        n_jobs=10,
+        n_jobs=5,
         direction="maximize",
         catch=(Exception,),
         custom_populate_registry_fn=None,
@@ -34,8 +32,8 @@ def hyperparameter_search(cfg: PsycopConfig, run_name: str):
 
 if __name__ == "__main__":
     populate_baseline_registry()
-
+    populate_with_fao_registry()
     hyperparameter_search(
         PsycopConfig().from_disk(Path(__file__).parent / "fao_all_features_hyperparam.cfg"),
-        run_name="full_model_all_features_TEST",
+        run_name="full_model_all_features",
     )
