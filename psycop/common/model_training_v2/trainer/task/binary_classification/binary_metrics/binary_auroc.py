69829248c5185cfeed709625513ead6da5dc6603
Jakob Grøhn Damgaard <bokajgd@gmail.com>
2026-03-11 15:37:10 +0100

    feat: add concentrated auroc metric

--- psycop/common/model_training_v2/trainer/task/binary_classification/binary_metrics/binary_auroc.py
+++ psycop/common/model_training_v2/trainer/task/binary_classification/binary_metrics/binary_auroc.py
@@ -31,3 +31,26 @@ class BinaryAUROC(BinaryMetric):
             name=f"{name_prefix}_BinaryAUROC" if name_prefix else "BinaryAUROC",
             value=float(roc_auc_score(y_true=y, y_score=y_hat_prob)),
         )
+
+
+@BaselineRegistry.metrics.register("concentrated_binary_auroc")
+class ConcentratedBinaryAUROC(BinaryMetric):
+    """
+    max_fpr allows for calculation of concentrated AUROC. If max_fpr is None, calculates standard AUROC.
+    """
+
+    def __init__(self, max_fpr: None | float = 0.2) -> None:
+        self.max_fpr = max_fpr
+
+    def calculate(
+        self,
+        y: pd.Series,  # type: ignore
+        y_hat_prob: PredProbaSeries,
+        name_prefix: str | None = None,
+    ) -> CalculatedMetric:
+        return CalculatedMetric(
+            name=f"{name_prefix}_ConcentratedBinaryAUROC"
+            if name_prefix
+            else "ConcentratedBinaryAUROC",
+            value=float(roc_auc_score(y_true=y, y_score=y_hat_prob, max_fpr=self.max_fpr)),
+        )
