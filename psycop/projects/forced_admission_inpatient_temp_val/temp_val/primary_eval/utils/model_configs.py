f7938f0c11bdf11b75929420f2a1b58ab1c40884
erikperfalk <erik.perfalk@gmail.com>
2026-06-04 11:18:32 +0200

    clean-up

--- psycop/projects/forced_admission_inpatient_temp_val/temp_val/primary_eval/utils/model_configs.py
+++
@@ -1,7 +0,0 @@
-MODELS = {
-    "full_model_without_text_features": {
-        "group_name": "photodramaturgy-missilry",
-        "algorithm": 1,  # 0 fo logistic regression and 1 for best xgboost
-        "project_model_dir": "E:/shared_resources/forced_admissions_inpatient/models/full_model_without_text_features/pipeline_eval",
-    }
-}
